d9b07471a56e0beb2c8376db454867c2a3737a8b
Bob Walker <bobw@bobw-lap2.local>
2008-08-06 14:26:49 -0700

    Corrected so that Config::CONFIG no longer returns nil, which was causing problems loading mpec/guards/guard.rb

--- src/lib/rbconfig.rb
+++ src/lib/rbconfig.rb
@@ -1,6 +1,15 @@
-module RbConfig
+# In other rubies this file gets created by mkconfig.rb when ruby is built. It's 
+# usually used to describe various settings and other information used in the build
+# For more informations refer to pickaxe p.183
+# 
+# Various files in the MSpec libs require this Module. At this point it's hard coded
+# rather than generated to fulfill the mspec requirement.
+#
+module Config
     CONFIG = {}
     CONFIG['bindir'] = ""
     CONFIG['ruby_install_name'] = ""
     CONFIG['EXEEXT'] = ""
 end
+
+RbConfig = Config
