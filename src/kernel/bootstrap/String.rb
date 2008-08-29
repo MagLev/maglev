c9011a768a575f800fd594cbe76608bbb5156832
Peter McLain <peter.mclain@gemstone.com>
2008-08-29 13:58:20 -0700

    String.rb: Rorder, reformat and add MNI comments

--- src/kernel/bootstrap/String.rb
+++ src/kernel/bootstrap/String.rb
@@ -1,312 +1,405 @@
 class String
 
-    primitive '[]' , '_rubyAt:'
-    primitive '[]' , '_rubyAt:length:'
-    primitive 'slice', '_rubyAt:'
-    primitive 'slice', '_rubyAt:length:'
+  # misc
+  primitive 'hash'
 
-    primitive '[]=', '_rubyAt:put:'
-    primitive '[]=', '_rubyAt:length:put:'
+  def to_rx
+    Regexp.new(self)
+  end
 
-    primitive '==', '='
-    primitive 'hash'
-    primitive 'length', 'size'
-    primitive 'size', 'size'
-    primitive 'size=', 'size:'
+  primitive 'size=', 'size:'
 
-    # note smalltalk addAll:  returns arg, not receiver
-    primitive '<<', '_rubyAddAll:'
+  primitive 'substring1', 'copyFrom:to:'
+  primitive '_findStringStartingAt', 'findString:startingAt:'
 
-    primitive '+', ','
+  def signal
+    raise RuntimeError, self
+  end
 
-    primitive 'replace', '_rubyReplace:'
-    primitive 'substring1', 'copyFrom:to:'
-    primitive '_strip', 'withBlanksTrimmed'
-    primitive 'to_f', 'asFloat'
-    primitive 'inspect', 'printString'
-    primitive 'empty?', 'isEmpty'
-    primitive 'dup', 'copy'
-    primitive 'unpack', 'rubyUnpack:'
-    primitive '_findStringStartingAt', 'findString:startingAt:'
-    primitive 'concat', '_rubyAddAll:'
-    primitive 'reverse', 'reverse'
+  # Class Methods
 
-    def signal
-        raise RuntimeError, self
+  # Instance Methods
+  def %(args)
+    a = Array(args).dup
+    gsub(/%(\d|\.)*./){a.shift.to_fmt}
+  end
+
+  def *(n)
+    str = ""
+    n.times{str << self}
+    str
+  end
+
+  primitive '+', ','
+
+  # note smalltalk addAll:  returns arg, not receiver
+  primitive '<<', '_rubyAddAll:'
+
+  # TODO: Need to find ST implemention for <=>
+  def <=>(o)
+    other = Type.coerce_to(o, String, :to_s)
+
+    i = 0
+    lim = size > other.size ? other.size : size # lim is the min
+    while i < lim
+      result = self[i] <=> other[i]
+      return result if result != 0
+      i += 1
+    end
+    size <=> other.size
+  end
+
+  # PERFORMANCE: String#== could use help
+  primitive '==', '='
+  #   def ==(other)
+  #     if other.kind_of?(String)
+  #       (self <=> other) == 0
+  #     elsif other.respond_to? :to_str
+  #       other == str
+  #     else
+  #       false
+  #     end
+  #   end
+
+  #   alias '===' '=='
+
+  # =~ is implemented somewhere....
+
+  primitive '[]' , '_rubyAt:'
+  primitive '[]' , '_rubyAt:length:'
+
+  primitive '[]=', '_rubyAt:put:'
+  primitive '[]=', '_rubyAt:length:put:'
+
+  # MNI: String#~
+
+  # MNI: capitalize
+  # MNI: capitalize!
+  # MNI: casecmp
+  # MNI: center
+
+  def chomp
+    if self[-1] == ?\r
+      return self[0..-2]
     end
 
-    def include?(item)
-        !self.index(item).nil?
-    end
-
-    def index(item, offset=nil)
-        if offset.nil?
-            item._index_string(self, 0)
-        else
-            offset = size + offset if offset < 0
-            string = self[offset..-1]
-            return item._index_string(string, offset)
-        end
-    end
-
-    def _index_string(string)
-        string._findStringStartingAt(self, 1) - 1
-    end
-
-    def strip
-        _strip
-    end
-
-    def gsub(regex, str, &block)
-       out = ""
-        start = 1
-        regex.to_rx.each_match(self) do |match|
-            out << substring1(start, match.begin(0))
-            if block
-                out << block.call.to_s
-            else
-                out << str
-            end
-            start = match.end(0) + 1
-        end
-        if start <= length
-            out << substring1(start, length)
-        end
-        out
-    end
-
-    def sub(regex, str, &block)
-        if match = regex.to_rx.match(self)
-           out = ""
-           out << self[0...(match.begin(0))]
-           if block
-                out << block.call.to_s
-            else
-                out << str
-            end
-           out << ((self[(match.end(0))...length]) || "")
-           out
-        else
-            dup
-        end
-    end
-
-    def sub!(regex, str, &block)
-        new = sub(regex, str, &block)
-        if new == self
-            nil
-        else
-            replace(new)
-            self
-        end
-    end
-
-    def scan(regex)
-        result = []
-        regex.to_rx.each_match(self) do |m|
-            result << m[0]
-        end
-        result
-    end
-
-    # asUppercase is a smalltalk primitive
-    primitive 'upcase', 'asUppercase'
-
-    # asLowercase is a smalltalk to:do: loop in CharacterCollection
-    primitive 'downcase', 'asLowercase'
-
-    primitive '_to_i', 'asInteger'
-    #  non-base-10 radix Ruby syntax not supported yet
-    primitive '_to_i', '_asInteger:'
-
-    def to_i
-        _to_i
-    rescue Exception
-        0
-    end
-
-    def hex
-        _to_i(16)
-    end
-
-    def *(n)
-        str = ""
-        n.times{str << self}
-        str
-    end
-
-    def ljust(n)
-        self
-    end
-
-    def to_s
-        self
-    end
-
-    def to_rx
-      Regexp.new(self)
-    end
-
-    def each_char
-        each_byte do |b|
-            temp = ' '
-            temp[0] = b
-            yield temp
-        end
-    end
-
-    def each_byte
-        0.upto(size-1) do |idx|
-            yield self[idx]
-        end
-    end
-
-    def each_line(&b)
-       /(.*)/.all_matches(self).each do |match|
-        str = match[1]
-        b.call(str)
-       end
-    end
-
-    def downcase!
-        replace(downcase)
-    end
-
-    def gsub!(regex, str, &block)
-        replace(gsub(regex, str, &block))
-    end
-
-    def strip!
-        replace(strip)
-    end
-
-    def chop!
-        replace(chop)
-    end
-
-    def chop
-        self[0..size-2]
-    end
-
-    def chomp
-      if self[-1] == ?\r
+    if self[-1] == ?\n
+      if self[-2] == ?\r
+        return self[0..-3]
+      else
         return self[0..-2]
       end
-
-      if self[-1] == ?\n
-        if self[-2] == ?\r
-          return self[0..-3]
-        else
-          return self[0..-2]
-        end
-      end
-
-      return self
     end
 
-    def chomp!
-      replace(chomp)
-    end
-
-    def _split_string(string, limit)
-        Regexp.new(self)._split_string(string, limit)
-    end
-
-    def split(pattern=nil, limit=nil)
-        return [] if empty?
-        pattern ||= ($; || " ")
-
-        pattern._split_string(self, limit)
-    end
-
-    def %(args)
-        a = Array(args).dup
-        gsub(/%(\d|\.)*./){a.shift.to_fmt}
-    end
-
-    def reverse!
-        replace(reverse)
-    end
-
-    def tr!(from, to)
-        map = []
-        if from[0] == ?^
-            for i in 0..255
-                unless(from.include? i)
-                    map[i] = to[0]
-                end
-            end
-        else
-            if from[1] == ?- && from.size == 3
-                start = from[0]
-                max = from[2]
-                if to[1] == ?- && to.size == 3
-                    offset = to[0] - start
-                    last = to[2]
-                    for i in start .. max
-                        n = i + offset
-                        n = last if n > last
-                        map[i] = n
-                    end
-                else
-                    for i in start .. max
-                        map[i] = to[i - start] || to[-1]
-                    end
-                end
-            else
-                for i in 0 ... from.size
-                    map[from[i]] = to[i] || to[-1]
-                end
-            end
-        end
-
-        for i in 0 ... size
-            if c = map[self[i]]
-                self[i] = c
-            end
-        end
-        self
-    end
-
-    def tr(from, to)
-        dup.tr!(from, to)
-    end
-
-    def match(pattern)
-      regexp = if pattern.kind_of?(Regexp)
-                 pattern
-               elsif pattern.respond_to?(:to_str)
-                 Regexp.new(pattern.to_str)
-               else
-                 raise TypeError, "wrong argument type #{pattern.class} (expected Regexp)"
-               end
-      regexp.match self
-    end
-
-
-    # RxINC: Need to find implemention for <=>
-    def <=>(o)
-      other = Type.coerce_to(o, String, :to_s)
-      # RxINC:
-      raise TypeError, "String#\<=> passed non-string: #{other.class}" unless
-        other.kind_of?(String)
-
-      i = 0
-      lim = size > other.size ? other.size : size # lim is the min
-      while i < lim
-        result = self[i] <=> other[i]
-        return result if result != 0
-        i += 1
-      end
-      size <=> other.size
-    end
-
-    # ====== Comparable:
-    # RxINC: This is a cut-n-paste to get things working for mspec.
-    # Need to either overwrite or allow a mixin.
-  def ==(other)
-    (self <=> other) == 0
+    return self
   end
+
+  def chomp!
+    replace(chomp)
+  end
+
+  def chop
+
+    self[0..size-2]
+  end
+
+  def chop!
+    replace(chop)
+  end
+
+  primitive 'concat', '_rubyAddAll:'
+
+  # MNI: count
+  # MNI: crypt
+  # MNI: delete
+  # MNI: delete!
+
+  # asLowercase is a smalltalk to:do: loop in CharacterCollection
+  primitive 'downcase', 'asLowercase'
+
+  def downcase!
+    replace(downcase)
+  end
+
+  # MNI: dump
+
+  # MNI: each
+
+  def each_byte
+    0.upto(size-1) do |idx|
+      yield self[idx]
+    end
+  end
+
+  # TODO: what is each_char?
+  def each_char
+    each_byte do |b|
+      temp = ' '
+      temp[0] = b
+      yield temp
+    end
+  end
+
+  def each_line(&b)
+    /(.*)/.all_matches(self).each do |match|
+      str = match[1]
+      b.call(str)
+    end
+  end
+
+  primitive 'empty?', 'isEmpty'
+
+  # MNI: eql?
+
+  def gsub(regex, str, &block)
+    out = ""
+    start = 1
+    regex.to_rx.each_match(self) do |match|
+      out << substring1(start, match.begin(0))
+      if block
+        out << block.call.to_s
+      else
+        out << str
+      end
+      start = match.end(0) + 1
+    end
+    if start <= length
+      out << substring1(start, length)
+    end
+    out
+  end
+
+  def gsub!(regex, str, &block)
+    replace(gsub(regex, str, &block))
+  end
+
+  def hex
+    _to_i(16)
+  end
+
+  def include?(item)
+    !self.index(item).nil?
+  end
+
+  def index(item, offset=nil)
+    if offset.nil?
+      item._index_string(self, 0)
+    else
+      offset = size + offset if offset < 0
+      string = self[offset..-1]
+      return item._index_string(string, offset)
+    end
+  end
+
+  def _index_string(string)
+    string._findStringStartingAt(self, 1) - 1
+  end
+
+  # MNI: insert
+  # MNI: intern
+
+  primitive 'length', 'size'
+
+  def ljust(n)
+    self
+  end
+
+  # MNI: lstrip
+  # MNI: lstrip!
+
+  def match(pattern)
+    regexp = if pattern.kind_of?(Regexp)
+               pattern
+             elsif pattern.respond_to?(:to_str)
+               Regexp.new(pattern.to_str)
+             else
+               raise TypeError, "wrong argument type #{pattern.class} (expected Regexp)"
+             end
+    regexp.match self
+  end
+
+  # MNI: next
+  # MNI: next!
+  # MNI: oct   def oct; _to_i(8); end
+
+  primitive 'replace', '_rubyReplace:'
+  primitive 'reverse', 'reverse'
+
+  def reverse!
+    replace(reverse)
+  end
+
+  # MNI: rindex
+  # MNI: rjust
+  # MNI: rstrip
+  # MNI: rstrip!
+
+  def scan(regex)
+    result = []
+    regex.to_rx.each_match(self) do |m|
+      result << m[0]
+    end
+    result
+  end
+
+  primitive 'size', 'size'
+
+  primitive 'slice', '_rubyAt:'
+  primitive 'slice', '_rubyAt:length:'
+  # MNI: slice!
+
+  def split(pattern=nil, limit=nil)
+    return [] if empty?
+    pattern ||= ($; || " ")
+
+    pattern._split_string(self, limit)
+  end
+
+  def _split_string(string, limit)
+    Regexp.new(self)._split_string(string, limit)
+  end
+
+  # MNI: squeeze
+  # MNI: squeeze!
+
+  def strip
+    _strip
+  end
+  primitive '_strip', 'withBlanksTrimmed'
+
+  def strip!
+    replace(strip)
+  end
+
+  def sub(regex, str, &block)
+    if match = regex.to_rx.match(self)
+      out = ""
+      out << self[0...(match.begin(0))]
+      if block
+        out << block.call.to_s
+      else
+        out << str
+      end
+      out << ((self[(match.end(0))...length]) || "")
+      out
+    else
+      dup
+    end
+  end
+
+  def sub!(regex, str, &block)
+    new = sub(regex, str, &block)
+    if new == self
+      nil
+    else
+      replace(new)
+      self
+    end
+  end
+
+
+  # MNI: succ
+  # MNI: succ!
+  # MNI: sum
+  # MNI: swapcase
+  # MNI: swapcase!
+
+  primitive 'to_f', 'asFloat'
+  def to_i
+    _to_i
+  rescue Exception
+    0
+  end
+
+  def to_i
+    _to_i
+  rescue Exception
+    0
+  end
+  primitive '_to_i', 'asInteger'
+  #  non-base-10 radix Ruby syntax not supported yet
+  primitive '_to_i', '_asInteger:'
+
+  def to_s
+    self
+  end
+
+  def to_str
+    self
+  end
+
+  def tr(from, to)
+    dup.tr!(from, to)
+  end
+
+  def tr!(from, to)
+    map = []
+    if from[0] == ?^
+      for i in 0..255
+        unless(from.include? i)
+          map[i] = to[0]
+        end
+      end
+    else
+      if from[1] == ?- && from.size == 3
+        start = from[0]
+        max = from[2]
+        if to[1] == ?- && to.size == 3
+          offset = to[0] - start
+          last = to[2]
+          for i in start .. max
+            n = i + offset
+            n = last if n > last
+            map[i] = n
+          end
+        else
+          for i in start .. max
+            map[i] = to[i - start] || to[-1]
+          end
+        end
+      else
+        for i in 0 ... from.size
+          map[from[i]] = to[i] || to[-1]
+        end
+      end
+    end
+
+    for i in 0 ... size
+      if c = map[self[i]]
+        self[i] = c
+      end
+    end
+    self
+  end
+
+  # MNI: tr_s
+  # MNI: tr_s!
+
+  primitive 'unpack', 'rubyUnpack:'
+  primitive 'upcase', 'asUppercase'
+
+  # MNI: upcase!
+  # MNI: upto
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # ====== Object
+  primitive 'inspect', 'printString'
+  primitive 'dup', 'copy'
+
+  # ====== Comparable:
+  # RxINC: This is a cut-n-paste to get things working for mspec.
+  # Need to either overwrite or allow a mixin.
 
   def >(other)
     (self <=> other) > 0
