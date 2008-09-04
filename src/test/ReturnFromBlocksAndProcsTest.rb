cd5fa4ecb81a2d8302b283ad431cd469987df255
Peter McLain <peter.mclain@gemstone.com>
2008-09-04 16:56:10 -0700

    Added test for difference in semantics or return between raw procs and lambdas

--- src/test/ReturnFromBlocksAndProcsTest.rb
+++ src/test/ReturnFromBlocksAndProcsTest.rb
@@ -1,15 +1,5 @@
-
 # See pickaxe p 359
 #
-# A return from a proc object, Proc.new, returns from the calling context,
-# so it should not get past the p.call line
-def test_from_raw_proc
-  p = Proc.new { return 99 }
-  p.call
-  raise "FAIL: should not get here..."
-end
-raise "FAIL: raw proc" unless test_from_raw_proc == 99
-
 
 # A return from within a lambda returns to the calling context, so this
 # method should continue past the l.call line.
@@ -29,4 +19,22 @@ def test_from_deprecated_proc
   return 88 # return something different from the lambda
 end
 raise "FAIL: deprecated proc" unless test_from_deprecated_proc == 88
+
+# TODO: Currently, the next test case fails in MagLev with:
+#
+#     topaz 1> error during /Users/pmclain/MagLev/maglev-git/src/test/ReturnFromBlocksAndProcsTest.rb
+#     -----------------------------------------------------
+#     GemStone: Error         Nonfatal
+#     Creating an instance of class ExecBlock is not allowed.
+#     Error Category: [GemStone] Number: 2111 Arg Count: 1
+#     Arg 1: ExecBlock
+
+# A return from a proc object, Proc.new, returns from the calling context,
+# so it should not get past the p.call line
+def test_from_raw_proc
+  p = Proc.new { return 99 }
+  p.call
+  raise "FAIL: should not get here..."
+end
+raise "FAIL: raw proc" unless test_from_raw_proc == 99
 
