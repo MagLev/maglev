96b6e1f83520c19738bfa9dd13514461df94f038
Don Watson <dwatson@jupiter.gemstone.com>
2008-09-05 11:22:22 -0700

    Change raise from ERR to ERROR to match other tests.

--- src/test/testBlockArgs1.rb
+++ src/test/testBlockArgs1.rb
@@ -3,26 +3,26 @@ def test
 end
 test { | a, *b |
   unless a == 9
-    raise 'ERR'
+    raise 'ERROR'
   end
   unless b == [ 10 ]
-    raise 'ERR'
+    raise 'ERROR'
   end
 }
 
 # simple test of ranges
 r = 1..3 
 unless r.last == 3
-  raise 'ERR'
+  raise 'ERROR'
 end
 a = r.to_a 
 unless a = [1,2,3]
-  raise 'ERR'
+  raise 'ERROR'
 end
 r = Range.new(1,5)
 a = r.to_a 
 unless a = [1,2,3,4,5]
-  raise 'ERR'
+  raise 'ERROR'
 end
 
 true
