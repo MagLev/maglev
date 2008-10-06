438a135b25ae036f695d17777e36ea1a6c1a6719
Peter McLain <peter.mclain@gemstone.com>
2008-10-06 15:59:25 -0700

    Work in progress: Work on filling out MatchData and Regexp.  Not done, but vm-tests pass

--- src/kernel/bootstrap/Regexp.rb
+++ src/kernel/bootstrap/Regexp.rb
@@ -8,6 +8,7 @@ class Regexp
   primitive 'source', 'source'
   self.class.primitive 'alloc', '_basicNew'
 
+  # BEGIN RUBINIUS
   # Rubinius uses this method as the main interface to their primitives.
   # GemStone keeps the interface, but adjusts impl per the Smalltalk
   # interface.  Both Rubinius and GemStone use Oniguruma as the underlying
@@ -19,6 +20,12 @@ class Regexp
       search(str, finish, start)
     end
   end
+
+  def match_from(str, count)
+    return nil if str.nil?
+    search_region(str, count, str.size, true)
+  end
+  # END RUBINIUS
 
   def initialize(str, options, lang)
     if options._isInteger   # options.kind_of? Integer
@@ -182,4 +189,45 @@ class MatchData
   primitive '[]' , '_rubyAt:length:'
 
   # Ruby global variables $1..$9 implemented by MatchData(C)>>nthRegexRef:
+
+
+  # BEGIN RUBINIUS
+
+  def full; self.at(1); end  # GEMSTONE
+
+  def collapsing?
+    self.begin(0) == self.end(0)
+  end
+
+  def pre_match_from(idx)
+#    return "" if full.at(0) == 0
+    return "" if full == 0  # GEMSTONE
+    nd = full.at(0) - 1
+#    @source[idx, nd-idx+1]
+    source[idx, nd-idx+1]   # GEMSTONE
+  end
+
+  def captures
+    out = []
+
+#  TODO: WIP: finish
+#     @region.each do |tup|
+
+
+#       x = tup.at(0)
+
+#       if x == -1
+#         out << nil
+#       else
+#         y = tup.at(1)
+# #        out << @source[x, y-x]
+#         out << source[x, y-x]  # GEMSTONE
+#       end
+#     end
+    return out
+  end
+
+
+  # END RUBINIUS
+
 end
