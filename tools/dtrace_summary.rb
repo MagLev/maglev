d4cf6713c1d98305c0785c3ac46636e99e20a196
Peter McLain <peter.mclain@gemstone.com>
2008-10-20 09:09:07 -0700

    Tools: Refactored scripts.  Added new script. parse_spec_results.rb prints the methods present in a given dtrace run, whose ruby specs are not passing (i.e., a list of methods needed in order to run the program under dtrace).

--- tools/dtrace_summary.rb
+++ tools/dtrace_summary.rb
@@ -1,49 +1,76 @@
 #!/usr/bin/env ruby
 #
 # Summarize the dtrace output from rubymethods.d
-#
+
+$:.unshift File.dirname(__FILE__)
+require 'mri_info'
 
 
-# Assume mri_info.rb is in the same dir as this file...
-require File.join(File.dirname(__FILE__), 'mri_info.rb')
-include MRIInfo
+class GsDtrace
+  include MRIInfo
 
+  attr_reader :counts
 
-# Print a hash of hashes in a human readable format.
-# Wrap lines at 80 columns.
-def pprint(h)
-  start_col = 17
-  width = 80 - start_col
+  def initialize(file)
+    @counts = hash_for_file(file)
+  end
 
-  h.sort.each do |k,v|
-
-    printf "%-15s", k
-    cur_col = start_col
-
-    v.keys.sort.each do |k|
-      needed = k.length + 1
-      if cur_col + needed > width
-        printf "\n%s", " " * 15
-        cur_col = start_col
+  # Returns a hash of hashes for all the counts in +dtrace_file+, a file
+  # generated via the dtrace script +rubymethods.d+ in this directory.
+  # To access the counts:
+  #
+  #    h = hash_for_file('rails.dtrace')
+  #    class_name = :String
+  #    method_name = "==="
+  #    count = h[class_name][method_name]
+  #
+  def hash_for_file(file)
+    counts = Hash.new { |h,k| h[k] = Hash.new }
+    File.open(file) do |f|
+      # The lines with real data start with a '|' character and include the
+      # module or class name, method name and count
+      f.grep(/^\|/).each do |line|
+        cname, mname, count = line.split[1,3]
+        counts[cname][mname] = count if mri?(cname, mname)
       end
-      printf "%s ", k
-      cur_col += needed
     end
-    printf "\n\n"
+    counts
+  end
 
+  # Print a hash of hashes in a human readable format.
+  # Wrap lines at 80 columns.
+  def pprint
+    GsDtrace.pprint_hash(@counts)
+  end
+
+  # Pretty print a hash of hashes.  For each key in the hash, prints the
+  # sorted list of sub-keys and wraps at 80 characters.
+  def self.pprint_hash(h)
+    start_col = 17
+    width = 80 - start_col
+
+    h.sort.each do |k,v|
+      printf "%-15s", k
+      cur_col = start_col
+
+      v.keys.sort.each do |k|
+        needed = k.length + 1
+        if cur_col + needed > width
+          printf "\n%s", " " * 15
+          cur_col = start_col
+        end
+        printf "%s ", k
+        cur_col += needed
+      end
+      printf "\n\n"
+    end
   end
 end
 
-counts = Hash.new { |h,k| h[k] = Hash.new }
-# The lines with real data start with a '|' character and include the
-# module or class name, method name and count
-ARGF.grep(/^\|/).each do |line|
-  cname, mname, count = line.split[1,3]
-  counts[cname][mname] = count if mri?(cname, mname)
+if $0 == __FILE__
+  trace = GsDtrace.new(ARGV[0])
+  trace.pprint
 end
-
-pprint counts
-
 
 
 
---
+++ tools/parse_spec_results.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+#
+# This script attempts to show the methods used by a program, e.g., rails,
+# whose specs are not currently passing.  It require two files (A) the
+# foo.dtrace file (the output of a previous run of dtrace_summary.rb) and
+# (B) the output of a recent spec run.
+#
+# Usage:  parse_spec_results rails.dtrace.sum passed-Darwin.txt
+
+$:.unshift File.dirname(__FILE__)
+
+require 'dtrace_summary'
+require 'mri_info'
+
+include MRIInfo
+
+app_counts   = ARGV[0]  # The summary of dtrace output
+spec_results = ARGV[1]
+
+if ARGV.length != 2
+  puts "Usage: #{File.basename(__FILE__)}: dtrace_file spec_result"
+  exit 1
+end
+
+failing_spec_methods = Hash.new { |h,k| h[k] = Hash.new }
+
+File.open(spec_results) do |f|
+  f.each do |line|
+    if line =~ /\s*(.+)\/(.+)\/(.+)_spec.rb/
+      klass = $2.capitalize
+      method = $3
+      if mri?(klass, method)
+        #puts "#{$2} #{$3}: OK"
+      else
+        failing_spec_methods[klass][method] = 1
+        #puts "#{$2} #{$3}: BAD"
+      end
+    end
+  end
+end
+
+puts "==================== Failing Specs ===================="
+GsDtrace.pprint_hash failing_spec_methods
