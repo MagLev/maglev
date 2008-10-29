739476537fbfddcc41c0c7c0cbeb0b84fa25c427
Peter McLain <peter.mclain@gemstone.com>
2008-10-29 13:28:55 -0700

    Backout gem version check

--- bin/maglev_parser.rb
+++ bin/maglev_parser.rb
@@ -1,5 +1,4 @@
-require 'rubygems'
-gem 'ParseTree', "= 2.2.0"
+require 'parse_tree'
 
 class ParseTree
     attr_accessor :include_newlines
@@ -9,19 +8,19 @@ class MaglevParser
     def initialize
         @pt = ParseTree.new(true)
     end
-
+    
     def newlines=(boolean)
         @pt.include_newlines = boolean
     end
-
+    
     def parse_file(path)
         self.parse(File.open(path).read, File.basename(path), 0)
     end
-
+    
     def parse_string(string)
         self.parse(string, "(string)", 0)
     end
-
+    
     def parse(source, filename="(string)", line=0, method=nil)
       begin
         sexp = @pt.parse_tree_for_str(source, filename, 0).first
@@ -35,7 +34,7 @@ class MaglevParser
         end
       rescue Exception => ex
         sexp = [ :parse_error , ex.message ]
-        return sexp
+        return sexp 
       end
     end
 end
