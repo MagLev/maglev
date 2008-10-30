fa3c105e91a00cf23c3bbb32acd76634404aa08d
Peter McLain <peter.mclain@gemstone.com>
2008-10-30 14:47:37 -0700

    Ticket#206: Moved attr_* into behavior so they are seen by singleton classes.

--- src/kernel/bootstrap/Behavior.rb
+++ src/kernel/bootstrap/Behavior.rb
@@ -3,9 +3,32 @@ class Behavior
   primitive_nobridge 'include', 'includeRubyModule:'
   primitive 'alias_method', 'rubyAlias:from:'
 
+  def attr_accessor(*names)
+    names.each do |n|
+        attr_reader(n)
+        attr_writer(n)
+    end
+  end
+
+  def attr_reader(*names)
+    names.each do |n|
+        module_eval "def #{n}; @#{n}; end"
+    end
+  end
+
+  def attr_writer(*names)
+    names.each do |n|
+        module_eval "def #{n}=(v); @#{n} = v; end"
+    end
+  end
+
+  def module_eval(str)
+    RUBY.module_eval(str, self)
+  end
+
   def protected(name)
   end
-  
+
   def private(name)
   end
 
@@ -14,11 +37,11 @@ class Behavior
 
   def alias(name)
   end
-  
+
   def inspect
     name
   end
-  
+
   def to_s
     name
   end
