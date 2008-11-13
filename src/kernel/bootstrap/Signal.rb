2603e9a99f64ab6dbd3a01c458c88d60f6926a3f
Peter McLain <peter.mclain@gemstone.com>
2008-11-12 20:34:28 -0800

    Stub out Signal.rb to help get mspecs running

---
+++ src/kernel/bootstrap/Signal.rb
@@ -0,0 +1,12 @@
+module Signal
+  # We currently don't have any signals...
+  SIGNALS = { }.freeze  # Hash of Signal name to number
+
+  def self.trap(sig, command=nil, &block)
+    # TODO: Signal.trap: MNI  Not Implemented
+  end
+
+  def self.list
+    SIGNALS
+  end
+end
