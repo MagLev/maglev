8c53baafc23d16f256f577eb3694bca0c1c7309f
Peter McLain <peter.mclain@gemstone.com>
2008-11-21 15:16:19 -0800

    Clean cruft

--- src/kernel/bootstrap/Module.rb
+++ src/kernel/bootstrap/Module.rb
@@ -10,11 +10,9 @@ class Module
   # puts, they are temporarily there.  As they are implemented, we should
   # pull them into here.
 
-  def const_set(symbol, obj)
-    nil  # TODO: MNI: stubbed
-  end
-
   primitive_nobridge 'constants', 'rubyConstants'
+  primitive_nobridge 'const_get', 'rubyGlobalAt:'
+  primitive_nobridge 'const_set', 'rubyConstAt:put:'
   primitive_nobridge 'include', 'includeRubyModule:'
 
   # Invoked as a callback when a method is added to the reciever
@@ -47,12 +45,5 @@ class Module
   def name
     _name.to_s
   end
-  primitive_nobridge 'const_get', 'rubyGlobalAt:'
-  primitive_nobridge 'const_set', 'rubyConstAt:put:'
-
-#   def const_set(symbol, obj)
-#     puts "== WARN: STUB: MNI: Module#const_set"
-#     nil
-#   end
 
 end
