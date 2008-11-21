aedf16f3113ba1f7bbe98c75caeef3676a93ce5f
Allen Otis <otisa@abaco.gemstone.com>
2008-11-21 10:28:26 -0800

    add test for ticket 257

--- src/test/Trac257.rb
+++ src/test/Trac257.rb
@@ -3,8 +3,8 @@ h1 = { :a => 2, :b => -1 }
 h2 = { :a => -2, :c => 1 }
 h3 = {:c => 1, :b => -1, :a => 3.14}  # guess we're not in Kansas any more...
 
-x = h1.update(h2) { |k,x,y| 3.14 }
-raise "Error 1" unless x  == h3
+hx = h1.update(h2) { |k,x,y| 3.14 }
+raise "Error 1" unless hx  == h3
 raise "Error 2" unless h1 == h3
 
 
@@ -13,8 +13,8 @@ h1 = { :a => 2, :b => -1 }
 h2 = { :a => -2, :c => 1 }
 h3 = {:c => 1, :b => -1, :a => 3.14}
 
-x = h1.send(:update, h2) { |k,x,y| 3.14 }
+hx = h1.send(:update, h2) { |k,x,y| 3.14 }
 # Maglev triggers both of these errors
-raise "Error 3" unless x == h3
+raise "Error 3" unless hx == h3
 raise "Error 4" unless h1 == h3
 
