3686f3dc2afd8d5dd84f54b4bc949db8076246b6
Allen Otis <otisa@abaco.gemstone.com>
2008-12-09 16:29:26 -0800

    edits for debugging

--- src/test/Trac269.rb
+++ src/test/Trac269.rb
@@ -1,7 +1,12 @@
 require File.expand_path('simple', File.dirname(__FILE__))
-require 'URI'
+require 'uri'
 
-pattern = /\A(#{URI::REGEXP::PATTERN::HOST})(?::(\d+))?\z/n
+nil.pause
+patt = /\A(#{URI::REGEXP::PATTERN::HOST})(?::(\d+))?\z/n
+
+str = 'localhost:3333'
+a = str.scan(pattern)
+nil.pause
 
 test('localhost:3333'.scan(pattern), [["localhost", "3333"]], 'A')
 
