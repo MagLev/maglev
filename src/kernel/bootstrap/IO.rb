4f4c967e544279e96a8e5d731775cff54dd22398
Peter McLain <peter.mclain@gemstone.com>
2008-12-23 10:41:45 -0800

    Added more constants to help sinatra/rack

--- src/kernel/bootstrap/IO.rb
+++ src/kernel/bootstrap/IO.rb
@@ -1,46 +1,42 @@
 class IO
 
-#  module Constants
-    # MRI, on a Mac, has the following constants defined:
+  module Constants
     FNM_SYSCASE  = 0x00
     FNM_NOESCAPE = 0x01
     FNM_PATHNAME = 0x02
     FNM_DOTMATCH = 0x04
     FNM_CASEFOLD = 0x08
 
-    # ...and these as well.
-    # TODO: Need to ensure the File inherits these.
-    #     LOCK_EX
-    #     LOCK_NB
-    #     LOCK_SH
-    #     LOCK_UN
+    # For flock(2)
+    LOCK_SH      = 0x01         # shared file lock
+    LOCK_EX      = 0x02         # exclusive file lock
+    LOCK_NB      = 0x04         # non block when locking
+    LOCK_UN      = 0x08         # unlock
 
-    #     APPEND
-    #     CREAT
-    #     EXCL
-    #     NOCTTY
-    #     NONBLOCK
-    #     SYNC
-    #     TRUNC
+    # Flags for open(2), fcntl(2)
+    APPEND       = 0x0008       # append mode
+    CREAT        = 0x0200       # create if not there
+    EXCL         = 0x0800       # open with exclusive lock
+    NOCTTY       = 0x20000      # open with no controlling tty
+    NONBLOCK     = 0x0004       # don't block
+    SYNC         = 0x0080       # sync
+    TRUNC        = 0x0400       # truncate at open
 
-    #     RDONLY
-    #     RDWR
-    #     WRONLY
+    RDONLY       = 0x0000       # open readonly
+    WRONLY       = 0x0001       # open write only
+    RDWR         = 0x0002       # open read and write
 
-    #     SEEK_CUR
-    #     SEEK_END
-    #     SEEK_SET
-#  end
-#  include Constants  # NOTE: this won't make the constants visible until
-                     # trac ticket 249 is fixed.
+    SEEK_SET     = 0            # set file position to offset
+    SEEK_CUR     = 1            # set file position to current + offset
+    SEEK_END     = 2            # set file position to end of file + offset
+  end
+  include Constants
 
   primitive 'binmode', 'binmode'
   primitive 'fileno', 'fileDescriptor'
   primitive 'sync', 'sync'
   primitive 'sync=', 'setSync:'
-
   primitive 'stat',  'stat'
-
 
   def <<(anObj)
     unless anObj._isString
