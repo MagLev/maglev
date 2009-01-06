f5aaa6c0d4cece3022778edfc49cf0cc3e358099
Allen Otis <otisa@abaco.gemstone.com>
2009-01-06 14:34:21 -0800

    adjust test

--- src/test/RangeTest.rb
+++ src/test/RangeTest.rb
@@ -6,4 +6,11 @@ r.each { |i| results << i }
 
 test(results, ['A', 'B', 'C', 'D'], "('A'..'D') test")
 
+r = 1..3
+test( r.last , 3, "last")
+
+r = Range.new(1,5)
+test( r.to_a, [ 1,2,3,4,5] , "to_a2")
+
+
 report
