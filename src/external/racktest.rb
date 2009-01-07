32ad22b9accd177022309bb4af8b5557e1319c88
Peter McLain <peter.mclain@gemstone.com>
2009-01-06 17:31:28 -0800

    Rack: updated to latest version of rack.  Added a racktest.rb

---
+++ src/external/racktest.rb
@@ -0,0 +1,21 @@
+$:.unshift File.dirname(__FILE__) + '/rack-0.9.0/lib'
+
+require 'webrick'
+require 'rack'
+require 'rack/handler'
+require 'rack/handler/webrick'
+
+class RackTest
+  include WEBrick
+
+  def self.test
+    app = Proc.new do | env |
+      status = 200
+      headers = {"Content-Type" => "text/html"}
+      body = "<html><h1>Hello From Rack</h1></html>"
+      [status, headers, body]
+    end
+    Rack::Handler::WEBrick.run app, :Port => 3333
+  end
+end
+RackTest.test
