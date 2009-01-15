53b492dda8cfa5864c415d3082f7c9fe2d127691
Peter McLain <peter.mclain@gemstone.com>
2009-01-15 12:56:34 -0800

    Added RubyContextTest

---
+++ src/test/RubyContextTest.rb
@@ -0,0 +1,7 @@
+# This is just a smoke test to ensure that RubyContext.load and
+# RubyContext.save are available to ruby.  It passes by not raising an
+# exception.
+
+RubyContext.load
+RubyContext.save
+
