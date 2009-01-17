d8cce53913c4ab5de9f31256702cddb6749c158e
Peter McLain <peter.mclain@gemstone.com>
2009-01-16 17:35:49 -0800

    Tweaks for improved load and require handling

--- src/kernel/post_prims.rb
+++ src/kernel/post_prims.rb
@@ -4,5 +4,4 @@
 # variables referenced for the first time by the files required here will
 # be dynamic instance variables.
 
-# Use RUBY.require so that these files are not put on $"
 RUBY.require 'kernel/post_prims/Object.rb'
