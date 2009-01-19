e24b9acbd64b2613dd91e94baaa93ce272662b7a
Allen Otis <otisa@abaco.gemstone.com>
2009-01-19 10:54:47 -0800

    implemente define_method and method protection

--- src/kernel/bootstrap/UnboundMethod.rb
+++ src/kernel/bootstrap/UnboundMethod.rb
@@ -1,22 +1,19 @@
 class UnboundMethod
-  # UnboundMethod is identically Smalltalk RubyUnboundMethod
+  # UnboundMethod is identically Smalltalk RubyUnboundMeth
 
-  # Gemstone implementation differs in that Method is a
+  # Maglev implementation differs from MRI in that Method is a
   #  subclass of UnboundMethod, not of Object,
-  #  and that binding of selector to an actual method is done
-  #  by the  call method (which does a method lookup using perform)
-  #  instead of by the instance creation of Method .
 
   def self.name
     'UnboundMethod'
   end
 
-  primitive 'arity'
-
-  primitive_nobridge '_bind', '_bind:'
+  primitive_nobridge 'arity', 'arity'
+  primitive_nobridge 'bind', 'bind:'
+  primitive_nobridge '_home_class', 'homeClass'
 
   def bind(obj)
-    if (obj.kind_of?(@rcvrClass))
+    if (obj.kind_of?(@gsmeth._home_class))
       return _bind(obj)   # returns a Method
     else
       raise TypeError , ('obj must be kind_of ' << @rcvrClass.name )
