2e89e670320960853028bbe08298d689204fccf3
Allen Otis <otisa@abaco.gemstone.com>
2009-01-29 14:54:44 -0800

    more test coverage

--- src/test/Trac325.rb
+++ src/test/Trac325.rb
@@ -1,11 +1,37 @@
-module Haml
-  def self.version
-    puts "Testing @@version.."
-    return @@version if defined?(@@version)
-    puts "Setting @@version"  # MagLev does not get here
-    @@version = { :string => "foo" }
+module Mha
+  @@mywarn = 90
+  def warning
+    @@mywarn
   end
-  puts "Calling Haml.version"
-  VERSION = version[:string]
 end
-puts Haml::VERSION
+
+class Haml
+  include Mha
+  @@mywarn = 99
+  def self.version
+    y = defined?(@@version)
+    if y
+      return @@version
+    else
+      @@version = 88
+      55
+    end
+  end
+  def self.warning
+    @@mywarn
+  end
+end
+
+a = Haml.version
+b = Haml.version
+unless a == 55 ; raise 'error' ; end
+unless b == 88 ; raise 'error' ; end
+
+c = Haml.warning
+unless c == 99 ; raise 'error' ; end
+
+d = Haml.new.warning
+unless d == 90 ; raise 'error' ; end
+
+true
+
