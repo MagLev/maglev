cbe5bbf8c1d4ce52fa43134a2d822ba3f2c2a1c5
Allen Otis <otisa@abaco.gemstone.com>
2009-01-29 14:47:38 -0800

    implement Binding

---
+++ src/kernel/bootstrap/Binding.rb
@@ -0,0 +1,14 @@
+class Binding
+  # Binding is identically the Smalltalk class  RubyBinding
+ 
+  class_primitive_nobridge '_build_names', '_bindingInfo:'
+
+  def initialize( binding_context , obj, blk )
+    info = Binding._build_names( binding_context )
+    @staticLink = info[0] 
+    @names = info[1]
+    @selfObj = obj 
+    @block = blk
+  end
+
+end
