f4f9554da037b598f3af8cd2edcf51140d82334a
Peter McLain <peter.mclain@gemstone.com>
2009-02-03 09:56:11 -0800

    Fixed typo in sinatratest.rb

--- src/external/sintatratest.rb
+++ src/external/sintatratest.rb
@@ -1,4 +1,8 @@
-$:.unshift (ENV[:MAGLEV_HOME] + '/src/external/Sinatra/lib')
-$:.unshift (ENV[:MAGLEV_HOME] + '/src/external/Rack/lib')
+$:.unshift(ENV['MAGLEV_HOME'] + '/src/external/Sinatra/lib')
+$:.unshift(ENV['MAGLEV_HOME'] + '/src/external/Rack/lib')
 
 require 'sinatra'
+
+get '/hi' do
+  "Hello world"
+end
