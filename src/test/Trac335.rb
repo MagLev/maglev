bab399f355f3d48a0f71c6edc5a9373566915551
Allen Otis <otisa@abaco.gemstone.com>
2009-02-07 16:01:59 -0800

    adjust test

--- src/test/Trac335.rb
+++ src/test/Trac335.rb
@@ -1,7 +1,5 @@
 # Distilled from rubygems-1.3.1
 #
-# If you comment out the definition of Gem::LoadError, then the script runs
-# fine.  But with Gem::LoadError, the rescue clause does not get triggered.
 module Gem
   class LoadError < ::LoadError
     attr_accessor :name, :version_requirement
@@ -9,13 +7,14 @@ module Gem
 end
 
 class Gem::ConfigFile
-  system_config_path =
+  Y = 99
+  x =
     begin
       require 'Win32API'  # A file that should not exist
       'FAILURE'
-    rescue LoadError
+    rescue LoadError   # should bind to ::LoadError
       'SUCCESS'
     end
-  puts system_config_path  # should be SUCCESS
+  unless x == 'SUCCESS' ;  raise 'Error' ; end
 end
-
+true
