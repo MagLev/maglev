334a4cda48a7e9ef7ab6982d041f2fb57e9935aa
Peter McLain <peter.mclain@gemstone.com>
2009-02-09 14:43:44 -0800

    Fixed bug in test case

--- src/test/NamespaceTest.rb
+++ src/test/NamespaceTest.rb
@@ -6,14 +6,14 @@
 #       for the structs
 #   (B) The second one gave an error that #'' was already defined in the
 #       parent namespace.
-A = Struct.new(:token_number, :required, :column)
-B = Struct.new(:baz)
-a = A.new(12, true, 4)
+X = Struct.new(:token_number, :required, :column)
+Y = Struct.new(:baz)
+a = X.new(12, true, 4)
 raise "Failed token_number" unless a.token_number == 12
 raise "Failed required"     unless a.required
 raise "Failed column"       unless a.column == 4
 
-b = B.new("b")
+b = Y.new("b")
 raise "Failed B" unless b.baz == "b"
 
 # ###############################################################
