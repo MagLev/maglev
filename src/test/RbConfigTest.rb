f4024d9e67ee8afe27541a565cb95aa8467e022c
Peter McLain <peter.mclain@gemstone.com>
2009-02-20 13:37:01 -0800

    Fixed RbConfigTest for new paths

--- src/test/RbConfigTest.rb
+++ src/test/RbConfigTest.rb
@@ -5,17 +5,24 @@ require 'rbconfig'
 M = ENV['MAGLEV_HOME']
 A = `uname -m`
 
-test(RbConfig::CONFIG['rubylibdir'],  "#{M}/1.8",      'rubylibdir')
-test(RbConfig::CONFIG['archdir'],     "#{M}/1.8/#{A}", 'archdir')
-test(RbConfig::CONFIG['topdir'],      "#{M}/1.8/#{A}", 'topdir')
-test(RbConfig::CONFIG['datadir'],     "#{M}/data",     'datadir')
+test(RbConfig::CONFIG['libdir'],     "#{M}/lib",                'libdir')
+test(RbConfig::CONFIG['rubylibdir'], "#{M}/lib/ruby/1.8",       'rubylibdir')
+test(RbConfig::CONFIG['archdir'],    "#{M}/lib/ruby/1.8/#{A}",  'archdir')
+test(RbConfig::CONFIG['topdir'],     "#{M}/lib/ruby/1.8/#{A}",  'topdir')
 
-test(RbConfig::CONFIG['sitedir'],     "#{M}/site_ruby",          'siteruby')
-test(RbConfig::CONFIG['sitelibdir'],  "#{M}/site_ruby/1.8",      'sitelibdir')
-test(RbConfig::CONFIG['sitearchdir'], "#{M}/site_ruby/1.8/#{A}", 'sitearchdir')
+test(RbConfig::CONFIG['sitedir'],    "#{M}/lib/ruby/site_ruby", 'siteruby')
+test(RbConfig::CONFIG['sitelibdir'],
+  "#{M}/lib/ruby/site_ruby/1.8",
+  'sitelibdir')
 
-test(RbConfig::CONFIG['vendordir'],    "#{M}/vendor_ruby",          'vendorruby')
-test(RbConfig::CONFIG['vendorlibdir'], "#{M}/vendor_ruby/1.8",      'vendorlibdir')
-test(RbConfig::CONFIG['vendorarchdir'],"#{M}/vendor_ruby/1.8/#{A}", 'vendorarchdir')
+test(RbConfig::CONFIG['sitearchdir'], "#{M}/lib/ruby/site_ruby/1.8/#{A}",
+  'sitearchdir')
+
+test(RbConfig::CONFIG['vendordir'],    "#{M}/lib/vendor_ruby",
+  'vendorruby')
+test(RbConfig::CONFIG['vendorlibdir'], "#{M}/lib/vendor_ruby/1.8",
+  'vendorlibdir')
+test(RbConfig::CONFIG['vendorarchdir'],"#{M}/lib/vendor_ruby/1.8/#{A}",
+  'vendorarchdir')
 
 report
