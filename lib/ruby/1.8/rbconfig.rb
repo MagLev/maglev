974b5c6719c5adbe88d5680058f41a4b8f0a40e3
Peter McLain <peter.mclain@gemstone.com>
2009-02-19 17:36:59 -0800

    Fix up rbconfig paths

--- lib/ruby/1.8/rbconfig.rb
+++ lib/ruby/1.8/rbconfig.rb
@@ -1,11 +1,10 @@
-# In other rubies this file gets created by mkconfig.rb when ruby is
-# built. It's usually used to describe various settings and other
+# In other implementation this file gets created by mkconfig.rb when ruby
+# is built. It's usually used to describe various settings and other
 # information used in the build For more informations refer to pickaxe
 # p.183
 #
 # Various files in the MSpec libs require this Module. At this point it's
 # hard coded rather than generated to fulfill the mspec requirement.
-
 
 # TODO: To get a per-install view of this, without having to generate the
 # data each time the vm starts up, we can do the real work, once, when
@@ -13,31 +12,9 @@
 # now, we'll just calculate each time.
 
 module Config
-  # The proposed layout is fairly standard, except I'm putting bindir
-  # under the main dir:
-  #
-  #  +-$MAGLEV_HOME/
-  #    |
-  #    +-1.8/                      rubylibdir
-  #    | |
-  #    | +-<arch>/                 archdir, topdir
-  #    |
-  #    +-gems/
-  #    |
-  #    +-site_ruby/                sitedir
-  #    | |
-  #    | +-1.8/                    sitelibdir
-  #    |   |
-  #    |   +-<arch>/               sitearchdir
-  #    +-vendor_ruby/              vendordir
-  #      |
-  #      +-1.8/                    vendorlibdir
-  #        |
-  #        +-<arch>/               vendorarchdir
-  #
   RUBY_VERSION == "1.8.6" or
     raise "ruby lib version (1.8.6) doesn't match executable version (#{RUBY_VERSION})"
-  VERSION = '1.8'   # Used in paths below
+  VERSION = '1.8'
   ARCH = `uname -m`
 
   # Note: MAGLEV_HOME and TOPDIR should end up being the same
@@ -47,41 +24,52 @@ module Config
   # TODO: MRI puts rbconfig in the architecture specific subdir of
   # .../lib/ruby/1.8/<archdir>/rbconfig.rb
   TOPDIR = File.dirname(__FILE__).chomp!("/lib/ruby/1.8")
+  DESTDIR = '' unless defined? DESTDIR
+
 
   CONFIG = {}
-  CONFIG['EXEEXT']            = ''
-  CONFIG['BASERUBY']          = 'ruby'  # MRI ruby used to build maglev-ruby?
-  CONFIG['RUBY_INSTALL_NAME'] = 'maglev-ruby'
-  CONFIG['ruby_install_name'] = CONFIG['RUBY_INSTALL_NAME']
-  CONFIG['RUBY_SO_NAME']      = CONFIG['RUBY_INSTALL_NAME']
-  CONFIG['arch']              = ARCH
-  CONFIG['ruby_version']      = '1.8.6'
+  CONFIG['prefix']            = MAGLEV_HOME
+  CONFIG['exec_prefix']       = MAGLEV_HOME
+  CONFIG['bindir']            = File.join(MAGLEV_HOME, 'bin')
+  CONFIG['sysconfdir']        = File.join(MAGLEV_HOME, 'etc')
+  CONFIG['includedir']        = File.join(MAGLEV_HOME, 'include')
+  CONFIG['libdir']            = File.join(MAGLEV_HOME, 'lib')
 
-  CONFIG['rubylibdir']        = File.join(MAGLEV_HOME, VERSION)
+  CONFIG['rubylibdir']        = File.join(CONFIG['libdir'], 'ruby', VERSION)
   CONFIG['archdir']           = File.join(CONFIG['rubylibdir'], ARCH)
   CONFIG['topdir']            = CONFIG['archdir']
 
-  CONFIG['bindir']            = File.join(MAGLEV_HOME, 'bin')
-  CONFIG['datadir']           = File.join(MAGLEV_HOME, 'share')
-
-  CONFIG['sitedir']           = File.join(MAGLEV_HOME, 'site_ruby')
+  CONFIG['sitedir']           = File.join(CONFIG['libdir'], 'ruby', 'site_ruby')
   CONFIG['sitelibdir']        = File.join(CONFIG['sitedir'], VERSION)
   CONFIG['sitearchdir']       = File.join(CONFIG['sitelibdir'], ARCH)
 
-  CONFIG['vendordir']         = File.join(MAGLEV_HOME, 'vendor_ruby')
+  CONFIG['DESTDIR']           = DESTDIR
+
+  # CONFIG['sbindir']         = File.join(MAGLEV_HOME, 'sbin')
+  # CONFIG['libexecdir']      = File.join(MAGLEV_HOME, 'libexec')
+  CONFIG['datadir']           = File.join(MAGLEV_HOME, 'share')
+  CONFIG['datarootdir']       = File.join(MAGLEV_HOME, 'share')
+  CONFIG['docdir']            = File.join(CONFIG['datarootdir'], 'doc')
+  CONFIG['infodir']           = File.join(CONFIG['datarootdir'], 'info')
+  CONFIG['htmldir']           = CONFIG['docdir']
+
+  CONFIG['vendordir']         = File.join(CONFIG['libdir'], 'vendor_ruby')
   CONFIG['vendorlibdir']      = File.join(CONFIG['vendordir'], VERSION)
   CONFIG['vendorarchdir']     = File.join(CONFIG['vendorlibdir'], ARCH)
 
-  CONFIG['libdir']            = File.join(MAGLEV_HOME, 'gemstone/lib')
+  # TODO SHELL
+  # TODO PATH_SEPARATOR
+  CONFIG['arch']              = ARCH
+  CONFIG['ruby_version']      = '1.8.6'
+  CONFIG['host_os']           = %w( not_used sparc_solaris linux-gnu PowerPC_AIX
+                                    darwin9.0 x86_64_Solaris Itanium_HP-UX
+                                 )[Exception._cpuOsKind - 1]
 
-  # TODO: this is usually something like /usr/share need to decide where to
-  # put this
-  CONFIG['datadir']           = File.join(MAGLEV_HOME, 'data')
-
-  CONFIG['host_os'] = %w( not_used sparc_solaris linux-gnu PowerPC_AIX
-                          darwin9.0 x86_64_Solaris Itanium_HP-UX
-                        )[Exception._cpuOsKind - 1]
-
+  CONFIG['EXEEXT']            = ''
+  CONFIG['ruby_install_name'] = 'maglev-ruby'
+  CONFIG['RUBY_INSTALL_NAME'] = CONFIG['ruby_install_name']
+  CONFIG['RUBY_SO_NAME']      = CONFIG['ruby_install_name']
+  CONFIG['BASERUBY']          = 'ruby'  # MRI ruby used to build maglev-ruby?
 end
 
 RbConfig = Config
