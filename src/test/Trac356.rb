2c988d9ae3ce114832031fbfb46dfb6598dc306d
Allen Otis <otisa@abaco.gemstone.com>
2009-02-23 10:00:08 -0800

    enable tests 356, 359

--- src/test/Trac356.rb
+++ src/test/Trac356.rb
@@ -1,10 +1,20 @@
 
+def test356
+  ignore = [ /a/, /b/ ]
+  x = []
+  y = []
 
-ignore = [ /a/, /b/ ]
-
-[ "a", "b", "c", "d" ].find do |file|
-  puts "== #{file}"
-  next if ignore.any? { |pattern| file =~ pattern }
-  puts "== #{file} passed"
-  nil
+  [ "a", "b", "c", "d" ].find do |file|
+    x << file
+    if ignore.any? { |pattern| file =~ pattern }
+      next
+    end
+    y << file
+    nil
+  end
+  unless x == ["a", "b", "c", "d"] ; raise 'err' ; end
+  unless y == ["c", "d"] ; raise 'err' ; end
 end
+
+test356
+true
--- src/test/Trac359.rb
+++ src/test/Trac359.rb
@@ -1,21 +1,41 @@
-# the initialzie function does not get called in MagLev (but does in MRI).
+# the initialize function does not get called in MagLev (but does in MRI).
 # So, @names is nil during the [] call
 
 # Case 1:
 class HeaderHash < Hash
-  def initialize(hash={ })
+  def initialize(hash= {  }  )
     puts "In HeaderHash#initialize"
-    @names = { }
+    @names = hash
   end
 
   def [](k)
-    idx = @names[k.downcase]  # @names is nil....
+    if @names.equal?(nil) ; raise 'error'; end
+    idx = @names[k.downcase]  
     super idx
   end
+
+  def []=(k, v)
+    @names[k.downcase] = k
+    super(k,v)
+  end
+
+  def self.testh(h)
+    h['ABC'] = 55
+    hv = h['abc'] 
+    unless hv.equal?(55) ; raise 'error'; end
+    hv = h['AbC']
+    unless hv.equal?(55) ; raise 'error'; end
+    hv = h.fetch('AbC', 99) ;
+    unless hv.equal?(99) ; raise 'error'; end
+    hv = h.fetch('ABC') ;
+    unless hv.equal?(55) ; raise 'error'; end
+  end
+
 end
 
 hh = HeaderHash.new   # HeaderHash#initialize is not called, so...
-hh['Content-Length']  # @names is nil at this point, and [] not found
+HeaderHash.testh(hh)
+
 
 # Case 2: Subclass defines initialize with more parameters than superclass
 class HeaderHash2 < Hash
@@ -29,14 +49,25 @@ class HeaderHash2 < Hash
     idx = @names[k.downcase]  # @names is nil....
     super idx
   end
+
+  def []=(k, v)
+    @names[k.downcase] = k
+    super(k,v)
+  end 
 end
+
 
 # Problem 2a: MagLev should throw an ArgumentError but doesn't
 # MRI throws:
 #   pbm.rb:35:in `initialize': wrong number of arguments (0 for 3) (ArgumentError)
 #       from pbm.rb:35:in `new'
 #       from pbm.rb:35
-hh2 = HeaderHash2.new
+begin
+  hh2 = HeaderHash2.new
+  raise Exception, 'should not be here' 
+rescue ArgumentError
+  "ok"
+end
 
 # Problem 2b: MagLev throws, but shouldn't:
 #  $ maglev-ruby pbm.rb
@@ -45,5 +76,8 @@ hh2 = HeaderHash2.new
 #  ERROR 2023, Error, 'too many arguments'
 #  topaz 1>
 #
-hh2 = HeaderHash2.new(:a, :b, :c)
-
+h2cl = HeaderHash2
+hhb = HeaderHash2.new(:a, :b, :c)
+unless hhb.class == HeaderHash2 ; raise 'error' ; end
+HeaderHash.testh(hhb)
+true
