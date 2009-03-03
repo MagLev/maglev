50dc8ffea38b6e442039742a1fc8bfdca41b62b9
Peter McLain <peter.mclain@gemstone.com>
2009-03-03 09:36:33 -0800

    Added Trac372.rb

---
+++ src/test/Trac372.rb
@@ -0,0 +1,29 @@
+# Distilled from lib/ruby/1.8/singleton.rb
+#
+# rubygems => rdoc which uses singleton.
+
+module Singleton
+end
+
+class << Singleton
+  FirstInstanceCall = proc do
+    @__instance__ = new   # => Undefined method `new' for Singleton
+  end
+
+  def __init__(klass)
+    class << klass
+      define_method(:instance, FirstInstanceCall)
+    end
+    klass
+  end
+
+  def included(klass)
+    Singleton.__init__(klass)
+  end
+end
+
+class Options
+  include Singleton
+end
+
+Options.instance
