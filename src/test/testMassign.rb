5bf9b89ac169863413490da7541640a2a8a9ccf3
Allen Otis <otisa@abaco.gemstone.com>
2009-03-10 10:09:53 -0700

    enable coverage for trac 370

--- src/test/testMassign.rb
+++ src/test/testMassign.rb
@@ -1,3 +1,56 @@
+def expect(act, exp)
+  unless act == exp ; raise 'error'; end
+end
+
+ a , b  = 1, 2, 3
+   expect(a, 1)
+   expect(b, 2)
+ y = (a , b  = 1, 2, 3)
+   expect(y, [1,2,3])
+   expect(a, 1)
+   expect(b, 2)
+
+ a,b,c = 1,2
+   expect(a, 1)
+   expect(b, 2)
+   expect(c, nil)
+ y = (a,b,c = 1,2)
+   expect(y, [1,2]) 
+   expect(a, 1)
+   expect(b, 2)
+   expect(c, nil)
+
+ c, *d  = 4, 5, 6
+   expect(c ,4)
+   expect(d , [ 5, 6 ] )
+ y = ( c, *d  = 4, 5, 6 )
+   expect(y , [4,5,6])
+   expect(c ,4)
+   expect(d , [ 5, 6 ] )
+
+
+ x = [ 9, 8, 7]
+ a , b, * = x[0]
+   expect( a , 9 )
+   expect( b , nil )
+
+ x = [11,[22,[33,44]]]
+ a,(b,(c,d)) = x
+   expect( a , 11 )
+   expect( b , 22 )
+   expect( c , 33 )
+   expect( d , 44 )
+
+ b,c = c,b
+   expect( c , 22 )
+   expect( b , 33 )
+
+ x = [1,2,3,4]
+ y = x[0,2] += [5]
+   expect( y , [1,2,5] )
+   expect( x , [1,2,5,3,4] )
+ 
+
 class Holder
   def get
     @val
@@ -21,43 +74,18 @@ def raiseErr
   raise 'ERR'
 end
 
- a , b  = 1, 2, 3
- unless a == 1 then raiseErr end
- unless b == 2 then raiseErr end
- c, *d  = 4, 5, 6
- unless c == 4 then raiseErr end
- unless d == [ 5, 6 ] then raiseErr end
-
- # TODO , more test cases from p97..100 of OReilly Flanagan & Mats book"
 
  # cases from debugging ticket 112
  h = Holder.new		# RubyAttrAssignNode for store to h.val 
  h.val , b = 33 , 44
- unless b == 44 then raiseErr end
- unless h.get == 33 then raiseErr end
+   unless b == 44 then raiseErr end
+   unless h.get == 33 then raiseErr end
 
  p = 345
  a,(*b),c = 5,p
 
- x = [11,[22,[33,44]]]
- a,(b,(c,d)) = x
- unless a == 11 then raiseErr end 
- unless b == 22 then raiseErr end 
- unless c == 33 then raiseErr end 
- unless d == 44 then raiseErr end 
-
- x = [1,2,3,4]
- y = x[0,2] += [5]
- unless y == [1,2,5] then raiseErr end
- unless x == [1,2,5,3,4] then raiseErr end
- 
- x = [ 9, 8, 7]
- a , b, * = x[0]
- unless a == 9 then raiseErr end
- unless b == nil then raiseErr end
-
  h.set2(20,30)  # cover a pattern seen in Sinatra , instVar on LHS of masgn
  x = h.getb
- unless x == 30 then raiseErr end
+   unless x == 30 then raiseErr end
 
  true
