48f68d4c3e38710a0aa0c278c0e6baf0f8907c91
Allen Otis <otisa@abaco.gemstone.com>
2009-03-10 10:06:23 -0700

    Implement transient constant MAGLEV_MARSHAL_CLASS_CACHE and use it to delete Rubinius style metaclass() which creates too many singleton classes during marshalling.

--- src/kernel/common/misc.rb
+++ src/kernel/common/misc.rb
@@ -88,10 +88,14 @@ module RecursionGuard
   end
 end
 
-class Object
-  # Rubinius uses metaclass() in several files
-  def metaclass
-    class << self;self;end
-  end
-  alias_method :__metaclass__, :metaclass
-end
+#  Do not use Rubinius implementation of metaclass .
+#  any non-Singleton object for which it is invoked
+#  will have a singleton class added, which is very inefficient.
+#
+#  class Object
+#    # Rubinius uses metaclass() in several files
+#    def metaclass
+#      class << self;self;end
+#    end
+#    alias_method :__metaclass__, :metaclass
+#  end
