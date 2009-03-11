b85700a4a4a1f2fa89a95e5b8467b9856e7d20f6
Peter McLain <peter.mclain@gemstone.com>
2009-03-10 17:57:26 -0700

    Added Trac375.rb

---
+++ src/test/Trac375.rb
@@ -0,0 +1,25 @@
+# Distilled from Sinatra
+class MyApp
+  def configure(*args, &b)
+    SinatraApp.configure(*args, &b)
+  end
+end
+
+class Base
+  def self.configure(*args, &b)
+    puts args.inspect
+    raise "Expecting block" unless block_given?
+  end
+end
+
+
+class SinatraApp < Base
+  def self.configure(*envs)
+    super
+  end
+end
+
+app = MyApp.new
+app.configure do
+  puts "In a block"
+end
