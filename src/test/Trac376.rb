6917c7444e482d6981627640d9dad539a03321c7
Allen Otis <otisa@abaco.gemstone.com>
2009-03-27 13:41:47 -0700

    adjust for fix of 376

--- src/test/Trac376.rb
+++ src/test/Trac376.rb
@@ -1,12 +1,19 @@
 #  file Trac376.rb
-def bug(&block_arg)
-  block = block_arg
-  # block = Proc.new(&block_arg)   # workaround is to uncomment
-  def block.each
-    yield call   # To iterate over the block is to call it
+
+class C376
+  R = []
+  def bug(&block_arg)
+    block = block_arg
+    def block.each
+      yield call   # To iterate over the block is to call it
+    end
+    block.each { |r| R << r }
   end
-  block.each { |r| puts "Result: #{r}" }
 end
 
-bug { "hello" }        # Can't add singleton, receiver is invariant
-bug { puts "hello" }   # should not be in ExecBlock>>_rubyCall
+o = C376.new
+o.bug { "helloA" }        # Can't add singleton, receiver is invariant
+o.bug { "helloB" }   # should not be in ExecBlock>>_rubyCall
+
+unless C376::R == [ 'helloA' , 'helloB' ] ; raise 'error'; end 
+true
