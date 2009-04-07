c8b8581dec5ac5fb1f30c16cdb445e4d2402cbeb
Peter McLain <peter.mclain@gemstone.com>
2009-04-07 11:25:24 -0700

    Updated tests for better String#inspect impl

--- src/test/test_string_misc.rb
+++ src/test/test_string_misc.rb
@@ -49,19 +49,6 @@ foo = 1
 test("#{foo}".inspect, "\"1\"",         'inspect F')
 test("\t\n".inspect,   "\"\\t\\n\"",    'inspect G')
 
-
-# Test that string.inspect uses double quotes etc.
-test("".inspect,       "\"\"",          'inspect A')
-test("A".inspect,      "\"A\"",         'inspect B')
-test("\A".inspect,     "\"A\"",         'inspect C')
-test("\\A".inspect,    "\"\\\\A\"",     'inspect D')
-test("\\\"A".inspect,  "\"\\\\\\\"A\"", 'inspect E')
-
-foo = 1
-test("#{foo}".inspect, "\"1\"",         'inspect F')
-test("\t\n".inspect,   "\"\\t\\n\"",    'inspect G')
-
-
 # Integer conversions from PickAxe p 622
 test('12345'.to_i,           12345, '12345')
 test('99 red balloons'.to_i,    99, '99 red balloons')
@@ -108,6 +95,21 @@ test('0a'.to_i(0),  0, '"0a".to_i(0)')
 
 # Ensure succ is present
 test('zzz'.succ, 'aaaa', '"zzz".succ')
+
+# Create a string with each possible byte value and ensure the inspect of
+# that string is ok.
+r = ''
+255.times { |i| r << i }
+ins = r.inspect
+expected = "\"\\000\\001\\002\\003\\004\\005\\006\\a\\b\\t\\n\\v\\f\\r\\016\\017\\020\\021\\022\\023\\024\\025\\026\\027\\030\\031\\032\\e\\034\\035\\036\\037 !\\\"\\\#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\\177\\200\\201\\202\\203\\204\\205\\206\\207\\210\\211\\212\\213\\214\\215\\216\\217\\220\\221\\222\\223\\224\\225\\226\\227\\230\\231\\232\\233\\234\\235\\236\\237\\240\\241\\242\\243\\244\\245\\246\\247\\250\\251\\252\\253\\254\\255\\256\\257\\260\\261\\262\\263\\264\\265\\266\\267\\270\\271\\272\\273\\274\\275\\276\\277\\300\\301\\302\\303\\304\\305\\306\\307\\310\\311\\312\\313\\314\\315\\316\\317\\320\\321\\322\\323\\324\\325\\326\\327\\330\\331\\332\\333\\334\\335\\336\\337\\340\\341\\342\\343\\344\\345\\346\\347\\350\\351\\352\\353\\354\\355\\356\\357\\360\\361\\362\\363\\364\\365\\366\\367\\370\\371\\372\\373\\374\\375\\376\""
+
+puts "========= ins ============="
+puts ins
+puts "========= expected ============="
+puts expected
+
+
+test(ins, expected, 'All byte values inspected')
 
 def test_chomp
   s = 'abcd'
