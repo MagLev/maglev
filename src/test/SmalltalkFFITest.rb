4a746039449cc179c0ceaf6a0149aa69b453a438
Peter McLain <peter.mclain@gemstone.com>
2009-04-15 12:10:14 -0700

    Update gen_st_wrappers and add test case

---
+++ src/test/SmalltalkFFITest.rb
@@ -0,0 +1,10 @@
+require 'smalltalk/System'
+puts Smalltalk::System._st_inTransaction # Crashes system
+
+require 'smalltalk/SymbolSet'
+s = Smalltalk::SymbolSet._st_new(10)
+
+require 'smalltalk/IdentityBag'
+b = Smalltalk::IdentityBag._st_new(10)
+puts b._st_hash            OK
+puts b._st_getIndexInfo
