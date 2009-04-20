51264b64ecbe61083edcf0d625e6c5929bcf58d5
Peter McLain <peter.mclain@gemstone.com>
2009-04-20 12:59:33 -0700

    Added src/test/Trac447.rb

---
+++ src/test/Trac447.rb
@@ -0,0 +1,31 @@
+# This uses ActiveSupport's compress and decompress methods
+require File.dirname(__FILE__) + '/simple'
+require 'zlib'
+require 'stringio'
+
+class Stream < StringIO
+  def close
+    rewind
+  end
+end
+
+def decompress(source)
+  Zlib::GzipReader.new(StringIO.new(source)).read
+end
+
+def compress(source)
+  output = Stream.new
+  gz = Zlib::GzipWriter.new(output)
+  gz.write(source)
+  gz.close
+  output.string
+end
+
+original = File.read("gziptest.txt")
+zipped = compress(original)
+unzipped = decompress(zipped)
+
+test(unzipped, original, 'Test 1')
+
+report
+
