c1ce409fd39574628d56c06d6fd892100bb3e104
Peter McLain <peter.mclain@gemstone.com>
2009-04-23 15:31:14 -0700

    Add ruby gem defaults file for maglev.  This is not working right now due to a bug in option parsing

---
+++ lib/ruby/site_ruby/1.8/rubygems/defaults/maglev.rb
@@ -0,0 +1,24 @@
+#--
+# maglev.rb
+#
+# Copyright (C) 2009 GemStone Systems, Inc. All rights reserved.
+#
+#++
+
+# = Overview
+#
+# File read by RubyGems that allows Ruby implementers to set defaults
+# appropriate for their implementation.
+#
+# For more information on this file, see
+# http://www.mail-archive.com/rubygems-developers@rubyforge.org/msg02161.html
+puts "================= rubygems/defaults/maglev.rb ====================="
+
+require 'rubygems/config_file'
+
+module Gem
+  # Since maglev-ruby is a shell script, we want the shebang line for shell
+  # scripts created by RubyGems that invoke maglev-ruby to use "#!/usr/bin/env maglev-ruby"
+  ConfigFile::PLATFORM_DEFAULTS['install'] = '--env-shebang --no-rdoc --no-ri'
+  ConfigFile::PLATFORM_DEFAULTS['update']  = '--env-shebang --no-rdoc --no-ri'
+end
