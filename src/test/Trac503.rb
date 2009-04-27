f5e2e28fc41a15f49adfc58165be97f0cf90117d
Peter McLain <peter.mclain@gemstone.com>
2009-04-26 18:00:11 -0700

    Added src/test/Trac503.rb

---
+++ src/test/Trac503.rb
@@ -0,0 +1,26 @@
+#
+# Original problem:
+#
+#   $ maglev-ruby src/test/Trac503.rb
+#     ERROR 2013, aC cannot perform the selector #'method_missing_test'
+#     with the arguments in anArray( ). Perform may have been attempted
+#     with wrong number of args.aC cannot perform the selector
+#     #'method_missing_test' with the arguments in anArray( ).
+#     Perform may have been attempted with wrong number of args.
+#
+
+class C
+  def method_missing(symbol, *args)
+    puts "Method missing for #{symbol.inspect} called: #{args.inspect}"
+    $count += 1
+  end
+end
+
+$count = 0
+c = C.new
+c.send :method_missing_test
+c.send(:method_missing_test, [0, 1])
+c.send(:method_missing_test, 0) { true }
+
+expected = 3
+raise "Fail expected #{expected} was #{$count}" unless $count == expected
