0eb1409a581ad03c68ce996f25b5e5a617c52897
Peter McLain <peter.mclain@gemstone.com>
2009-05-15 09:35:04 -0700

    Symbol: fix problem with accidental inheritance of :to_str from String.

--- src/test/SymbolTest.rb
+++ src/test/SymbolTest.rb
@@ -5,5 +5,12 @@ require File.expand_path('simple', File.dirname(__FILE__))
 test(:s.to_i.class, Fixnum, 'to_i')
 test(Symbol.all_symbols.length > 0, true, 'all_symbols')
 
+# Because in ST Symbol derives from String, but in Ruby it does not,
+# need to ensure respond_to? works correctly
+test(:xyz.respond_to?(:to_str), false, "symbol should not respond to :to_str")
+test(:xyz.respond_to?(:to_s), true, "symbol should respond to :to_s")
+test(:x == "x", false, ":x == 'x'")
+test("x" == :x, false, "'x' == :x")
+
 report
 true
