59b7baebba547fbdfc51bbd3d4cffa966b9d85e9
Allen Otis <otisa@abaco.gemstone.com>
2009-05-29 14:16:17 -0700

    added

---
+++ src/kernel/parser/load_parser.rb
@@ -0,0 +1,15 @@
+# file parser.rb
+#   used for loading parser separately from bootstrap
+require 'kernel/parser/RpStClasses.rb'
+require 'kernel/parser/RubyNode.rb'
+require 'kernel/parser/ffi.rb'
+require 'kernel/parser/rp_classes.rb'
+require 'kernel/parser/strscan.rb'
+require 'kernel/parser/strscan2.rb'
+# require 'kernel/parser/sexp.rb'
+require 'kernel/parser/racc_def_parser.rb'
+require 'kernel/parser/extras.rb'
+require 'kernel/parser/lexer.rb'
+require 'kernel/parser/ruby_parser.rb'
+require 'kernel/parser/racc_init_parser.rb'
+require 'kernel/parser/racc_parser.rb'
