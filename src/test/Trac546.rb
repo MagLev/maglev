d383ef680d9634c867c7788dd79dc7975a728960
Peter McLain <peter.mclain@gemstone.com>
2009-06-22 13:24:31 -0700

    Added src/test/Trac546.rb

---
+++ src/test/Trac546.rb
@@ -0,0 +1,28 @@
+# Distilled from RubyGems
+module Gem
+  class Specification
+
+    def initialize
+      yield self if block_given?
+    end
+
+    def self.overwrite_accessor(name, &block)
+      puts "#{self}.overwrite_accessor(#{name}, #{block_given?})"
+      define_method(name, &block)
+    end
+
+    overwrite_accessor :version= do |version|
+      @version = version
+      return @version          # removing this line "fixes" the issue...
+    end
+
+  end
+end
+
+spec_code=<<EOS
+  Gem::Specification.new do |s|
+    s.version = 10
+  end
+EOS
+
+eval(spec_code, binding, "foo")
