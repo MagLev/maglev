31caf0184544c0e6dd02720f09da19362a4fd8e7
Peter McLain <peter.mclain@gemstone.com>
2009-07-08 09:11:42 -0700

    Start to move hat_trick to new persistence mode

--- examples/hat_trick/hat.rb
+++ examples/hat_trick/hat.rb
@@ -1,20 +1,26 @@
-class Hat
-	def initialize
-		@contents = []
-	end
-	
-	def put(item)
-		@contents << item
-		nil
-	end
-	
-	def contents
-		@contents
-	end
-	
-	def size
-		@contents.size
-	end
-end
+# Define a Hat class that is persistent
+Maglev.persistent do
+  class Hat
+    def initialize
+      @contents = []
+    end
 
-$hat = Hat.new
+    def put(item)
+      @contents << item
+      nil
+    end
+
+    def contents
+      @contents
+    end
+
+    def size
+      @contents.size
+    end
+  end
+
+  ::HAT = Hat.new
+end
+Maglev.commit_transaction # Save HAT and the class definition
+
+puts "::HAT: #{::HAT.inspect}"
--- examples/hat_trick/show_hat_contents.rb
+++ examples/hat_trick/show_hat_contents.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env maglev-ruby
 # Print the contents of the hat
 
-require 'rabbit.rb'
-puts "Do you know how many objects are in the hat?\nPerhaps there are #{$hat.size}."
-puts "You can count them for yourself ..."
-print $hat.contents.inspect
+# require 'rabbit.rb'
+# puts "Do you know how many objects are in the hat?\nPerhaps there are #{::HAT.size}."
+# puts "You can count them for yourself ..."
+# print ::HAT.contents.inspect
+p ::HAT
