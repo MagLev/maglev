52cd7a93229759eaeeb24943446f56d85b8e97fe
Peter McLain <peter.mclain@gemstone.com>
2009-07-15 14:30:17 -0700

    Added non-zero exit case to exit tests

--- src/test/ExitTest.rb
+++ src/test/ExitTest.rb
@@ -2,12 +2,19 @@
 # (success).  Exiting by raising a SystemExit should be ok...
 require File.expand_path('simple', File.dirname(__FILE__))
 
-["raise SystemExit", "exit 0"].each do |s|
-  sys_val = system "maglev-ruby -e '#{s}'"
-  exit_val = $?.to_i
-  test(sys_val, true,  "[#{s}]: sys_val")
-  test(exit_val, 0, "[#{s}]: exit_val")
+def dash_e(cmd, expected_bool, expected_num)
+  sys_val = system "maglev-ruby -e '#{cmd}'"
+  exit_val = $?.exitstatus
+  test(sys_val,  expected_bool, "[#{cmd}] via -e: sys_val")
+  test(exit_val, expected_num,  "[#{cmd}] via -e: exit_val")
 end
 
-report
+# Test exit 0 via -e
+dash_e("raise SystemExit", true, 0)
+dash_e("exit 0", true, 0)
 
+# Test non-zero exit status via -e
+dash_e("raise SystemExit.new(22)", false, 22)
+dash_e("exit 44", false, 44)
+
+report
