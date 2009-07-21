14b11a33fdf8ade05a2a43908f3a088f0abb2d80
Monty Williams <monty.williams@gemstone.com>
2009-07-21 11:56:16 -0700

    iferror should have been iferr

--- rakelib/contrib/ottobehrens/stone.rb
+++ rakelib/contrib/ottobehrens/stone.rb
@@ -221,8 +221,8 @@ class Stone
     commands << ["limit oops 100",
                  "limit bytes 1000",
                  "display oops",
-                 "iferror 1 stack",
-                 "iferror 2 exit" ]
+                 "iferr 1 stack",
+                 "iferr 2 exit" ]
     commands << user_commands
     commands << ["output pop",
                  "exit" ]
