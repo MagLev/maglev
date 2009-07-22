d5adca0c88716807ed503f75922ff44de25eeb08
Peter McLain <peter.mclain@gemstone.com>
2009-07-22 14:44:15 -0700

    Renamed rack-1.0.0 to rack, and moved all rack example apps to examples/rack.

--- examples/rack/racktest.rb
+++ examples/rack/racktest.rb
@@ -1,14 +1,13 @@
-# $:.unshift File.dirname(__FILE__) + '/Rack/lib'
-$:.unshift File.dirname(__FILE__) + '/rack-1.0.0/lib'
+$:.unshift "#{ENV['MAGLEV_HOME']}/src/external/rack/lib"
 
-require 'webrick'
 require 'rack'
+require 'webrick'
 
 # For debugging, stop on all exceptions
-Exception.install_debug_block do |e|
-  puts "=== caught #{e.inspect} ================="
-  nil.pause
-end
+# Exception.install_debug_block do |e|
+#   puts "=== caught #{e.inspect} ================="
+#   nil.pause
+# end
 
 class RackTest
   include WEBrick
@@ -20,7 +19,7 @@ class RackTest
       body = "<html><h1>Hello From Rack</h1><p>Rack.version: #{Rack.version}</p><p>Rack.release: #{Rack.release}</p></html>"
       [status, headers, body]
     end
-    Rack::Handler::WEBrick.run app, :Port => 3333
+    Rack::Handler::WEBrick.run app, :Port => 9292
   end
 end
 RackTest.test
