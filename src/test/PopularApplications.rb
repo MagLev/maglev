0dfa9fbdf8b2bf2f9d16b5401ab2a8040ba9070c
Peter McLain <peter.mclain@gemstone.com>
2009-07-22 15:07:14 -0700

    Update to new locations of rack and sinatra

--- src/test/PopularApplications.rb
+++ src/test/PopularApplications.rb
@@ -3,9 +3,23 @@
 
 ext_dir = File.dirname(__FILE__) + '/../external/'
 
-$: << ext_dir + 'rack-1.0.0/lib'
-$: << ext_dir + 'sinatra-0.9.3/lib'
+$: << ext_dir + 'rack/lib'
+$: << ext_dir + 'sinatra/lib'
 
 require 'webrick'
+
 require 'rack'
+# Rack autoloads everything, so you have to mention a constant in the
+# source to force loading of it.  We don't do complete coverage, but we try
+# to load a few things.
+Rack::Handler::WEBrick
+Rack::CommonLogger
+Rack::File
+Rack::Lint
+Rack::ShowStatus
+Rack::ShowExceptions
+Rack::Static
+Rack::Request
+Rack::Response
+
 #require 'sinatra'
