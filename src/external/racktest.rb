1ed356847ec3c2bfc2d56d5d6cb8a82c4acb9913
Peter McLain <peter.mclain@gemstone.com>
2009-07-22 11:02:18 -0700

    Support to start debugging Rack 1.0.0

--- src/external/racktest.rb
+++ src/external/racktest.rb
@@ -1,7 +1,14 @@
 $:.unshift File.dirname(__FILE__) + '/Rack/lib'
+# $:.unshift File.dirname(__FILE__) + '/rack-1.0.0/lib'
 
 require 'webrick'
 require 'rack'
+
+# For debugging, stop on all exceptions
+# Exception.install_debug_block do |e|
+#   puts "=== caught #{e.inspect} ================="
+#   nil.pause
+# end
 
 class RackTest
   include WEBrick
@@ -10,7 +17,7 @@ class RackTest
     app = Proc.new do | env |
       status = 200
       headers = {"Content-Type" => "text/html"}
-      body = "<html><h1>Hello From Rack</h1></html>"
+      body = "<html><h1>Hello From Rack</h1><p>Rack.version: #{Rack.version}</p><p>Rack.release: #{Rack.release}</p></html>"
       [status, headers, body]
     end
     Rack::Handler::WEBrick.run app, :Port => 3333
