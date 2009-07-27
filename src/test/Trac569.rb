0116312c8a085c3975adcb03040aafb95f5374cb
Peter McLain <peter.mclain@gemstone.com>
2009-07-27 10:29:43 -0700

    Added more failure cases

--- src/test/Trac569.rb
+++ src/test/Trac569.rb
@@ -1,13 +1,33 @@
 # From YAML: Looks like doing a for loop over the key/value pairs in a Hash
 # does not setup the correct handler for RubyBreakException.
+#
+# This test passes if there are no exceptions.
+
 regexes =  {
   'foo' => /foo/,
   'tag' => Regexp.new('^!ruby/object:'),
   'bar' => /bar/
 }
 
-for tag,reg in regexes
-  if reg =~ '!ruby/object:'
-    break
-  end
+
+# Each of the following loop forms is broken in MagLev:
+
+regexes.each_value do |x|
+  break
+end
+
+regexes.each_pair do |x,y|
+  break
+end
+
+regexes.each_key do |x|
+  break
+end
+
+regexes.each do |x|
+  break
+end
+
+for key,value in regexes do
+  break
 end
