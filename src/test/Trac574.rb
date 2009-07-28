8e362bd702e0ead1811f133a54b281d5088591bd
Peter McLain <peter.mclain@gemstone.com>
2009-07-28 15:14:14 -0700

    Added more tests

--- src/test/Trac574.rb
+++ src/test/Trac574.rb
@@ -1,2 +1,21 @@
-copy = YAML::load(YAML::dump(:foo))
-raise "YAML converted Symbol to #{copy.class}" unless Symbol === copy
+require File.expand_path('simple', File.dirname(__FILE__))
+require 'yaml'
+
+# Regular string
+test_cases =
+  [
+   ["foo",  "--- foo\n"],
+   [":foo", "--- \":foo\"\n"],
+   [:foo,   "--- :foo\n" ]
+  ]
+
+test_cases.each do |orig,yaml|
+  yaml_obj = YAML::dump(orig)
+  test(yaml_obj, yaml, "dumping #{orig.inspect}")
+
+  copy = YAML::load(yaml_obj)
+  test(orig, copy, "loading #{orig.inspect}")
+  test(orig.class, copy.class, "loading #{orig.inspect} class")
+end
+
+report
