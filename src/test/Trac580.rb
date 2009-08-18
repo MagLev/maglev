e9f128e15d456477f0577ec1f73354ebe8126830
Allen Otis <otisa@abaco.gemstone.com>
2009-08-18 10:51:02 -0700

    add coverage for trac 565

--- src/test/Trac580.rb
+++ src/test/Trac580.rb
@@ -1,3 +1,4 @@
+# coverage for Trac 580
 xx_map = {
       "\C-[" => 22 ,
       "\C-\\" => 33 ,
@@ -7,4 +8,16 @@ unless xx_map.size == 3 ; raise 'error'; end
 k = ' '
 k[0] = 28
 unless xx_map[k] == 33 ; raise 'error' ; end
+
+# Coverage for Trac565
+ra = /abc/iomxiomx
+rb = /abc/iomx
+unless ra == rb ; raise 'error'; end
+
+# coverage for lexing of octals
+na = 0o1234
+nb = 0O1234
+unless na == 668 ; raise 'error'; end
+unless nb == 668 ; raise 'error'; end
+
 true
