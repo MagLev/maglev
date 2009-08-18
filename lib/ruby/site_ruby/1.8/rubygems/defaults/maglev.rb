40cbbc2a5c8d35e5c9fe1a49eddbc61fefda0d62
Peter McLain <peter.mclain@gemstone.com>
2009-08-18 10:03:42 -0700

    Add comment on how to check for extensions

--- lib/ruby/site_ruby/1.8/rubygems/defaults/maglev.rb
+++ lib/ruby/site_ruby/1.8/rubygems/defaults/maglev.rb
@@ -22,4 +22,15 @@ module Gem
   # scripts created by RubyGems that invoke maglev-ruby to use "#!/usr/bin/env maglev-ruby"
   ConfigFile::PLATFORM_DEFAULTS['install'] = '--env-shebang --no-rdoc --no-ri'
   ConfigFile::PLATFORM_DEFAULTS['update']  = '--env-shebang --no-rdoc --no-ri'
+
+# This doesn't work, since @spec isn't set yet (@spec is set in the
+# initializer for Installer, but we don't seem to be going through that
+# before the install hooks are called....)
+#
+#   @pre_install_hooks ||= []
+#   @pre_install_hooks << lambda do
+#     raise ... unless @spec.extensions.empty?
+#     puts "============= HELLO with lambda #{@spec.inspect}==========="
+#     nil.pause
+#   end
 end
