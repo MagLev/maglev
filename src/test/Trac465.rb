860c96115244aca24b3c996a3ed17a76b6129b4a
Peter McLain <peter.mclain@gemstone.com>
2009-08-25 09:31:50 -0700

    Updated test case with new problems.

--- src/test/Trac465.rb
+++ src/test/Trac465.rb
@@ -1,5 +1,23 @@
 def g
-    yield
-    end
+  yield
+end
 
 g { break }
+
+
+# Further test cases: blocks and procs work differently
+def m1
+  yield
+  :m1
+end
+
+def m2(pr)
+  pr.call
+  :m2
+end
+
+x = m2(proc { break 37 } )
+raise "Expecting :m2 but got #{x} for break in block" unless x == :m2
+
+x = m1 {  break 44 }
+raise "Expecting 44  but got #{x} for break in proc"  unless x == 44
