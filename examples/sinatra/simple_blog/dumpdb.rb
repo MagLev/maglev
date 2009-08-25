ce5cd2ce53cf9e35a1f80da44899616650789dfb
Peter McLain <peter.mclain@gemstone.com>
2009-08-25 11:34:23 -0700

    Remove cruft files

--- examples/sinatra/simple_blog/dumpdb.rb
+++
@@ -1,6 +0,0 @@
-# This just prints out the current state of the blog
-Maglev::PERSISTENT_ROOT[Post].values.each { |p| p p}
-p Maglev::PERSISTENT_ROOT[Tag]
-
-puts "======== TAGS: "
-Maglev::PERSISTENT_ROOT[Tag].values.each { |t| puts "TAG: #{t.name}: #{t.inspect}"}
