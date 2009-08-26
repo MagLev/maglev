0fd441e4c0fcf7e116f4813b4287932ac336a256
Peter McLain <peter.mclain@gemstone.com>
2009-08-26 14:58:39 -0700

    Added object log viewer sample app.

--- lib/ruby/1.8/maglev/objectlog.rb
+++ lib/ruby/1.8/maglev/objectlog.rb
@@ -22,6 +22,21 @@ class ObjectLogEntry
   # Return receiver's priority attribute
   primitive_nobridge 'priority', 'priority'
 
+  # Return receiver's pid attribute
+  primitive_nobridge 'pid', 'pid'
+
+  primitive_nobridge '_timestamp', 'stamp'  # the timestamp is held as stamp
+  # Return receiver's timestamp attribute
+  def timestamp
+    _timestamp.as_time
+  end
+
+  # Return true if the reciever has been tagged
+  primitive_nobridge 'tagged?', 'hasTag'
+
+  # Return receiver's label attribute
+  primitive_nobridge 'label', 'label'
+
   def to_s
     inspect
   end
@@ -38,5 +53,16 @@ class OrderedCollection
   primitive '_at', 'at:'
   def [](index)
     _at(index + 1)
+  end
+end
+
+DateAndTime = _resolve_smalltalk_global(:DateAndTime)
+class DateAndTime
+  primitive 'as_posix_seconds', 'asPosixSeconds'
+  def as_time
+    psecs = as_posix_seconds
+    secs = psecs.to_i
+    usecs = psecs - secs
+    Time.at(secs, usecs)
   end
 end
