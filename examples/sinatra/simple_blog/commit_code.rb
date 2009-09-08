647e7abafb8e3174c73b5e74d02cc2dea61c14b6
Peter McLain <peter.mclain@gemstone.com>
2009-09-08 10:18:37 -0700

    Commit all files used by app

--- examples/sinatra/simple_blog/commit_code.rb
+++ examples/sinatra/simple_blog/commit_code.rb
@@ -5,6 +5,8 @@ Maglev.abort_transaction
 if force or not defined? Post
   Maglev.persistent do
     load 'blog.rb'
+    load 'txn_wrapper.rb'
+    load 'maglev/objectlog.rb'
   end
   Maglev.commit_transaction
   puts "== Committed blog.rb"
