50d8ab96a43171367202f50125b053607aa92694
Allen Otis <otisa@abaco.gemstone.com>
2009-09-22 13:05:30 -0700

    enable 609

--- src/test/Trac609.rb
+++ src/test/Trac609.rb
@@ -14,13 +14,16 @@ def bar
   raise I::IException.new
 end
 
+xx = 11
 begin
   bar
 rescue I::Failure => e
   # Ok...
+  xx = 22
 rescue Object => e
   raise "Failed to catch the I::Failure..."
 rescue Exception => e
   raise "Failed to cat the I::Failure and the Object"
 end
-
+unless xx == 22 ; raise 'error' ; end
+true
