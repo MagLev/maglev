17053572d2e3085e816975280650d6a170ef455f
Peter McLain <peter.mclain@gemstone.com>
2009-09-23 16:34:11 -0700

    Added src/test/Trac613.rb

---
+++ src/test/Trac613.rb
@@ -0,0 +1,26 @@
+
+class JSON
+  attr_reader :count
+  def initialize;  @count = 5 end
+
+  def parse
+    until eos?()
+      case
+      when true
+        puts "yup"
+        break
+      else
+        puts "else"
+      end
+    end
+  end
+
+  def eos?
+    @count -= 1
+    @count == 0
+  end
+end
+
+j = JSON.new
+j.parse
+raise "FAIL" if j.count == 0
