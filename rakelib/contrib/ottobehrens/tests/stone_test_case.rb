5107732f688dc1ae522decbb4e2d981cf00065f2
Monty Williams <monty.williams@gemstone.com>
2009-09-23 12:14:25 -0700

    Updated test cases from Otto and Danie

--- rakelib/contrib/ottobehrens/tests/stone_test_case.rb
+++ rakelib/contrib/ottobehrens/tests/stone_test_case.rb
@@ -11,26 +11,23 @@ class StoneUnitTestCase < StoneTestCase
     stone = Stone.create(TEST_STONE_NAME)
     partial_mock_stone = flexmock(stone)
 
-    log_number = "1313"
-
-    partial_mock_stone.should_receive(:topaz_commands).with(/SystemRepository startNewLog/).and_return(["[4202 sz:0 cls: 74241 SmallInteger] #{log_number}"]).once.ordered
-    partial_mock_stone.should_receive(:topaz_commands).with(/System startCheckpointSync/).once.ordered
-    expected_backup_directory = "#{stone.backup_directory}/#{stone.name}_#{Date.today.strftime('%F')}.full.gz"
-    partial_mock_stone.should_receive(:topaz_commands).with(/System abortTransaction. SystemRepository fullBackupCompressedTo: '#{expected_backup_directory}'/).once.ordered
-    partial_mock_stone.should_receive(:log_sh).with("tar --transform='s,.*/,,' -zcf #{stone.backup_filename_for_today} #{stone.extent_backup_filename_for_today} #{stone.data_directory}/tranlog/tranlog#{log_number}.dbf").once.ordered
+    partial_mock_stone.should_receive(:topaz_commands).with(/System startCheckpointSync/).and_return(["[4202 sz:0 cls: 74241 Boolean] true"]).once.ordered
+    partial_mock_stone.should_receive(:topaz_commands).with(/SystemRepository startNewLog/).and_return(["[4202 sz:0 cls: 74241 SmallInteger] 1313"]).once.ordered
+    expected_backup_file_name = "#{stone.backup_directory}/#{stone.name}_#{Date.today.strftime('%F')}.full"
+    partial_mock_stone.should_receive(:topaz_commands).with(/System abortTransaction. SystemRepository fullBackupCompressedTo: '#{expected_backup_file_name}'/).once.ordered
     
     stone.full_backup
   end
 
   def test_restore_latest_backup
     stone = Stone.create(TEST_STONE_NAME)
-    mock_out_restore_commands(stone, stone.backup_filename_for_today, stone.extent_backup_filename_for_today)
+    mock_out_restore_commands(stone, stone.backup_filename_for_today)
     stone.restore_latest_full_backup
   end
 
   def test_restore_backup_of_another_stone_on_date
     stone = Stone.create(TEST_STONE_NAME)
-    mock_out_restore_commands(stone, "#{stone.backup_directory}/anotherstone_2005-05-13.bak.tgz", "#{stone.backup_directory}/anotherstone_2005-05-13.full.gz")
+    mock_out_restore_commands(stone, "#{stone.backup_directory}/anotherstone_2005-05-13.full.gz")
     stone.restore_full_backup('anotherstone', Date.civil(2005, 5, 13))
   end
 
@@ -42,25 +39,31 @@ class StoneUnitTestCase < StoneTestCase
     stone.input_file("test.gs")
   end
 
+  def test_parse_tranlog_number
+    assert(3 == Stone.tranlog_number_from("[18446744073709551610 sz:0 cls: 74241 SmallInteger] 3"))
+    assert(-1 == Stone.tranlog_number_from("[18446744073709551610 sz:0 cls: 74241 SmallInteger] -1 == 0xffffffffffffffff"))
+  end
+
   private
   
-  def mock_out_restore_commands(stone, backup_filename, extent_filename)
+  def mock_out_restore_commands(stone, backup_filename)
     partial_mock_stone = flexmock(stone)
 
     partial_mock_stone.should_receive(:recreate!).once.ordered
-    partial_mock_stone.should_receive(:log_sh).with("tar -C '#{stone.backup_directory}' -zxf '#{backup_filename}'").once.ordered
-    partial_mock_stone.should_receive(:log_sh).with("cp #{stone.backup_directory}/tranlog*.dbf #{stone.data_directory}/tranlog/").once.ordered
-    partial_mock_stone.should_receive(:topaz_commands).with(/System commitTransaction. SystemRepository restoreFromBackup: '#{extent_filename}'/).once.ordered
-    partial_mock_stone.should_receive(:topaz_commands).with(/SystemRepository restoreFromCurrentLogs/).and_return('Restore from transaction log(s) succeeded').once.ordered
+    partial_mock_stone.should_receive(:topaz_commands).with(/System abortTransaction. SystemRepository restoreFromBackup: '#{backup_filename}'.*/).once.ordered
     partial_mock_stone.should_receive(:topaz_commands).with(/SystemRepository commitRestore/).and_return('commitRestore succeeded').once.ordered
   end
 end
 
 class StoneIntegrationTestCase < StoneTestCase
 
+  def delete_existing_backup(stone)
+    File.delete stone.backup_filename_for_today if File.exist? stone.backup_filename_for_today
+  end
+
   def test_full_backup
     stone = Stone.create(TEST_STONE_NAME)
-    remove_previous_backup_files(stone)
+    delete_existing_backup(stone)
     stone.start
     stone.full_backup
     assert File.exist? stone.backup_filename_for_today
@@ -68,12 +71,12 @@ class StoneIntegrationTestCase < StoneTestCase
 
   def test_restore
     stone = Stone.create(TEST_STONE_NAME)
+    delete_existing_backup(stone)
     stone.start
-    random_method_name = add_restore_worked_method(stone)
-    remove_previous_backup_files(stone)
+    random_method_name = add_restore_worked_in_userglobals(stone)
     stone.full_backup
     stone.restore_latest_full_backup
-    assert_nothing_raised { stone.run_topaz_command("String #{random_method_name}") }
+    assert_nothing_raised { stone.run_topaz_command("UserGlobals at: ##{random_method_name}") }
   end
 
   def test_netldi
@@ -180,23 +183,14 @@ class StoneIntegrationTestCase < StoneTestCase
 
   private
   
-  def remove_previous_backup_files(stone)
-    rm stone.extent_backup_filename_for_today if File.exist? stone.extent_backup_filename_for_today
-    rm stone.backup_filename_for_today if File.exist? stone.backup_filename_for_today
+  def add_restore_worked_in_userglobals(stone)
+    random_key = "restoreWorked#{rand 1000}"
+    stone.topaz_commands([
+                          ["run",
+                           "UserGlobals at: ##{random_key} put: 'yay'",
+                           '%', 'commit'].join("\n"),
+                          ])
+    return random_key
   end
 
-  def add_restore_worked_method(stone)
-    # Not assuming a GLASS installation
-    stone.run_topaz_command('GsPackageLibrary installPackage: (GsPackageLibrary createPackageNamed: #SessionMethods). System commitTransaction.')
-
-    random_method_name = "restoreWorked#{rand 1000}"
-    stone.topaz_commands(['set class String', 
-                          'set category for-integration-testing',
-                          ['classmethod:',
-                           random_method_name,
-                           " ^ 'yay restore worked'",
-                           '%'].join("\n"),
-                          'commit'])
-    return random_method_name
-  end
 end
