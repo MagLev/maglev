0563b59267ed0170dc8d56280b4ec52bcd44b26c
Allen Otis <otisa@abaco.gemstone.com>
2009-09-29 11:24:49 -0700

    comments

--- src/test/Trac612.rb
+++ src/test/Trac612.rb
@@ -9,7 +9,10 @@ def Closed.f
 end
 doors = [Closed] * (n+1)
 for mul in 1..n
+#   puts "mul = #{mul.inspect}"
     for x in 1..n
+#       puts "x = #{x.inspect}"
+# nil.pause
         doors[mul*x] = (doors[mul*x] || break).f
     end
 end
