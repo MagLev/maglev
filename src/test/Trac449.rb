bf85237198fb77b3f2018f35422d8c201bfd394f
Allen Otis <otisa@abaco.gemstone.com>
2009-10-06 11:13:12 -0700

    add more checks

--- src/test/Trac449.rb
+++ src/test/Trac449.rb
@@ -10,8 +10,8 @@ puts "---"
 # Correct: Does return the singleton class
 oc2 = class << Object; self; end
 p oc2
-unless oc2.name == '' ; raise 'error'; end
-unless oc2.inspect == '#<Class:Object>' ; raise 'error'; end
+unless (xx = oc2.name) == '' ; raise 'error'; end
+unless (xx = oc2.inspect) == '#<Class:Object>' ; raise 'error'; end
 unless oc2.is_a?(Object) ; raise 'error'; end
 unless oc2.is_a?(Class) ; raise 'error'; end
 unless oc2.is_a?(Module) ; raise 'error'; end
