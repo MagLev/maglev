9daed1f2b4231de3e0f3a0465b36c1a1601de494
Allen Otis <otisa@abaco.gemstone.com>
2009-10-06 11:14:06 -0700

    add check for class name

--- src/test/SmalltalkFFITest.rb
+++ src/test/SmalltalkFFITest.rb
@@ -2,7 +2,7 @@ dirname = "#{ENV['MAGLEV_HOME']}/lib/ruby/site_ruby/1.8/smalltalk"
 unless File.directory?(dirname)
   Dir.chdir "#{ENV['MAGLEV_HOME']}" do
     puts "generating stwrappers"
-    system 'rake dev:stwrappers'
+    system 'rake stwrappers'
   end
 end
 
@@ -13,6 +13,11 @@ require 'smalltalk/SymbolSet'
 s = Smalltalk::SymbolSet._st_new(10)
 
 require 'smalltalk/IdentityBag'
-b = Smalltalk::IdentityBag._st_new(10)
+ibcls = Smalltalk::IdentityBag
+b = ibcls._st_new(10)
 puts b._st_hash
 puts b._st_getIndexInfo
+
+n = ibcls.name
+unless n == 'Smalltalk::IdentityBag' ; raise 'error' ; end
+true
