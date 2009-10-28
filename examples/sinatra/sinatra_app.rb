c9fc23f549c2593491a8ca1293e9b605d419c35a
Peter McLain <peter.mclain@gemstone.com>
2009-10-28 13:57:36 -0700

    Added a sample of how to test a sinatra app running under maglev

--- examples/sinatra/sinatra_app.rb
+++ examples/sinatra/sinatra_app.rb
@@ -3,8 +3,10 @@ require 'sinatra'
 
 # Workaround for issue automatically starting app.
 # See comments in README file
-set :run,     true
-set :server,  'webrick'
+if defined? Maglev and not defined? DO_NOT_RUN
+  set :run,     true
+  set :server,  'webrick'
+end
 
 get '/' do
   "Sinatra #{Sinatra::VERSION} says Hello"
---
+++ examples/sinatra/sinatra_app_test.rb
@@ -0,0 +1,33 @@
+# Hack to workaround Sinatra start up problem.
+#
+# To workaround a startup problem, the sinatra_app.rb file does:
+#
+#   set :run, true
+#
+# As a consequence, after the unit tests run, then webrick will fire up.
+# So, in sinatra_app.rb, we test for DO_NOT_RUN, and if it is defined, then
+# we don't set the :run to true....
+DO_NOT_RUN = true  
+
+require 'rubygems'
+require 'sinatra_app'
+require 'minitest/unit'
+require 'rack/test'
+
+set :environment, :test
+
+class HelloWorldTest < MiniTest::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_it_says_hello_world
+    get '/'
+    assert last_response.ok?, "response not ok"
+    assert last_response.body == 'Sinatra 0.9.4 says Hello', "response content wrong: #{last_response.body}"
+  end
+end
+
+MiniTest::Unit.new.run(ARGV)
