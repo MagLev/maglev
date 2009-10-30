9477c9dadcb1fba375b29614c0908b00374e8843
Allen Otis <otisa@abaco.gemstone.com>
2009-10-30 09:51:29 -0700

    fixes to pass more library specs

--- src/kernel/delta/Range.rb
+++ src/kernel/delta/Range.rb
@@ -1,31 +1,6 @@
 class Range
   include Enumerable
 
-  # Overrides of Enumerable for performance reasons
-  def collect(&block)
-    res = []
-    current = @from
-    lim = @to
-    if @from._isNumeric
-      unless @excludeEnd
-        lim = lim + 1
-      end
-      begin
-        res << block.call(current)
-        current += 1
-      end while current < lim
-    else
-      unless @excludeEnd
-        lim = lim.succ
-      end
-      begin
-        res << block.call(current)
-        current = current.succ 
-      end while current < lim
-    end
-    res
-  end
-
   def member?(val)
     # implementation to conform to rubyspecs, Pickaxe docs incorrect
     fr = @from
