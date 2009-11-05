3fd84454859761cfef9b2a8fa74caf60d462b19b
Allen Otis <otisa@abaco.gemstone.com>
2009-11-05 12:26:40 -0800

    add more result checking

--- src/test/Trac630.rb
+++ src/test/Trac630.rb
@@ -1,9 +1,11 @@
 # From minitest
 module Kernel
-  def describe desc, &block
+  def describe(desc, &block)
     stack = MiniTest::Spec.describe_stack
     name = desc.to_s.capitalize
     cls = Object.class_eval "class #{name} < #{stack.last}; end; #{name}"
+    block.call
+    cls
   end
 end
 
@@ -17,6 +19,10 @@ class MiniTest::Spec
   end
 end
 
-describe :foo do
-  puts "whatever"
+$b = 0
+cx = describe :foo do
+  $b = 5
 end
+unless $b == 5 ; raise 'error'; end
+unless cx.name == 'Foo' ; raise 'error'; end
+true
