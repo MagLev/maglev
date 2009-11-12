f38d30e6fe1e4b678b6916a9dac2bf40f095b9cf
Allen Otis <otisa@abaco.gemstone.com>
2009-11-12 12:05:59 -0800

    fixes for Solaris

--- src/test/SystemTest.rb
+++ src/test/SystemTest.rb
@@ -2,10 +2,20 @@ require File.expand_path('simple', File.dirname(__FILE__))
 
 #     BEGIN TEST CASES
 
-test(Maglev::System.getgid,  `id -r -g`.chomp.to_i, "getgid")
-test(Maglev::System.getegid, `id -g`.chomp.to_i,    "getegid")
+plat = RUBY_PLATFORM
 
-test(Maglev::System.getuid,  `id -r -u`.chomp.to_i, "getuid")
-test(Maglev::System.geteuid, `id -u`.chomp.to_i,    "geteuid")
+if plat.index('solaris')
+  # use xpg4 compatibility version
+  test(Maglev::System.getgid,  `/usr/xpg4/bin/id -r -g`.chomp.to_i, "getgid")
+  test(Maglev::System.getegid, `/usr/xpg4/bin/id -g`.chomp.to_i,    "getegid")
 
+  test(Maglev::System.getuid,  `/usr/xpg4/bin/id -r -u`.chomp.to_i, "getuid")
+  test(Maglev::System.geteuid, `/usr/xpg4/bin/id -u`.chomp.to_i,    "geteuid")
+else
+  test(Maglev::System.getgid,  `id -r -g`.chomp.to_i, "getgid")
+  test(Maglev::System.getegid, `id -g`.chomp.to_i,    "getegid")
+
+  test(Maglev::System.getuid,  `id -r -u`.chomp.to_i, "getuid")
+  test(Maglev::System.geteuid, `id -u`.chomp.to_i,    "geteuid")
+end
 report
