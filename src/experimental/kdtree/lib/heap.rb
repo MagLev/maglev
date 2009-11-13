ed53684dffccbd31386c0483688f442dc21e43cb
Peter McLain <peter.mclain@gemstone.com>
2009-11-12 16:29:50 -0800

    Added a heap implementation

---
+++ src/experimental/kdtree/lib/heap.rb
@@ -0,0 +1,116 @@
+# A simple binary heap, that is limited to N elements
+puts "--------- HEAP"
+module Heap
+  # A simple heap based on arrays.
+  class Heap
+    def self.left_idx(i);  2*i + 1 end
+    def self.right_idx(i)  2*i + 2 end
+    def self.parent_idx(i) (i-1)/2 end
+
+    def initialize(size=nil, &block)
+      @cmp = block_given? ? block : proc {|a,b| a < b}
+      @data = size.nil? ? Array.new : Array.new(size)
+      @bottom = 0 # index of bottom of heap
+    end
+
+    def size
+      @bottom
+    end
+
+    # Add an element to the heap.  Returns self.
+    def add(el)
+      @data[@bottom] = el
+      bubble_up(@bottom)
+      @bottom += 1
+      self
+    end
+
+    # returns, but does not remove, the top of the heap.
+    def top
+      raise "Empty heap" if @bottom == 0
+      @data[0]
+    end
+
+    # removes and returns the top of the heap
+    def delete_top
+      raise "Empty heap" if @bottom == 0
+      ret_value = @data[0]
+      @bottom -= 1
+
+      if @bottom > 0
+        @data[0] = @data[@bottom]
+        percolate_down(0)
+      end
+
+      ret_value
+    end
+
+    def bubble_up(i)
+      return if i <= 0
+
+      pi = Heap.parent_idx i
+      if @cmp.call(@data[i], @data[pi])
+        @data[i], @data[pi] = @data[pi], @data[i]
+        bubble_up(pi)
+      end
+    end
+
+    def percolate_down(i)
+      return if i >= @bottom
+
+      li = Heap.left_idx  i
+      return if li >= @bottom # we fill left to right, so => right is empty too
+
+      ri = Heap.right_idx i
+      swap_child = if ri >= @bottom
+                     li
+                   else
+                     @cmp.call(@data[li], @data[ri]) ? li : ri
+                   end
+      if @cmp.call(@data[swap_child], @data[i])
+        @data[swap_child], @data[i] = @data[i], @data[swap_child]
+        percolate_down(swap_child)
+      end
+    end
+  end
+
+
+  class NHeap < Heap
+    def initialize(size, &block)
+      super
+      @limit = size
+    end
+
+    # Adds el to the heap.  If the heap already contains N elements, then
+    # it removes the top element before inserting.  Returns nil or the
+    # removed element.
+    def add(el)
+      ret_value = nil
+      if @bottom >= @limit
+        # Only bother to insert if it is not worse than
+        # the top element.
+        if @cmp.call(@data[0], el)
+          ret_value = @data[0]
+          @data[0] = el
+          percolate_down(0)
+        end
+      else
+        super(el)
+      end
+      ret_value
+    end
+  end
+
+#   # TODO: NHeap
+
+#   class Node
+#     attr_accessor :left, :right
+#     attr_reader   :value
+
+#     def initialize(value, left=nil, right=nil)
+#       @value = value
+#       @left = left
+#       @right = right
+#     end
+#   end
+end
---
+++ src/experimental/kdtree/test/heap_tests.rb
@@ -0,0 +1,69 @@
+require 'rubygems'
+require 'minitest/spec'
+require 'heap'
+
+MiniTest::Unit.autorun
+
+describe Heap do
+  it 'calculates parent, left and right indicies correctly' do
+    # Array of pairs: [node, left, right]
+    expected = [[0, 1, 2], [1, 3, 4], [2, 5, 6]]
+    expected.each do |(parent, left, right)|
+      Heap::Heap.parent_idx(left).must_equal parent
+      Heap::Heap.parent_idx(right).must_equal parent
+      Heap::Heap.left_idx(parent).must_equal left
+      Heap::Heap.right_idx(parent).must_equal right
+    end
+    Heap::Heap.parent_idx(0).must_equal -1
+  end
+
+  it 'returns elements in min order by default' do
+    h = Heap::Heap.new
+    elements = [0,1,2,3,4,5,6,7,8,9].sort_by {|i| rand(100) }
+    elements.each {|el| h.add(el) }
+    10.times { |i| h.delete_top.must_equal i }
+  end
+
+  it 'returns elements in max order if so configured' do
+    h = Heap::Heap.new {|a,b| a > b}
+    elements = [0,1,2,3,4,5,6,7,8,9].sort_by {|i| rand(100) }
+    elements.each {|el| h.add(el) }
+    10.times { |i| h.delete_top.must_equal(9 - i) }
+  end
+end
+
+describe Heap::NHeap do
+  it 'does not grow beyond N elements' do
+    n = 20
+    h = Heap::NHeap.new(n)
+    h.size.must_equal 0
+
+    input_size = n * 20
+    input_elements = Array.new(input_size) {|i| i}.sort_by{ |i| rand }
+    input_elements.each do |el|
+      h.add(el)
+      (n >= h.size).must_equal true
+    end
+
+    h.size.must_equal n
+
+    n.times { |i| h.delete_top.must_equal(input_size - n + i) }
+  end
+
+  it 'maintains smallest N if cmp is >' do
+    n = 20
+    h = Heap::NHeap.new(n) {|a,b| a > b}
+    h.size.must_equal 0
+
+    input_size = n * 20
+    input_elements = Array.new(input_size) {|i| i}.sort_by{ |i| rand }
+    input_elements.each do |el|
+      h.add(el)
+      (n >= h.size).must_equal true
+    end
+
+    h.size.must_equal n
+
+    n.times { |i| h.delete_top.must_equal(n-1-i) }
+  end
+end
