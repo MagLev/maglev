7c5c63890c8842eed8968d0849c7c3bdba9fbdc6
Monty Williams <monty.williams@gemstone.com>
2010-01-04 22:43:12 -0800

    Added version.txt with 22683 as correct version

--- src/kernel/bootstrap/Globals.rb
+++ src/kernel/bootstrap/Globals.rb
@@ -142,10 +142,10 @@ RUBY_ENGINE = 'maglev'
 RUBY_PLATFORM = 'Unspecified.platform'
 RUBY_VERSION = '1.8.6'
 # Note - the packager modifies the date on any line starting with RUBY_RELEASE_DATE
-RUBY_RELEASE_DATE = '2009-12-10'
+RUBY_RELEASE_DATE = '2010-01-04'
 RUBY_PATCHLEVEL = 114  # TODO: this is what MRI 1.8.6 shows...
 
-VERSION = '22635-1076'
+VERSION = '22683-1088'
 # If DEBUG_SPEC is true an executing rubyspec pauses on exception for topaz debugging
 # If false, the handling is unchanged: exception is reported along with the
 #  stringified stack.
