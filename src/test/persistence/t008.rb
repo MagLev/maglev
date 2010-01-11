d805afa14da67d348d5b0cc82e1f93751c2b740b
Peter McLain <peter.mclain@gemstone.com>
2010-01-11 11:19:25 -0800

    Turned on a persistence test that now passes

--- src/test/persistence/t008.rb
+++ src/test/persistence/t008.rb
@@ -1,16 +1,16 @@
 # Define a class
-class Foo
+class Foo456
    # stuff
 end
 
 # Mark the class as persistable
-Foo.maglev_persistable = true
+Foo456.maglev_persistable
 
 # Create a space in PERSISTENT_ROOT to hold persistent Foo objects:
 Maglev::PERSISTENT_ROOT[:my_favorite_foos] = Array.new
 
 # Connect a Foo instance to a persistent root
-Maglev::PERSISTENT_ROOT[:my_favorite_foos] << Foo.new
+Maglev::PERSISTENT_ROOT[:my_favorite_foos] << Foo456.new
 
 Maglev.commit_transaction  # commit class Foo and the instance f.
 
