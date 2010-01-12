d8f1938931b3915ee7abb6b4cf64f225e4034582
Peter McLain <peter.mclain@gemstone.com>
2010-01-12 11:34:35 -0800

    libyaml: WIP

--- src/experimental/yaml/c/main.c
+++ src/experimental/yaml/c/main.c
@@ -3,7 +3,19 @@
 
 int main(int argc, char **argv) {
 
-  const unsigned char *input = "--- a";
+
+  /*
+    #   %YAML 1.1
+    #   %TAG ! tag:gemstone.com,2009:
+    #   --- !squee
+    
+    const unsigned char *input = "--- a";
+
+    const unsigned char *input = "%YAML 1.1\n%TAG ! tag:gemstone.com,2009:\n--- !squee\n";
+
+  */
+  const unsigned char *input =
+    "%YAML 1.1\n%TAG ! tag:gemstone.com,2009:\n--- !squee\n";
   fprintf(stderr, "-- main: A: about to create parser\n");
   parser_context_t *parser_context = create_parser_context(input);
 
