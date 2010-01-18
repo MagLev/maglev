e579e16c4399b8a3b6a491dd87c8d9c3502223f2
Allen Otis <otisa@abaco.gemstone.com>
2010-01-18 14:32:41 -0800

    enable test 652

--- src/test/Trac652.rb
+++ src/test/Trac652.rb
@@ -10,3 +10,19 @@ module ActiveSupport
     end
   end
 end
+
+class Cx
+  @@under_path = 95
+  def self.under(path)
+    @@under_path, old_path = path, @@under_path
+    unless old_path == 95; raise 'error'; end 
+    @@under_path
+  end
+  def self.__under
+    @@under_path
+  end
+end
+rx = Cx.under(88)
+unless rx == 88 ; raise 'error'; end
+unless Cx.__under == 88 ; raise 'error'; end
+true
