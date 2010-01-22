679e287059ce8620681ed2c22487a4ac5636a76f
Peter McLain <peter.mclain@gemstone.com>
2010-01-22 14:02:02 -0800

    libyaml: Remove cruft files

--- src/experimental/testing/active_support_test.rb
+++
@@ -1,9 +0,0 @@
-# This file includes the Rails 3 version of active support
-#
-# git://github.com/rails/rails.git
-
-
-$:.unshift "/Users/pmclain/external/rails/activesupport/lib"
-
-require 'active_support'
-
