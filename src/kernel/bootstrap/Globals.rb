49fa5f68ed3a465bc7b9d9bf41bbeee96535eae7
Monty Williams <monty.williams@gemstone.com>
2010-02-24 15:50:52 -0800

    Updated MagLev version to 22907

--- src/kernel/bootstrap/Globals.rb
+++ src/kernel/bootstrap/Globals.rb
@@ -142,10 +142,10 @@ RUBY_ENGINE = 'maglev'
 RUBY_PLATFORM = 'Unspecified.platform'
 RUBY_VERSION = '1.8.6'
 # Note - the packager modifies the date on any line starting with RUBY_RELEASE_DATE
-RUBY_RELEASE_DATE = '2010-02-18'
+RUBY_RELEASE_DATE = '2010-02-24'
 RUBY_PATCHLEVEL = 114  # TODO: this is what MRI 1.8.6 shows...
 
-VERSION = '22891-1122'
+VERSION = '22907-1126'
 # If DEBUG_SPEC is true an executing rubyspec pauses on exception for topaz debugging
 # If false, the handling is unchanged: exception is reported along with the
 #  stringified stack.
