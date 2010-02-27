0ee436e8a0c0eb5026c524d6e27c63c9bd1616d4
Monty Williams <monty.williams@gemstone.com>
2010-02-26 21:11:25 -0800

    Update to version 22923

--- src/kernel/bootstrap/Globals.rb
+++ src/kernel/bootstrap/Globals.rb
@@ -145,7 +145,7 @@ RUBY_VERSION = '1.8.7'
 RUBY_RELEASE_DATE = '2010-02-26'
 RUBY_PATCHLEVEL = 249  # TODO: this is what MRI 1.8.7 shows...
 
-VERSION = '22907-1126'
+VERSION = '22923-1126'
 # If DEBUG_SPEC is true an executing rubyspec pauses on exception for topaz debugging
 # If false, the handling is unchanged: exception is reported along with the
 #  stringified stack.
