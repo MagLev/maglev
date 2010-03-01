afe4814aa7859dc7cee5fb39ab414f1b3a9dfe0b
Allen Otis <otisa@abaco.gemstone.com>
2010-03-01 11:01:11 -0800

    added for 1.8.7

---
+++ src/kernel/common/Enumerable2.rb
@@ -0,0 +1,16 @@
+module Enumerable
+  def sorter
+    Sort.new
+  end
+
+  class Sort
+    def sort_by(xs)
+      # The ary and its elements sould be inmutable while sorting
+
+      elements = xs.map { |x| SortedElement.new(x, yield(x)) }
+      sort(elements).map { |e| e.value }
+    end
+  end
+
+end
+
