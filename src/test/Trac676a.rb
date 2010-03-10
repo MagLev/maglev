3931f75285492941bc7c8c66a3ea1d48deca5eef
Allen Otis <otisa@abaco.gemstone.com>
2010-03-10 10:01:35 -0800

    file changes for 1.8.7

---
+++ src/test/Trac676a.rb
@@ -0,0 +1,32 @@
+# Trac 676
+
+
+class A
+  def ma
+    $A << 100
+  end
+end
+
+class B < A
+  def ma
+    $A << 200
+    super()
+  end
+end
+
+class C < B
+  alias_method :get, :ma
+end
+
+$A = []
+B.new.ma
+bx = $A
+$A = []
+C.new.get
+cx = $A
+puts bx.inspect
+puts cx.inspect
+unless bx == [200, 100] ; raise 'error'; end
+unless cx == [200, 100] ; raise 'error'; end
+
+true
