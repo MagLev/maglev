81a4fefae5c9b2b5081d467b1645d19d36174629
Peter McLain <peter.mclain@gemstone.com>
2010-03-17 15:40:26 -0700

    Removed old experimental yaml dir

--- src/experimental/yaml/lib/psych/psych.rb
+++
@@ -1,5 +0,0 @@
-# The real psych.rb requires psych/psych, which resolves here.
-# This is the hook that allows us to require all of our ffi patches.
-require 'psych/ffi/psych'
-require 'psych/ffi/parser'
-require 'psych/ffi/emitter'
