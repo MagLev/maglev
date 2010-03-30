f66c68c4b9590252687215db3cb4dd7383407a56
Peter McLain <peter.mclain@gemstone.com>
2010-03-30 11:00:33 -0700

    Added another test case

--- src/test/Trac686.rb
+++ src/test/Trac686.rb
@@ -9,10 +9,11 @@ class C
     p args     # MRI:
     raise "Wrong num args #{args.size} expecting 3" unless args.size == 3
   end
- 
+
   def self.test
     pat = []
-    self.new.zero(:one, :long, "pre", true, *pat) 
+    self.new.zero(:one, :long, "pre", true, *pat)
+    self.new.zero(:one, :long, "pre", true, *pat) { 10 }
     true
   end
 end
