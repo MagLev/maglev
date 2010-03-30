ecc65331e612127e729c0ca9a611255d2c66d375
Allen Otis <otisa@abaco.gemstone.com>
2010-03-30 14:49:47 -0700

    enable 690

--- src/test/Trac690.rb
+++ src/test/Trac690.rb
@@ -1,8 +1,14 @@
 class C
   def eval(an_arg)
+    $_ = '998'
     Kernel.eval(an_arg)
+  end
+  def self.test(arg)
+    self.new.eval(arg)
   end
 end
 
-C.new.eval "puts 10"
+x = C.test("987 + 10")
+unless x == 997 ; raise 'error'; end
+true
 
