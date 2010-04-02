422b88ccc39bad616a19b76b57a0d572c24d78a0
Allen Otis <otisa@abaco.gemstone.com>
2010-04-02 08:44:15 -0700

    test define_finalizer

---
+++ src/test/testFinalizer.rb
@@ -0,0 +1,28 @@
+
+class C
+  def self.test
+    $w = 0 ; $x = 0
+    a = [ 'firstStr' , 'secondStr', 'thirdStr' , 'fourthStr' ]
+    p = Proc.new { |o| puts "GCa of #{o}" ; $w += 100; }
+    ObjectSpace.define_finalizer(a[0], p );
+    ObjectSpace.define_finalizer(a[1], p );
+    ObjectSpace.define_finalizer(a[2]) { |o| puts "GCb of #{o}" ; $x += 200; }
+    ObjectSpace.define_finalizer(a[3]) { |o| puts "GCb of #{o}" ; $x += 200; } # should not execute
+    ObjectSpace.define_finalizer(a[0]) { |o| puts "GCc of #{o}" ;  $w += 1000; }
+    ObjectSpace.undefine_finalizer(a[1])
+    a[0] = nil
+    a[1] = nil
+    a[2] = nil
+    s = 'abc'
+    (1.. 3000000).each { |n|  
+      s << '00000000000000000000000000000dddddddddddddddddddddddddddddddddddddd' 
+      if s.size > 100000
+        s = 'abc'
+      end
+    }
+    unless (ww = $w) == 1100 ; raise 'error'; end
+    unless (xx = $x) == 200 ; raise 'error'; end
+  end
+end
+C.test
+true
