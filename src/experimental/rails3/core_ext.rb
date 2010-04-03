28ad87e6c58f47888d4462f917e8fa59f6aec3c1
Peter McLain <peter.mclain@gemstone.com>
2010-04-02 18:02:48 -0700

    Work-around bundler issue with to_yaml_properites.

---
+++ src/experimental/rails3/core_ext.rb
@@ -0,0 +1,11 @@
+# Patches to Core classes
+
+# Bundler assumes to_yaml_properties is defined on Object.
+# Psych treats it as an override only.  Patch a default impl
+# that works with psych
+class Object
+  def to_yaml_properties
+    instance_variables
+  end
+end
+
