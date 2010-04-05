101b8e99ea4bb23fb44dd6dd3d8a3f2f6590c36c
Peter McLain <peter.mclain@gemstone.com>
2010-04-05 13:47:13 -0700

    Updated default rails app to rails3 beta2 version

--- src/experimental/rails3/my_app/config/initializers/cookie_verification_secret.rb
+++ src/experimental/rails3/my_app/config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '8640624811f4a864cc0a3d27efc7c65a81a06ac50e80d96ab462a1a36caf1a92f9f1df536a0a3f92d3637be0b01cbc7b6b44e410a3735dbb3ced77a6dfa26385'
+Rails.application.config.cookie_secret = '24f08c848babbebd2bceae4c18e605ead2e8781acd2aa30d92dcde58019e8c21cec1bf4aa272a43dcec5f768bd3f5475bd14e7c9eb6cc045651459a75789471b'
