f8f20c2a388236b6ad61817444fd10fd2aa30038
Allen Otis <otisa@abaco.gemstone.com>
2010-04-09 08:51:39 -0700

    enable 705

--- src/test/Trac705.rb
+++ src/test/Trac705.rb
@@ -5,32 +5,26 @@
 # It seems the regexp within blocks is necessary to reproduce the error.
 class Engine
   class << self
-    attr_accessor :called_from
-    # def called_from=(value)
-    #   @called_from = value
-    # end
-    # def called_from
-    #   @called_from
-    # end
-
-    def abstract_railtie?
-      false
+    def called_from
+      @called_from
+    end
+    def called_from=(v)
+      @called_from = v
     end
 
     def inherited(base)
-      puts "=== #{self}.inherited(#{base})"
+#     puts "=== #{self}.inherited(#{base})"
       base.called_from = begin
-        # Remove the line number from backtraces making sure we don't leave anything behind
-        call_stack = caller.map { |p| p.split(':')[0..-2].join(':') }
-        File.dirname(call_stack.detect { |p|
-           p !~ %r[railties[\w\-\.]*/lib/rails|rack[\w\-\.]*/lib/rack]
-        })
+         call_stack = caller.map { |p| p.split(':')[0..-2].join(':') }
+         File.dirname(call_stack.detect { |p|
+            p !~ %r[railties[\w\-\.]*/lib/rails|rack[\w\-\.]*/lib/rack]
+         })
       end
     end
 
     def find_root_with_flag
       root_path = self.called_from
-      p root_path  # should be a string
+      puts "root_path #{root_path}"  # should be a string
       raise "Fail" unless root_path.kind_of? String
     end
   end
@@ -43,3 +37,4 @@ class MyApp < Application
 end
 
 Application.find_root_with_flag
+true
