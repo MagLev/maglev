203af278bdc48b211729d348853f2fd1665ddc15
Peter McLain <peter.mclain@gemstone.com>
2010-04-12 10:30:46 -0700

    Clean up examples.

--- examples/sinatra/simple_blog/lib/blog.rb
+++ examples/sinatra/simple_blog/lib/blog.rb
@@ -1,63 +1,7 @@
 # This file defines the blog classes.  To commit the code, load this
 # file from within a Maglev.persistent block and then commit it.
 
-# Maglev::Model implements a very simple, and un-production-worthy,
-# persistence model.  A class that includes the Maglev::Model module, will
-# automatically persist all new instances in a hash table for the class.
-# If the module is re-included (or the file that defines the class is
-# re-run), then all previous saved instances will be lost.
-module Maglev::Model
-
-  # These methods will be defined as class methods of the class that
-  # includes this module.
-  module ClassMethods
-
-    # Creates a new instance and stages the new instance.  Equivalent to:
-    #   instance = new(*args)
-    #   stage instance
-    def persistent_new(*args)
-      instance = new(*args)
-      stage instance
-      instance
-    end
-
-    # Returns an array of all the posts
-    def all
-      Maglev::PERSISTENT_ROOT[self].values
-    end
-
-    # Get an object by object id.  If +id+ is not the id for an instance of
-    # the calling class, then no object will be found (even if +id+ is the
-    # valid id of soome instance of another class).
-    def get(id)
-      Maglev::PERSISTENT_ROOT[self][id.to_i]
-    end
-
-    # Stages +obj+ in the collection of persisted objects for this class.
-    # This method does not commit, so the object will not be committed
-    # until client code calls <tt>Maglev#commit_transaction</tt>.
-    def stage(obj)
-      Maglev::PERSISTENT_ROOT[self][obj.__id__] = obj
-    end
-
-    # Iterate over all saved items
-    def each
-      all.each do |el|
-        yield el
-      end
-    end
-  end
-
-  def self.included(host)
-    # TODO: Almost certainly not a good thing beyond the dev env: This will
-    # blow away any persistent data from previous runs.  Perhaps we need a
-    # migration idiom so that this can be controlled.
-    Maglev::PERSISTENT_ROOT[host] = Hash.new
-    host.extend ClassMethods
-    host.extend Enumerable
-  end
-
-end
+require 'maglev_model'
 
 class SimplePost
   include Maglev::Model
