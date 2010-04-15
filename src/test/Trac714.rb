665c7b32a50d449c221904e408efc71fa833ba55
Peter McLain <peter.mclain@gemstone.com>
2010-04-15 15:16:20 -0700

    Added more test cases to Trac714.

--- src/test/Trac714.rb
+++ src/test/Trac714.rb
@@ -32,3 +32,40 @@ end
 x = C.new.m
 unless x == 714 ; raise 'error'; end
 true
+
+
+require File.expand_path('simple', File.dirname(__FILE__))
+
+class C
+  def m1
+  end
+
+  protected
+  def m2
+  end
+
+  private
+  def m3
+  end
+end
+
+c = C.new
+
+test(c.respond_to?(:m1),        true, "public method")
+test(c.respond_to?(:m1, true),  true, "public method true")
+test(c.respond_to?(:m1, false), true, "public method false")
+
+test(c.respond_to?(:m2),        true, "protected method")
+test(c.respond_to?(:m2, true),  true, "protected method true")
+test(c.respond_to?(:m2, false), true, "protected method false")
+
+test(c.respond_to?(:m3),        false, "private method")
+test(c.respond_to?(:m3, true),  true,  "private method true")
+test(c.respond_to?(:m3, false), false, "private method false")
+
+test(c.respond_to?(:m4),        false, "undefined method")
+test(c.respond_to?(:m4, true),  false, "undefined method true")
+test(c.respond_to?(:m4, false), false, "undefined method false")
+
+report
+true
