4a9a92c77a090e431d9583a79369b0fded391991
Monty Williams <monty.williams@gemstone.com>
2010-04-23 12:25:16 -0700

    Enabled creating a dataset of 1 million people in addition to the default 100,000 people in the indexing example

--- examples/persistence/indexing/search_random_people.rb
+++ examples/persistence/indexing/search_random_people.rb
@@ -1,29 +1,29 @@
 require 'benchmark'
 
-Benchmark.bm do |x|
-  youngsters = nil
-  x.report("search the youngsters") {
-    youngsters = Person::RANDOM_PEOPLE.search([:@age], :lt, 25)
-  }
-  puts "Found #{youngsters.length} youngsters"
-
+Benchmark.bm(37) do |x|
   selected_youngsters = nil
-  x.report("select the youngsters") {
+  x.report("Find youngsters (select without index)") {
     selected_youngsters = Person::RANDOM_PEOPLE.select {|p| p.age < 25}
   }
-  puts "Selected #{youngsters.length} youngsters"
+  puts "Found #{selected_youngsters.length} youngsters\n\n"
+
+  youngsters = nil
+  x.report("Find youngsters (search using index)  ") {
+    youngsters = Person::RANDOM_PEOPLE.search([:@age], :lt, 25)
+  }
+  puts "Found #{youngsters.length} youngsters\n\n"
 
   s1, s2 = youngsters.size, selected_youngsters.size
   raise "Incompatible results Search: #{s1} Select: #{s2}" unless s1 == s2
 
   hermits = nil
-  x.report("Find the youngsters") {
+  x.report("Find hermits                          ") {
     hermits  = Person::RANDOM_PEOPLE.search([:@marital_status], :eql, :hermit)
   }
-  puts "Found #{hermits.length} hermits"
+  puts "Found #{hermits.length} hermits\n\n"
 
   young_hermits = nil
-  x.report("Intersect  hermits with youngsters") { young_hermits = hermits & youngsters }
+  x.report("Intersect youngsters with hermits     ") { young_hermits = hermits & youngsters }
   puts "Found #{young_hermits.length} young hermits"
 end
 
