fb4fcc22dc980b1a6903044f58819ac74040137c
Allen Otis <otisa@abaco.gemstone.com>
2010-04-27 15:19:17 -0700

    adjust tests

--- src/test/Trac721c.rb
+++ src/test/Trac721c.rb
@@ -8,33 +8,35 @@
 ######################################################################
 class Class
   def cattr_reader(*syms)
-#    options = syms.extract_options!
     syms.each do |sym|
       class_eval(<<-EOS, __FILE__, __LINE__ + 1)
         unless defined? @@#{sym}
+          sx = self
           @@#{sym} = nil
         end
 
         def self.#{sym}
-          @@#{sym}
+          r = @@#{sym}
+          # nil.pause
+          r
         end
       EOS
 
- #     unless options[:instance_reader] == false
-        class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+      class_eval(<<-EOS, __FILE__, __LINE__ + 1)
           def #{sym}
-            @@#{sym}
+            r = @@#{sym}
+            # nil.pause
+            r
           end
-        EOS
+      EOS
       end
-#    end
   end
 
   def cattr_writer(*syms)
- #   options = syms.extract_options!
     syms.each do |sym|
       class_eval(<<-EOS, __FILE__, __LINE__ + 1)
         unless defined? @@#{sym}
+          sx = self
           @@#{sym} = nil
         end
 
@@ -43,13 +45,12 @@ class Class
         end
       EOS
 
-      #unless options[:instance_writer] == false
-        class_eval(<<-EOS, __FILE__, __LINE__ + 1)
+      class_eval(<<-EOS, __FILE__, __LINE__ + 1)
           def #{sym}=(obj)
             @@#{sym} = obj
+            # nil.pause
           end
-        EOS
-      #end
+      EOS
       self.send("#{sym}=", yield) if block_given?
     end
   end
@@ -64,9 +65,12 @@ module ActiveSupport
   module Concern
     def append_features(base)
       super
-      base.class_eval(&@_included_block) if instance_variable_defined?("@_included_block")
-      puts base.foo1
-      raise "Fail X" unless base.foo1 == 'foo 1 initial value'
+      if instance_variable_defined?("@_included_block")
+        # @_included_block.call( base)
+        base.class_eval(&@_included_block) 
+      end
+      fx1 = base.foo1
+      raise "Fail X" unless fx1 == 'foo 1 initial value'
     end
 
     def included(base = nil, &block)
@@ -81,13 +85,14 @@ end
 
 module Pages
   extend ActiveSupport::Concern
-  included do
-    @@foo1 = 'foo 1 initial value'
-    cattr_accessor :foo1
+  included { | mod |
+    @@foo1 = 'foo 1 initial value' 
+    sx = self 
+    cattr_accessor( :foo1 )
 
-    @@foo2 = 'foo 2 initial value'
-    cattr_accessor :foo2
-  end
+    @@foo2 = 'foo 2 initial value' 
+    cattr_accessor( :foo2 )
+  }
 end
 
 class Base
@@ -95,12 +100,12 @@ class Base
 end
 
 puts "---------- begin tests --------------"
-
 # MagLev can use cattr_accessor to set and get the attribute:
 Base.foo1 = 10
 raise unless Base.foo1 == 10    # as expected
 
 # But the initial value is not recognized by MagLev
-p Base.foo2  # Maglev gets nil, rather than @@foo2
-raise 'fail foo2 initial value' unless Base.foo2 == 'foo 2 initial value'
+# p Base.foo2  # Maglev gets nil, rather than @@foo2
+raise 'fail foo2 initial value' unless (bfx = Base.foo2) == 'foo 2 initial value'
+
 
--- src/test/Trac726.rb
+++ src/test/Trac726.rb
@@ -8,21 +8,30 @@ require 'test/unit'
 
 $flag = false
 
+$aa = []
+
 class MyTests < Test::Unit::TestCase
   def test_a
     puts "---- in test_a"
     assert(true)
+    $aa << 10
   end
 
   def test_b
     puts "---- in test_b"
     assert_raise(RuntimeError) { raise 'xxx' }
     assert(true)
+    $aa << 20
   end
 
   def test_c
     puts "---- in test_c"
     assert(true)
     $flag = true
+    $aa << 30
   end
 end
+r = Test::Unit::AutoRunner.new(false) 
+r.run
+unless (ax = $aa) == [ 10, 20, 30 ] ; raise 'error' ; end
+true
