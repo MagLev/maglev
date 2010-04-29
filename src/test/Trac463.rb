46c45caae8a24f644d89b2cc4819d3812fb9c14d
Allen Otis <otisa@abaco.gemstone.com>
2010-04-29 10:08:02 -0700

    more coverage for break

--- src/test/Trac463.rb
+++ src/test/Trac463.rb
@@ -1,4 +1,11 @@
-# trac 463
+# trac 463  coverage for  next  and   break
+
+unless defined?(Maglev)
+  class NilClass
+    def pause
+    end
+  end
+end
 
 class Trac463
    def testOne
@@ -49,12 +56,15 @@ class Trac463
   def testThree
     n = 0
     x = while true
+      puts "n = #{n}" if $debugx
       n += 1
-      if n >= 1000
+      if n >= 10
+        nil.pause if $debugx
         break 95
       end
       begin
-        if n >= 1000
+        if n >= 10
+          nil.pause if $debugx
           break 96
         end
         next
@@ -89,14 +99,163 @@ class Trac463
     puts "testThreeU ok"
   end
 
+  # more uses of break from the ruby specs
+  def testArrayNew
+     a = Array.new(3) do |i|
+       if i == 2
+         nil.pause if $debugx
+         break 98 
+       end
+       i.to_s
+     end
+     unless a == 98; raise 'fail'; end
+  end
 
+  def testClsNew
+     klass = Class.new do
+       def initialize
+	 yield
+       end
+     end
+     x = klass.new { break 42 }
+     unless x == 42 ; raise 'fail'; end
+  end
 end
 
+class BreakTest
+  # case #1: yield
+  def self.fail(msg)
+    raise "error #{msg}"
+  end
+  def self.meth_with_yield(&b)
+    yield
+    fail("break returned from yield to wrong place")
+  end
+  def self.invoking_method(&b)
+    meth_with_yield(&b)
+    fail("break returned from 'meth_with_yield' method to wrong place")
+  end
+
+  # case #2: block.call
+  def self.meth_with_block_call(&b)
+    b.call
+    fail("break returned from b.call to wrong place")
+  end
+  def self.invoking_method2(&b)
+    meth_with_block_call(&b)
+    fail("break returned from 'meth_with_block_call' method to wrong place")
+  end
+
+  def self.test
+    # this calls a method that calls another method that yields to the block
+    ax = self.invoking_method do
+      break 46
+      fail("break didn't, well, break")
+    end
+    unless ax == 46 ; raise "#{ax} not equal 46"; end
+    puts "okA"
+
+    # this calls a method that calls another method that calls the block
+    bx = self.invoking_method2 do
+      break 47
+      fail("break didn't, well, break")
+    end
+    unless bx == 47 ; raise "#{bx} not equal 46"; end
+    puts "okB"
+
+    res = self.invoking_method do
+      break 35
+      fail("break didn't, well, break")
+    end
+    unless res == 35 ; raise "#{res} not equal 35"; end
+    puts "okC"
+
+    res = self.invoking_method2 do
+      break 36
+      fail("break didn't, well, break")
+    end
+    unless res == 36 ; raise "#{res} not equal 36"; end
+    puts "BreakTest.test ok"
+  end
+end
+
+class TUnit
+  def _wrap_assertion(&blk)
+    blk.call
+  end
+  def assert_block(msg, &blk)
+    blk.call(msg)
+  end
+  def assert_raise(*args, &blk)
+    _wrap_assertion do
+      actual_exception = nil
+      full_message = 'abc'
+      rx = assert_block(full_message) do
+	begin
+	  blk.call
+	rescue TypeError => actual_exception
+	  break 776
+	end
+	false
+      end
+      unless rx == 776 ; raise 'fail'; end
+      unless actual_exception.class == TypeError; raise 'fail'; end
+      455
+    end
+  end
+
+  def test
+    x = assert_raise() { raise TypeError }
+    unless x == 455 ; raise 'fail'; end
+    puts "aTUnit.test ok"
+  end
+end
+
+$debugx = false
+
 o = Trac463.new
+o.testArrayNew
+o.testClsNew
+TUnit.new.test
+
 o.testOne
 o.testTwo
 o.testThree
 o.testTwoU
 o.testThreeU
+
+$debugx = true
+
+BreakTest.test
+
+class Brk2
+  def self.break_test()
+    yield 1
+    yield 2
+    yield 3
+  end
+  ax = break_test {|i| break i if i == 2 }
+  unless ax == 2 ; raise "#{ax} not equal 2"; end
+  i = 0 
+  bx = break_test {|i| break i if i == 1 }
+  unless i == 0 ; raise "#{i} not equal 1"; end # 1.9 behavior
+  puts "Brk2 done"
+end
+
+class Brk3
+  def self.mx
+    break
+  end
+  ax = 0
+  begin
+    (lambda { mx }).call
+  rescue LocalJumpError
+    ax = 23
+  end
+  unless ax == 23 ; raise 'failed';end
+  puts "Brk3 done"
+end
+
+
 puts "OK"
 true
