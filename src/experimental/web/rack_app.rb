665bb5ab5f9fcfbe8162ec52279788307b4d5d22
Peter McLain <peter.mclain@gemstone.com>
2010-05-17 17:28:57 -0700

    Fix issue with ipv4 vs ipv6

--- src/experimental/web/rack_app.rb
+++ src/experimental/web/rack_app.rb
@@ -1,12 +1,20 @@
 # A bare-bones app to do Gprof testing of MagLev w/o Sinatra in the way.
 #
 # Usage: maglev-ruby rack_app.rb
+#
+# This uses a Rack SCGI handler to listen for SCGI requests.
+#   Rack::Handler::SCGI takes :Host and :Port and :Socket options
 
 require 'rack'
 
-app = Proc.new do |env|
+$app = Proc.new do |env|
+  $stderr.puts "-- NEW request: #{env['REQUEST_PATH']}"
+
   case env['REQUEST_PATH']
 
+  when '/foo.scgi'
+    $stderr.puts "FOO DOT SCGI"
+    
   when '/start'
     puts "START"
     ns_per_sample = Maglev::Gprof.compute_interval(10)  # figure out for 10 seconds
@@ -38,5 +46,15 @@ app = Proc.new do |env|
   [200, { 'Content-Type' => 'text/html' }, "<h1>hello</h2>" ]
 end
 
-Rack::Handler::SCGI.run(app, :Host=> 'localhost', :Port => '4567')
+Rack::Builder.new do
+  require 'socket'
+  use Rack::ContentLength
+  #Rack::Handler::SCGI.run($app, :Host=> 'localhost', :Port => '4567')
+  #sock = Socket.new(Socket::AF_INET6, Socket::SOCK_STREAM, 0)
+  # addr = Socket.sockaddr_in(4567, 'localhost')
+  sock = TCPServer.new('127.0.0.1', 4567)
+  puts "PASSING SOCKET: #{sock.inspect}"
+  Rack::Handler::SCGI.run($app, :Socket=> sock)
+end
+#Rack::Handler::SCGI.run(app, :Host=> 'localhost', :Port => '4567')
 
