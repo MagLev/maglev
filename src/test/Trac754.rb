36cb1d564017f7eceb8f3323497627a058b527ce
Allen Otis <otisa@abaco.gemstone.com>
2010-06-23 15:23:09 -0700

    Fix Trac 754

--- src/test/Trac754.rb
+++ src/test/Trac754.rb
@@ -4,4 +4,17 @@ class C
   end
   remove_method :try
   alias_method :try, :__send__
+
+  def mysend(method, *args, &block)
+    puts "mysend"
+  end
+
+  $aa = 0
+  begin
+    alias_method :__send__ , :mysend
+  rescue NameError
+    $aa = 8
+  end
+  unless $aa == 8 ; raise 'fail'; end
 end
+true
