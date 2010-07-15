67468f83f5997ebdb59b84f1b57886aabf77f121
Daniel Starling <ds@blinkbeam.com>
2010-07-16 05:52:14 +0800

    multiple changes to merge in old kdtree example functionality

--- examples/persistence/kdtree/public/javascripts/application.js
+++ examples/persistence/kdtree/public/javascripts/application.js
@@ -2,7 +2,21 @@ var map;
 var marker;
 var infoWindow;
 
+// Fits the canvas to the page
+function fitMapCanvas() {
+  $("#map_canvas").height($(window).height() - $("#map_menu").height() - $("#footer").height() - 50);
+}
+
 function initialize() {
+  // Auto-select results count on focus
+  $("#num_results").focus(function() {
+    this.select();
+  });
+  
+  // Resize the map canvas on window resize
+  $(window).resize(fitMapCanvas);
+  fitMapCanvas();
+
 
   // let's set the initial location of the map.
   var latLng = new google.maps.LatLng( 41.6326327769545, -100.1024599609375 );
@@ -16,36 +30,50 @@ function initialize() {
 
   // register a click event handler with our Map instance.
   google.maps.event.addListener( map, 'click', function(event) {
-
-     // remove info window from the map if it exists.  
-     if ( infoWindow != null ) {
-        infoWindow.close(); 
-     }
-     
-     // remove the marker from the map if it exists.
-     if ( marker != null ) {
-       marker.setMap(null);
-     }
-
-     // create a new marker and place it on the map 
-     // of the clicked location.
-     placeMarker( event.latLng );
-     
-     // get the values for the parameter hash.
-     var markerLocation = marker.getPosition();
-     var latitudeS = markerLocation.lat();
-     var longitudeS = markerLocation.lng();
-     
-     // create a parameter hash.
-     var params = { lat: latitudeS, lon: longitudeS, k: 1 };
-        
-     // post an Ajax request to the server.
-     postAjaxRequest( params );
-      
+     searchZips(event.latLng.lat(), event.latLng.lng());           
      return false;
-
    });
 
+}
+
+function searchZips(lat, lng) {
+  // remove info window from the map if it exists.  
+  if ( infoWindow != null ) {
+    infoWindow.close(); 
+  }
+     
+  // remove the marker from the map if it exists.
+  if ( marker != null ) {
+    marker.setMap(null);
+  }
+
+  // create a new marker and place it on the map 
+  // of the clicked location.
+  placeMarker( new google.maps.LatLng( lat, lng ) ); 
+  
+  // show our current location
+  $("#location_display").text("Location: " + lat + ", " + lng);
+  
+  // get the values for the parameter hash.
+  var markerLocation = marker.getPosition();
+  var latitudeS = markerLocation.lat();
+  var longitudeS = markerLocation.lng();
+   
+  // determine how many results the user wanted (modifying the text box if there's invalid input)
+  var k_results = $.trim($("#num_results").val());
+  var isNumber = /^\d+$/.test(k_results);
+  var numValid = k_results > 1;
+  if( !isNumber || !numValid ) {
+    k_results = 5; // Default to this many results if input not valid
+  }
+       
+  $("#num_results").val(k_results);
+     
+  // create a parameter hash.
+  var params = { lat: latitudeS, lon: longitudeS, k: k_results };
+        
+  // post an Ajax request to the server.
+  postAjaxRequest( params );
 }
 
 
@@ -62,13 +90,10 @@ function postAjaxRequest( params ) {
     data: params,
 
     success: function( locations ) {
-     
-      // evaluate the response from the server
-      var location = locations[ 0 ];
-                        
+                            
       // create a new info window, open it, and extend 
       // it from the current marker on the map.
-      attachInfoWindow( marker, 0, location );
+      attachInfoWindow( marker, 0, locations );
 
     },
 
@@ -98,25 +123,47 @@ function placeMarker(location) {
   
 }
 
-function attachInfoWindow( marker, number, location ) {
+function attachInfoWindow( marker, number, locations ) {
 
   var markerLocation = marker.getPosition();
   var latitude = markerLocation.lat();
   var longitude = markerLocation.lng();
-  var zipcode = location['zipcode'];
+  
+  var html  = '<div id="info_window">';
+      html += '  <table class="results"> <thead> <th>ZIP</th> <th>City</th>  <th>State</th>  <th>Distance</th> </thead>';
+      html += '  <tbody>';
+  
+  for(var idx in locations) {  
+    loc = locations[idx];
+    
+    // First row <td>s are 'first', after that it's 'other'; used to highlight first result
+    tdTag = '<td class="' + ( (idx == 0) ? 'first' : 'other' ) + '">'; 
+    
+    // Generate a zip code link they can click
+    if(loc['miles'] > 0.000001)
+      zipCodeHtml = '<a href="#'+loc['zipcode']+'" onclick="searchZips('+loc['latitude']+', '+loc['longitude']+')">' + htmlEncode(loc['zipcode']) + '</a>';     
+    else // Don't link to zip codes we're right on top of 
+      zipCodeHtml = htmlEncode(loc['zipcode']);    
 
-  var contentString  = '<div id="info_window">';
-      contentString += '<ul>';
-      contentString += "<li>Latitude:&nbsp;"          + latitude  + "</li>";
-      contentString += "<li>Longitude:&nbsp;"         + longitude + "</li>";
-      contentString += "<li>Closet US Zipcode:&nbsp;" + zipcode   + "</li>";
-      contentString += '</ul';
-      contentString += '</div>';
+    html += "<tr>";
+    html += tdTag + zipCodeHtml                                   + "</td>";
+    html += tdTag + htmlEncode(loc['city'])                       + "</td>";
+    html += tdTag + htmlEncode(loc['state'])                      + "</td>";
+    html += tdTag + htmlEncode(loc['miles'].toFixed(1) + " mi")   + "</td>";
+    html += "</tr>"
+  }
+  
+  html += "</tbody> </table>";
 
-  infoWindow = new google.maps.InfoWindow( { content: contentString, zIndex: number } );
+  infoWindow = new google.maps.InfoWindow( { content: html, zIndex: number } );
 
   infoWindow.open( map, marker );
 }
+
+function htmlEncode(value){ 
+  // http://stackoverflow.com/questions/1219860/javascript-jquery-html-encoding
+  return $('<div/>').text(value+"").html(); 
+} 
 
 $(document).ready(function() {
   initialize();
