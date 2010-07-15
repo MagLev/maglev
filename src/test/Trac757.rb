402939b29a198496b5e90f2789882aa678701735
Peter McLain <peter.mclain@gemstone.com>
2010-07-15 16:41:42 -0700

    Updated Trac757 files.

--- src/test/Trac757.rb
+++ src/test/Trac757.rb
@@ -2,8 +2,13 @@
 $:.unshift(File.dirname(__FILE__))
 
 begin
-  require 'Trac757a'
-rescue Foo::NOT_FOUND
-  raise "Fail: In rescue clause"
+  require 'Trac757a'  # defines and raises Foo::FooException
+rescue Foo::FooException => e
+  if Foo::FooException === e
+    puts "OK"
+  else
+    raise "Fail: caught wrong exception: #{e}"
+  end
 end
-puts "OK"
+
+true
