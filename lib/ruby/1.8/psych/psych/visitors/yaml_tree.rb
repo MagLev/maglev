d18f3b9d628c3cf92cbd0b9c67cf6d719abed1c4
Allen Otis <otisa@abaco.gemstone.com>
2010-07-28 15:51:40 -0700

    revert previous 2 checkins

--- lib/ruby/1.8/psych/psych/visitors/yaml_tree.rb
+++ lib/ruby/1.8/psych/psych/visitors/yaml_tree.rb
@@ -106,8 +106,7 @@ module Psych
           @emitter.scalar member.to_s, nil, nil, true, false, Nodes::Scalar::ANY
           accept o[member]
         end
-        # dump_ivars o  # Maglev it duplicates the dump of members now that
-                        #  members are implemented with fixed instVars
+        dump_ivars o
 
         @emitter.end_mapping
       end
