5c052ea0b9323e0591e7f699a0c8d47db9241399
Peter McLain <peter.mclain@gemstone.com>
2010-08-11 16:07:33 -0700

    Updated test case.

--- src/test/Trac779.rb
+++ src/test/Trac779.rb
@@ -10,10 +10,10 @@ module Kernel
     end
   end unless respond_to?(:singleton_class) # exists in 1.9.2
 
-  # class_eval on an object acts like singleton_class.class_eval.
-  def class_eval(*args, &block)
-    singleton_class.class_eval(*args, &block)
-  end
+  # # class_eval on an object acts like singleton_class.class_eval.
+  # def class_eval(*args, &block)
+  #   singleton_class.class_eval(*args, &block)
+  # end
 end
 
 # From: lib/active_support/core_ext/class/attribute.rb
@@ -25,6 +25,7 @@ class Class
     attrs.each do |name|
       class_eval <<-RUBY, __FILE__, __LINE__ + 1
         def #{name}
+          singleton_class.logger  # This raises method not found
           raise "Fail A" unless singleton_class.methods.include?("logger")
           22
         end
@@ -33,8 +34,19 @@ class Class
   end
 end
 
+# class LogSubscriber
+#   def self.logger
+#     puts "redefined logger"
+#     10
+#   end
+# end
+
 class LogSubscriber
   class_attribute :logger
+
+  # class << self
+  #   remove_method :logger
+  # end
 
   def self.logger
     puts "redefined logger"
