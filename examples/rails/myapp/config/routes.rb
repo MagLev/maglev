0eff0ecc89bbabc11033eccb0afa7ff21763220f
Peter McLain <peter.mclain@gemstone.com>
2010-08-11 10:51:53 -0700

    Update to the 3.0.0.rc generated standard app.

--- examples/rails/myapp/config/routes.rb
+++ examples/rails/myapp/config/routes.rb
@@ -1,7 +1,4 @@
-Myapp::Application.routes.draw do |map|
-
-  get "home/index"
-
+Myapp::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
