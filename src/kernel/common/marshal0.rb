924ae797eeb0156ab5f319d896bb9f5a2970ff4c
Peter McLain <peter.mclain@gemstone.com>
2010-09-07 15:00:28 -0700

    Fix bug where Marshal::TYPE_IVAR was being modified during object serialization.  The bug is fixed and the constants are frozen to make finding similar bugs easier.

--- src/kernel/common/marshal0.rb
+++ src/kernel/common/marshal0.rb
@@ -1,4 +1,4 @@
-# file marshal0.rb 
+# file marshal0.rb
 # first opening of Marshal, so bootstrap can use non-dynamic constants
 
 module Marshal
@@ -6,59 +6,89 @@ module Marshal
   MAJOR_VERSION = 4
   MINOR_VERSION = 8
 
-  VERSION_STRING = "\x04\x08"
+  #--
+  # NOTE: All string constants are frozen.  Protects against a bug where
+  # the constant was returned from a serialize_* method, and then used to
+  # build the serialized data for an object, thus turning "I" into
+  # "I%:blahblah..." for the next time...
+  #++
 
-  TYPE_ARRAY = '['
+  VERSION_STRING = "\x04\x08".freeze
+
+  TYPE_ARRAY = '['.freeze
   TYPE_ARRAY_ch = ?[
-  TYPE_BIGNUM = 'l'
+
+  TYPE_BIGNUM = 'l'.freeze
   TYPE_BIGNUM_ch = ?l
-  TYPE_CLASS = 'c'
+
+  TYPE_CLASS = 'c'.freeze
   TYPE_CLASS_ch = ?c
-  TYPE_DATA = 'd'  # no specs
+
+  TYPE_DATA = 'd'.freeze  # no specs
   TYPE_DATA_ch = ?d  # no specs
-  TYPE_EXTENDED = 'e'
+
+  TYPE_EXTENDED = 'e'.freeze
   TYPE_EXTENDED_ch = ?e
-  TYPE_FALSE = 'F'
+
+  TYPE_FALSE = 'F'.freeze
   TYPE_FALSE_ch = ?F
-  TYPE_FIXNUM = 'i'
+
+  TYPE_FIXNUM = 'i'.freeze
   TYPE_FIXNUM_ch = ?i
-  TYPE_FLOAT = 'f'
+
+  TYPE_FLOAT = 'f'.freeze
   TYPE_FLOAT_ch = ?f
-  TYPE_HASH = '{'
-  TYPE_HASH_DEF = '}'
+
+  TYPE_HASH = '{'.freeze
+  TYPE_HASH_DEF = '}'.freeze
+
   TYPE_HASH_DEF_ch = ?}
   TYPE_HASH_ch = ?{
-  TYPE_IVAR = 'I'
-  TYPE_IVAR_ch = ?I
-  TYPE_LINK = '@'
-  TYPE_LINK_ch = ?@
-  TYPE_MODULE = 'm'
-  TYPE_MODULE_OLD = 'M'  # no specs
-  TYPE_MODULE_OLD_ch = ?M   # no specs
-  TYPE_MODULE_ch = ?m
-  TYPE_NIL = '0'
-  TYPE_NIL_ch = ?0
-  TYPE_OBJECT = 'o'
-  TYPE_OBJECT_ch = ?o
-  TYPE_REGEXP = '/'
-  TYPE_REGEXP_ch = ?/
-  TYPE_STRING = '"'
-  TYPE_STRING_ch = ?"
-  TYPE_STRUCT = 'S'
-  TYPE_STRUCT_ch = ?S
-  TYPE_SYMBOL = ':'
-  TYPE_SYMBOL_ch = ?:
-  TYPE_SYMLINK = ';'
-  TYPE_SYMLINK_ch = ?; 
-  TYPE_TRUE = 'T'
-  TYPE_TRUE_ch = ?T
-  TYPE_UCLASS = 'C'
-  TYPE_UCLASS_ch = ?C
-  TYPE_USERDEF = 'u'
-  TYPE_USERDEF_ch = ?u
-  TYPE_USRMARSHAL = 'U'
-  TYPE_USRMARSHAL_ch = ?U
 
+  TYPE_IVAR = 'I'.freeze
+  TYPE_IVAR_ch = ?I
+
+  TYPE_LINK = '@'.freeze
+  TYPE_LINK_ch = ?@
+
+  TYPE_MODULE = 'm'.freeze
+  TYPE_MODULE_ch = ?m
+
+  TYPE_MODULE_OLD = 'M'.freeze  # no specs
+  TYPE_MODULE_OLD_ch = ?M   # no specs
+
+  TYPE_NIL = '0'.freeze
+  TYPE_NIL_ch = ?0
+
+  TYPE_OBJECT = 'o'.freeze
+  TYPE_OBJECT_ch = ?o
+
+  TYPE_REGEXP = '/'.freeze
+  TYPE_REGEXP_ch = ?/
+
+  TYPE_STRING = '"'.freeze
+  TYPE_STRING_ch = ?"
+
+  TYPE_STRUCT = 'S'.freeze
+  TYPE_STRUCT_ch = ?S
+
+  TYPE_SYMBOL = ':'.freeze
+  TYPE_SYMBOL_ch = ?:
+
+  TYPE_SYMLINK = ';'.freeze
+  TYPE_SYMLINK_ch = ?;
+
+  TYPE_TRUE = 'T'.freeze
+  TYPE_TRUE_ch = ?T
+
+  TYPE_UCLASS = 'C'.freeze
+  TYPE_UCLASS_ch = ?C
+
+  TYPE_USERDEF = 'u'.freeze
+  TYPE_USERDEF_ch = ?u
+
+  TYPE_USRMARSHAL = 'U'.freeze
+  TYPE_USRMARSHAL_ch = ?U
 end
 Marshal.__freeze_constants
 
