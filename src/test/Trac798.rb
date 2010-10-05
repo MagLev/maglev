c854912bb18d0fd967b672caf68ab37aa6ca23ba
Peter McLain <peter.mclain@gemstone.com>
2010-10-05 10:39:50 -0700

    Remove junk at bottom of file

--- src/test/Trac798.rb
+++ src/test/Trac798.rb
@@ -11,25 +11,5 @@ x = methods.sort_by do |_, k, a, _, m|
   [k, a, m].compact
 end
 
-raise "Fail #{methods.inspect}" unless x == expected
+raise "Fail" unless x == expected
 
-# Behavior = __resolve_smalltalk_global(:Behavior)
-
-# class Behavior
-#   primitive_nobridge '__source_code_for', 'sourceCodeAt:environmentId:'
-#   def source_code_for(sym)
-#     __source_code_for(sym, 1)
-#   end
-# end
-
-# class C
-#   def m
-#     puts "A method"
-#   end
-# end
-
-# # This one works (ends in "end")
-# puts C.source_code_for(:m)
-
-# # But this one, and many others, doesn't work:
-# puts Math.source_code_for(:'asinh:')
