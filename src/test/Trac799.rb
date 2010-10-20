9dc061186a1b9548c310e2a879179e464ba529ee
Allen Otis <otisa@vmware.com>
2010-10-20 15:36:19 -0700

    Fix Trac 805

--- src/test/Trac799.rb
+++ src/test/Trac799.rb
@@ -1,5 +1,12 @@
 require 'socket'
 
-STDIN.reopen(TCPServer.new(5555))
+sa = TCPServer.new(5555)
+STDIN.reopen(sa)
+sb = STDIN
+
+sc = Socket.for_fd(STDIN.fileno)   # coverage for Trac 805
+unless sb._equal?(sc) ; raise 'Fail A'; end
+unless sb.class._equal?(TCPServer) ; raise 'Fail B'; end
 
 puts "OK"
+true
