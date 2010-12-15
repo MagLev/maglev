beaa329d6764d43b32ded758c0eeb30b8ff1dafe
Peter McLain <peter.mclain@gemstone.com>
2010-12-15 10:13:28 -0800

    Close socket after test (open socket broke subsequent tests).

--- src/test/Trac799.rb
+++ src/test/Trac799.rb
@@ -8,5 +8,8 @@ sc = Socket.for_fd(STDIN.fileno)   # coverage for Trac 805
 unless sb._equal?(sc) ; raise 'Fail A'; end
 unless sb.class._equal?(TCPServer) ; raise 'Fail B'; end
 
+# Be sure to close, other tests use this port....
+sa.close
+sc.close
 puts "OK"
 true
