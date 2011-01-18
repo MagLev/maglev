9a377d1230a89b4dffdfa2241900110f3687e076
Peter McLain <peter.mclain@gemstone.com>
2011-01-17 16:34:13 -0800

    Updated Trac837.rb

--- src/test/Trac837.rb
+++ src/test/Trac837.rb
@@ -1,3 +1,3 @@
-eval "require 'rational'"
+eval "def Rational ; end"
 puts "---- private methods: #{self.private_methods.grep(/Rat/).inspect}"
 raise "Fail" unless self.private_methods.grep(/Rat/) == ['Rational']
