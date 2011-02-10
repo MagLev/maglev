e10ca90c063829885d4ed8a8100843305cbccba5
Allen Otis <otisa@vmware.com>
2011-02-10 10:30:45 -0800

    Fix 850

--- src/test/Trac850.rb
+++ src/test/Trac850.rb
@@ -6,13 +6,16 @@
 #               during /Users/pmclain/tmp/x.rb
 #  ERROR 2010 , a MessageNotUnderstood occurred (error 2010), a UndefinedObject does not understand  #'persistentCopy' (MessageNotUnderstood)
 
+$x = nil
 Maglev.persistent do
   class C
     include Module.new {
       def foo
-        puts "hi"
+        $x = "hi"
       end
     }
   end
 end
 C.new.foo
+unless $x == 'hi' ; raise 'Fail'; end
+true
