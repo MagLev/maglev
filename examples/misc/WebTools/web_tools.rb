51a501c6845afdb4bf327c6b8e2dde2fe46a7a0d
Peter McLain <pmclain@vmware.com>
2011-03-11 16:34:27 -0800

    WebTools demo now seems to work (requires SVN 25439).

--- examples/misc/WebTools/web_tools.rb
+++ examples/misc/WebTools/web_tools.rb
@@ -3,16 +3,14 @@
 #  registers that code into the Ruby namespace, and then starts the
 #  application.  Once it is running, it will print a URL to connect to.
 
-# Register the Smalltalk WebTools class into the Ruby Namespace
-WebTools = __resolve_smalltalk_global(:WebTools)
+# Register the Smalltalk WebTools Server class into the Ruby Namespace
+WebTools = __resolve_smalltalk_global(:Server)
 
 # Expose the class-side method needed to run the application
 class WebTools
-  class_primitive_nobridge 'run', 'run'
+  class_primitive_nobridge 'run', 'runInForeground'
 end
 
 # Invoke.  This will print out something like: http://cairo:60166/
 # Point your web browser to the given url and play.
 WebTools.run
-
-
