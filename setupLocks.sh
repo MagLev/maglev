4d0f8c7634029269577d6935acd30c08af6054a0
Monty Williams <monty.williams@gemstone.com>
2011-03-16 14:58:48 -0700

    Update Changes and README in prep for 25476 Added setupLocks.sh to help run GemStone and MagLev on the same machine

---
+++ setupLocks.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+#=========================================================================
+# Copyright (C) GemStone Systems, Inc. 2011.
+#
+# Name - setupLocks.sh
+#
+# Purpose - Make sure MagLev and GemStone are both using the same locks directory
+#
+# $Id:$
+#
+# Description:
+#    GemStone expects locks to be in /opt/gemstone/locks
+#    MagLev expects locks to be in $MAGLEV_HOME/locks
+#    Ditto for log directory
+#    This script links them together
+#    Use at your own risk!
+#
+# Actions:
+#    Stop MagLev first if it is running
+#    Create /opt/gemstone/locks and /opt/gemstone/log
+#    Remove existing $MAGLEV_HOME/locks and $MAGLEV_HOME/log
+#    Soft link respective directories to equivalent ones in /opt/gemstone
+
+cd $MAGLEV_HOME
+maglev stop
+
+sudo mkdir -p /opt/gemstone /opt/gemstone/log /opt/gemstone/locks
+sudo chown $USER:${GROUPS[0]} /opt/gemstone /opt/gemstone/log /opt/gemstone/locks
+sudo chmod 770 /opt/gemstone /opt/gemstone/log /opt/gemstone/locks
+
+rm -rf locks log
+
+ln -s /opt/gemstone/log
+ln -s /opt/gemstone/locks
