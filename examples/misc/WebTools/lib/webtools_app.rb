3be66a7a129531d32a9d92949c073240ad2e302b
Peter McLain <pmclain@vmware.com>
2011-03-22 11:28:14 -0700

    WIP: Split off from the WebTools javascript and css

--- examples/misc/WebTools/lib/webtools_app.rb
+++ examples/misc/WebTools/lib/webtools_app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'webtools'
 require 'json'
 
 class WebToolsApp < Sinatra::Base
@@ -11,15 +12,60 @@ class WebToolsApp < Sinatra::Base
     erb :index
   end
 
+  get '/version' do
+    stone_rpt = WebTools::API.stone_version_report
+    gem_rpt = WebTools::API.gem_version_report
+    @data = { }
+    (stone_rpt.keys + gem_rpt.keys).each do |k|
+      @data[k] = [stone_rpt[k], gem_rpt[k]]
+    end
+    erb :version
+  end
+
+  get '/sessions' do
+    content_type :json
+    ['sessions: Not Implemented'].to_json
+  end
+
+  get '/browsecode' do
+    content_type :json
+    ['browsecode: Not Implemented'].to_json
+  end
+
+  get '/statistics' do
+    content_type :json
+    ['statistics: Not Implemented'].to_json
+  end
+
   # Provide compatibility with the WebTools app.  This returns information
   # about the tools provided by WebTools
+  #
+  # TODO: Perhaps we can have a class per tool?
   get '/tools' do
     pages = []
     pages << {
-      'file' => 'xyz.html',
+      'file' => '/version',
       'name' => 'Version Report',
       'description' => 'Information about the Stone and Gem processes and their host(s)',
       'title' => 'the title'
+    }
+    pages << {
+      'file' => 'sessions',
+      'name' => 'Current Sessions',
+      'description' => 'Information about current sessions and other processes',
+      'title' => ''
+    }
+    pages << {
+      'file' => 'browsecode',
+      'name' => 'Code Browser',
+      'description' => 'Browse Classes, Constants, Methods and source',
+      'title' => ''
+    }
+    pages << {
+      'file' => 'statistics',
+      'name' => 'Statistics',
+      'description' => 'Load and view statmonitor files',
+      'title' => ''
     }
 
     content_type :json
