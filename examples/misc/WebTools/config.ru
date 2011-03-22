96399756819477024a70832cdfe42ab170ba29a8
Peter McLain <pmclain@vmware.com>
2011-03-21 17:55:28 -0700

    WIP: Get more of skeletal app working in the WebTools manner.

--- examples/misc/WebTools/config.ru
+++ examples/misc/WebTools/config.ru
@@ -1,4 +1,8 @@
 
 require 'webtools_app'
 
+WebToolsApp.disable :run
+#WebToolsApp.set :public, File.expand_path(File.dirname(__FILE__)) + '/public'
+WebToolsApp.set :root, File.dirname(__FILE__)
+
 WebToolsApp.run!
--- examples/misc/WebTools/lib/webtools_app.rb
+++ examples/misc/WebTools/lib/webtools_app.rb
@@ -1,7 +1,30 @@
 require 'sinatra/base'
+require 'json'
 
 class WebToolsApp < Sinatra::Base
+
+  before do
+    @ts = Time.now
+  end
+
   get '/' do
-    'hello'
+    erb :index
+  end
+
+  # Provide compatibility with the WebTools app.  This returns information
+  # about the tools provided by WebTools
+  get '/tools' do
+    pages = []
+    pages << {
+      'file' => 'xyz.html',
+      'name' => 'Version Report',
+      'description' => 'Information about the Stone and Gem processes and their host(s)',
+      'title' => 'the title'
+    }
+
+    content_type :json
+    sinatra_time = ((Time.now - @ts) * 1_000).to_i
+    { 'tools' => pages,
+      '_time'  => sinatra_time }.to_json
   end
 end
