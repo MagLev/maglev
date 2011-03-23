65b49ce236db6887ead734bf8f84eb29174a19ef
Allen Otis <otisa@vmware.com>
2011-03-23 13:01:43 -0700

    fix spec failures

--- src/kernel/common/dir_glob.rb
+++ src/kernel/common/dir_glob.rb
@@ -4,6 +4,13 @@ class Dir
       def initialize(nxt, flags)
         @flags = flags
         @next = nxt
+        @separator = nil
+      end
+
+      attr_writer :separator
+
+      def separator
+        @separator || "/"
       end
 
       def path_join(parent, ent)
@@ -11,7 +18,7 @@ class Dir
         if parent == "/"
           "/#{ent}"
         else
-          "#{parent}/#{ent}"
+          "#{parent}#{separator}#{ent}"
         end
       end
     end
@@ -25,8 +32,42 @@ class Dir
       def call(env, path)
         full = path_join(path, @dir)
 
-        if File.directory? full
-          @next.call env, full
+        # Don't check if full exists. It just costs us time
+        # and the downstream node will be able to check properly.
+        @next.call env, full
+      end
+    end
+
+    class ConstantEntry < Node
+      def initialize(nxt, flags, name)
+        super nxt, flags
+        @name = name
+      end
+
+      def call(env, parent)
+        path = path_join(parent, @name)
+
+        if File.exists? path
+          env.matches << path
+        end
+      end
+    end
+
+    class ConstantSuffixEntry < Node
+      def initialize(nxt, flags, name, suffixes)
+        super nxt, flags
+        @name = name
+        @suffixes = suffixes
+      end
+
+      def call(env, parent)
+        stem = path_join(parent, @name)
+
+        @suffixes.each do |s|
+          path = "#{stem}#{s}"
+          if File.exists?(path)
+            env.matches << path
+          end
         end
       end
     end
@@ -39,17 +80,69 @@ class Dir
 
     class RecursiveDirectories < Node
       def call(env, start)
-        allow_dots = ((@flags & File::FNM_DOTMATCH) != 0)
+        return unless File.exists? start
 
-        if start.nil? or File.exists? start
-          @next.call env, start
-        end
+        # Even though the recursive entry is zero width
+        # in this case, it's left seperator is still the
+        # dominant one, so we fix things up to use it.
+        switched = @next.dup
+        switched.separator = @separator
+        switched.call env, start
 
         stack = [start]
 
+        allow_dots = ((@flags & File::FNM_DOTMATCH) != 0)
+
         until stack.empty?
           path = stack.pop
-          dir = Dir.new(path ? path : ".")
+          dir = Dir.new(path)
+          while ent = dir.read
+            next if ent == "." || ent == ".."
+            full = path_join(path, ent)
+
+            if File.directory? full and (allow_dots or ent[0] != ?.)
+              stack << full
+              @next.call env, full
+            end
+          end
+          dir.close
+        end
+      end
+    end
+
+    class StartRecursiveDirectories < Node
+      def call(env, start)
+        raise "invalid usage" if start
+
+        # Even though the recursive entry is zero width
+        # in this case, it's left seperator is still the
+        # dominant one, so we fix things up to use it.
+        if @separator
+          switched = @next.dup
+          switched.separator = @separator
+          switched.call env, start
+        else
+          @next.call env, start
+        end
+
+        stack = []
+
+        allow_dots = ((@flags & File::FNM_DOTMATCH) != 0)
+
+        dir = Dir.new(".")
+        while ent = dir.read
+          next if ent == "." || ent == ".."
+
+          if File.directory? ent and (allow_dots or ent[0] != ?.)
+            stack << ent
+            @next.call env, ent
+          end
+        end
+        dir.close
+
+        until stack.empty?
+          path = stack.pop
+          dir = Dir.new(path)
           while ent = dir.read
             next if ent == "." || ent == ".."
             full = path_join(path, ent)
@@ -64,38 +157,14 @@ class Dir
       end
     end
 
-    class EachDirectory < Node
-      def call(env, path)
-        dir = Dir.new(path)
-        while ent = dir.read
-          @next.call env, ent
-        end
-        dir.close
-      end
-    end
-
     class Match < Node
       def initialize(nxt, flags, glob)
         super nxt, flags
         @glob = glob
       end
 
-      Escape = %w!+ | ^ $ ( ) [ ]!
-      def to_regex(glob)
-        str = glob.gsub ".", "\\."
-        Escape.each do |char|
-          str.gsub! char, "\\#{char}"
-        end
-        str.gsub! /[^\\]\*/, ".+"
-        str.gsub! /[^\\]\?/, "."
-        # handle {}
-
-        Regexp.new "^#{str}$"
-      end
-
       def match?(str)
         File.fnmatch @glob, str, @flags
-        # FFI::Platform::POSIX.fnmatch(@glob, str, 4) == 0
       end
     end
 
@@ -107,6 +176,8 @@ class Dir
       end
 
       def call(env, path)
+        return if path and !File.exists?("#{path}/.")
+
         dir = Dir.new(path ? path : ".")
         while ent = dir.read
           if match? ent
@@ -123,9 +194,11 @@ class Dir
 
     class EntryMatch < Match
       def call(env, path)
+        return if path and !File.exists?("#{path}/.")
+
         begin
           dir = Dir.new(path ? path : ".")
-        rescue Errno::EACCES
+        rescue SystemCallError
           return
         end
 
@@ -138,56 +211,82 @@ class Dir
       end
     end
 
-    class DirectoriesOnly < Match
+    class SuffixEntryMatch < Match
+      def initialize(nxt, flags, glob, suffixes)
+        super nxt, flags, glob
+        @suffixes = suffixes
+      end
+
       def call(env, path)
-        allow_dots = ((@flags & File::FNM_DOTMATCH) != 0)
-        all_dots = (@glob[0] == ?.)
+        return if path and !File.exists?("#{path}/.")
 
-        dir = Dir.new(path ? path : ".")
-        while ent = dir.read
-          unless all_dots
-            if ent[0] == ?.
-              next if ent.size == 1 or ent[1] == ?.
-              next unless allow_dots
+        begin
+          dir = Dir.new(path ? path : ".")
+        rescue SystemCallError
+          return
+        end
+
+        while f = dir.read
+          @suffixes.each do |s|
+            ent = "#{f}#{s}"
+            if match? ent
+              env.matches << path_join(path, ent)
             end
-          end
-
-          full = path_join(path, ent)
-
-          if File.directory? full and match? ent
-            env.matches << "#{full}/"
           end
         end
         dir.close
       end
     end
 
-    class Environment
-      attr_reader :matches
-
-      def initialize
-        @matches = []
+    class DirectoriesOnly < Node
+      def call(env, path)
+        if path and File.exists?("#{path}/.")
+          env.matches << "#{path}/"
+        end
       end
     end
 
-    def self.compile(glob, flags=0)
-      parts = glob.split("/")
+    class Environment
+      attr_reader :matches
 
-      file = parts.pop
+      def initialize(matches=[])
+        @matches = matches
+      end
+    end
+
+    def self.single_compile(glob, flags=0, suffixes=nil)
+      parts = glob.split(%r!(/+)!)
 
       if glob[-1] == ?/
-        last = DirectoriesOnly.new nil, flags, file
-        if parts.empty?
-          last = RecursiveDirectories.new last, flags
-        end
+        last = DirectoriesOnly.new nil, flags
       else
-        last = EntryMatch.new nil, flags, file
+        file = parts.pop
+        if /^[a-zA-Z0-9._]+$/.match(file)
+          if suffixes
+            last = ConstantSuffixEntry.new nil, flags, file, suffixes
+          else
+            last = ConstantEntry.new nil, flags, file
+          end
+        else
+          if suffixes
+            last = SuffixEntryMatch.new nil, flags, file, suffixes
+          else
+            last = EntryMatch.new nil, flags, file
+          end
+        end
       end
 
-      parts.reverse_each do |dir|
+      until parts.empty?
+        last.separator = parts.pop
+        dir = parts.pop
+
         if dir == "**"
-          last = RecursiveDirectories.new last, flags
-        elsif /[a-zA-Z0-9.]+/.match(dir)
+          if parts.empty?
+            last = StartRecursiveDirectories.new last, flags
+          else
+            last = RecursiveDirectories.new last, flags
+          end
+        elsif /^[a-zA-Z0-9.]+$/.match(dir)
           last = ConstantDirectory.new last, flags, dir
         elsif !dir.empty?
           last = DirectoryMatch.new last, flags, dir
@@ -201,29 +300,63 @@ class Dir
       last
     end
 
-    def self.run(node)
-      env = Environment.new
+    def self.run(node, matches=[])
+      env = Environment.new(matches)
       node.call env, nil
       env.matches
     end
 
-    def self.glob(pattern, flags)
-      if pattern.include? "{"
-        return brace_glob(pattern, flags)
+    def self.glob(pattern, flags, matches=[])
+      # Rubygems typicall uses Dir[] as basicly a glorified File.exists?
+      # to check for multiple extensions. So we went ahead and sped up
+      # that specific case.
+
+      if flags == 0 and
+             m = /^([a-zA-Z0-9_.\/\s]*[a-zA-Z0-9_.])(?:\{([^{}\/\*\?]*)\})?$/.match(pattern)
+        # no meta characters, so this is a glorified
+        # File.exists? check. We allow for a brace expansion
+        # only as a suffix.
+
+        if braces = m[2]
+          stem = m[1]
+
+          braces.split(",").each do |s|
+            path = "#{stem}#{s}"
+            if File.exists? path
+              matches << path
+            end
+          end
+
+          # Split strips an empty closing part, so we need to add it back in
+          if braces[-1] == ?,
+            matches << stem if File.exists? stem
+          end
+        else
+          matches << pattern if File.exists?(pattern)
+        end
+
+        return matches
       end
 
-      if node = compile(pattern, flags)
-        run node
+      if pattern.include? "{"
+        patterns = compile(pattern, flags)
+
+        patterns.each do |node|
+          run node, matches
+        end
+      elsif node = single_compile(pattern, flags)
+        run node, matches
       else
-        []
+        matches
       end
     end
 
-    def self.brace_glob(pattern, flags, matches=[])
+    def self.compile(pattern, flags=0, patterns=[])
       escape = (flags & File::FNM_NOESCAPE) == 0
 
       rbrace = nil
       lbrace = nil
+      escapes = false
 
       # Do a quick search for a { to start the search better
       i = pattern.index("{")
@@ -231,10 +364,11 @@ class Dir
       # If there was a { found, then search
       if i
         nest = 0
-        total = pattern.size
+        data = pattern
+        total = pattern.__size
 
         while i < total
-          char = pattern.__at(i)
+          char = data[i]
 
           if char == ?{ and nest == 0
             lbrace = i
@@ -248,12 +382,24 @@ class Dir
           end
 
           if char == ?\\ and escape
+            escapes = true
             i += 1
           end
 
           i += 1
         end
       end
+
+      # Detect if it's a simple suffix brace
+      # if !escapes and lbrace and rbrace == pattern.size - 1
+        # parts = pattern.substring(lbrace+1, rbrace - lbrace - 1).split(",")
+        # front = pattern.substring(0, lbrace)
+
+        # if node = single_compile(front, flags, parts)
+          # patterns << node
+          # return patterns
+        # end
+      # end
 
       # There was a full {} expression detected, expand each part of it
       # recursively.
@@ -281,7 +427,7 @@ class Dir
 
           brace_pattern = "#{front}#{pattern[last...pos]}#{back}"
 
-          brace_glob brace_pattern, flags, matches
+          compile brace_pattern, flags, patterns
         end
 
         # No braces found, match the pattern normally
@@ -290,12 +436,12 @@ class Dir
         # if a { is a brace or a just a normal character, but .glob can't.
         # if .glob is used and there is a { as a normal character, it will
         # recurse forever.
-        if node = compile(pattern, flags)
-          matches.concat run(node)
+        if node = single_compile(pattern, flags)
+          patterns << node
         end
       end
 
-      return matches
+      return patterns
     end
   end
 end
