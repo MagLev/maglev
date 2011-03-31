f1ec0c923b45657f7ee86864bb8a446ce58d4b04
Peter McLain <pmclain@vmware.com>
2011-03-31 13:26:46 -0700

    Fix problem with Safari; clean up debug output;

--- examples/misc/WebTools/public/javascript/webtools.js
+++ examples/misc/WebTools/public/javascript/webtools.js
@@ -2,7 +2,6 @@ maglevInfo = (function() {
   var requestCount = 0, rubyEditor = null;
 
   $(document).ready(function() {
-    console.log('document ready');
     setupTabs();
     setupSelectables();
     setupEditor();
@@ -10,22 +9,16 @@ maglevInfo = (function() {
   });
 
   function setupTabs() {
-    console.log('setupTabs()');
     var tabs = $("#tabs");
     tabs.tabs();
     tabs.bind('tabsselect', function(event, ui) {
-      console.log("Tabs Select: " + ui.panel.id);
       if (ui.panel.id == 'rubyCodeBrowser') {
-        console.log('rubyCodeBrowser selected');
         updateCodeBrowser();
       }
     });
   }
 
   function setupSelectables() {
-    // May have to re-run this if we reload module list?
-    console.log('setupSelectables()');
-
     $('#rubyModules').selectable({
       selected: function(event, ui) { setSelectedClass(ui.selected.title); }
     });
@@ -76,7 +69,6 @@ maglevInfo = (function() {
   };
 
   function setSelectedModuleMethod(methodName) {
-    console.log('selected module method ' + methodName);
     $('#rubyConstants .ui-selected').removeClass('ui-selected');
     $('#rubyInstanceMethods .ui-selected').removeClass('ui-selected');
 
@@ -88,7 +80,6 @@ maglevInfo = (function() {
   };
 
   function setSelectedInstanceMethod(methodName) {
-    console.log('selected instance method ' + methodName);
     $('#rubyConstants .ui-selected').removeClass('ui-selected');
     $('#rubyModuleMethods .ui-selected').removeClass('ui-selected');
 
@@ -105,14 +96,13 @@ maglevInfo = (function() {
     var startTime = new Date().getTime();
     var statusBar = $('#statusBar');
     $(statusBar).text('Sent request #' + (++requestCount) + ' for ' + url);
-
-    console.log('Calling $.getJSON with data: ');
-    console.log(data);
     $.getJSON(url, data, success);
     return;
 
     function success(data) {
       console.log("getJSON success");
+      console.log(data);
+
       var serverTime = data['_time'];
       var networkTime = new Date().getTime() - startTime - serverTime;
       startTime = new Date().getTime();
@@ -134,7 +124,6 @@ maglevInfo = (function() {
 
   // Retrieve fresh data for the codebrowser and render it
   function updateCodeBrowser() {
-    console.log('updateCodeBrowser()');
     getJSON('/modulelist', null, renderCodeBrowser);
   };
 
@@ -143,52 +132,26 @@ maglevInfo = (function() {
   };
 
   function renderCodeBrowser(data) {
-    console.log('renderCodeBrowser()');
-    renderClassModuleList();
-    renderSelection();
+    renderList(data['modules'],          $('#rubyModules'));
+    renderList(data['constants'],        $('#rubyConstants'));
+    renderList(data['module_methods'],   $('#rubyModuleMethods'));
+    renderList(data['instance_methods'], $('#rubyInstanceMethods'));
     return;
 
-    function renderSelection() {
-      console.log('renderSelection()');
-      console.log(data);
-
-      renderList(data['constants'],        $('#rubyConstants'));
-      renderList(data['module_methods'],   $('#rubyModuleMethods'));
-      renderList(data['instance_methods'], $('#rubyInstanceMethods'));
-    };
-
     function renderList(items, ui) {
-      console.log('renderList('+ui+')');
       if (items) {
         ui.empty();
         $.each(items, function(n, item) {
-          var li = $('<li>', { title: item, class: 'ui-widget-content'});
-          li.html(item);
-          li.appendTo(ui);
+          $('<li>', { title: item, 'class': 'ui-widget-content' }).html(item).appendTo(ui);
         });
       }
-    };
-
-    function renderClassModuleList() {
-      var modules = data['classNames'],
-      selected = data['selected_module'];
-      console.log('renderClassModuleList()');
-      if (modules) {
-        var moduleList = $('#rubyModules').empty();
-        $.each(modules, function(n, mod){
-          var li = $('<li>', { title: mod,
-                               class: 'ui-widget-content'});
-          li.html(mod);
-          li.appendTo(moduleList);
-        });
-      };
     };
   }
 
   function renderSource(string) {
-    console.log('renderSource: ' + string);
     if (rubyEditor.editor) {
       rubyEditor.setCode(string);
     }
   };
+
 })();
