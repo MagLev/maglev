244f68b5e08b8927bf7d60c3c27d0fbc09f40884
Peter McLain <pmclain@vmware.com>
2011-04-05 11:33:24 -0700

    Add a demo data file

---
+++ examples/misc/WebTools/demo_data.rb
@@ -0,0 +1,46 @@
+# This file defines a class that has lots of interesting features.
+#
+# 1. The class is named to appear near the top of the class list (easy
+#    access for demos).
+# 2. The class has public, private and protected instance methods
+
+Maglev.persistent do
+  class AAADemo
+    # Provide easy access to PERSISTENT_ROOT
+    PROOT = Maglev::PERSISTENT_ROOT
+
+    def self.foo
+      "This is a class method"
+    end
+
+    def initialize
+      @foo = "foo"
+      @bar = "bar"
+      @rand = rand(100)
+    end
+
+    def inspect
+    end
+    protected
+    def protected_method
+      "This is a protected method"
+    end
+
+    private
+    def private_method
+      "This is a private method"
+    end
+
+    INSTANCES = [AAADemo.new, AAADemo.new]
+    AN_ARRAY = []
+    30.times { |i| AN_ARRAY << i }
+  end
+
+  # Define a method programmatically
+
+  AAADemo.define_method(:metaprogrammed) do
+    x = :foo
+    "This method metaprogrammed to return #{x}"
+  end
+
+end
