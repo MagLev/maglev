a28d10f15d08a3432fff340dab6fceec1b61a461
Allen Otis <otisa@vmware.com>
2011-04-11 12:49:37 -0700

    adjust name of source file showing up in methods generated by attr_reader, attr_writer

--- src/kernel/bootstrap/Module3.rb
+++ src/kernel/bootstrap/Module3.rb
@@ -3,10 +3,11 @@ class Module
     bnd = Binding.__basic_new(self)
     bnd.__set_lex_scope(LexicalPath.new)
     fake_vcglbl = [nil,nil]
+    args = [ bnd , '(attr_reader)', 1 ]
     names.each do |n|
       the_name = self.__attr_type_check(n)
       str = "def #{the_name}; @#{the_name}; end"
-      self.__module_eval_string( str, fake_vcglbl, bnd )
+      self.__module_eval_string( str, fake_vcglbl, *args )
     end
   end
 
@@ -14,10 +15,11 @@ class Module
     bnd = Binding.__basic_new(self)
     bnd.__set_lex_scope(LexicalPath.new)
     fake_vcglbl = [nil,nil]
+    args = [ bnd , '(attr_writer)', 1 ]
     names.each do |n|
       the_name = self.__attr_type_check(n)
       str = "def #{the_name}=(v); @#{the_name} = v; end"
-      self.__module_eval_string( str, fake_vcglbl, bnd )
+      self.__module_eval_string( str, fake_vcglbl, *args )
     end
   end
 end
