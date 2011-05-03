75964f724aade7563675ee22c46e36bfba1a3fbf
Allen Otis <otisa@vmware.com>
2011-05-02 19:09:09 -0700

    changes to go with new Smalltalk implementation of Random

--- src/kernel/bootstrap/Random.rb
+++ src/kernel/bootstrap/Random.rb
@@ -1,13 +1,27 @@
-# Random is the Smalltalk class TransientRandom
+# Random is the Smalltalk class RubyRandom
 #
-# We use TransientRandom so that we the persistent instance,
-# RandomInstance, is still usable in the face of abort_transaction.
 class Random
   class_primitive_nobridge 'new'
   primitive_nobridge 'next', 'nextInt:'
   primitive_nobridge 'next'
-  primitive_nobridge 'seed', 'seed:'
-  primitive_nobridge 'seed', 'seed'
+  primitive_nobridge '_seed', 'seed:'
+
+  def seed
+    @_st_seed
+  end
+  def seed(v)
+    v = Type.coerce_to(v, Fixnum, :to_int)
+    @_st_seed = v
+    self._seed(v)
+  end
+     
 end
-RandomInstance = Random.new
+
+class RandomNp < Random
+  # Smalltalk class RubyRandomNp
+  # instances may not be committed
+end
+
+# transient constant RandomInstance , instance of RandomNp,
+# defined in delta/Object3.rb 
 
