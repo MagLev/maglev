222ef76ecbff81687f323c3fd0a1d9f8f7cb96d3
Allen Otis <otisa@vmware.com>
2011-06-06 15:35:04 -0700

    Changes to go with reentrant eval args, and Trac 907

--- src/kernel/bootstrap/Proc.rb
+++ src/kernel/bootstrap/Proc.rb
@@ -7,12 +7,6 @@ class VariableContext
   primitive_nobridge 'at', 'at:'
   primitive_nobridge 'put', 'at:put:'
 
-  def __evVcGput(tilde, underscore)
-    # for use by eval implementation only
-         tilde.__storeRubyVcGlobal(0x70)
-    underscore.__storeRubyVcGlobal(0x71)
-    self
-  end
 end
 
 class ExecBlock
@@ -35,101 +29,113 @@ class ExecBlock
   primitive_nobridge '__set_self', 'setSelf:'
   primitive_nobridge '__get_self' , 'selfValue'
 
-  # call, call:, call::, call::: , call&, call:&, call::&
-  #  will be compiled to special bytecodes
+  # call, call:, call::, call::: , call*
+  #  variants without & will be compiled to special bytecodes
   #  and won't use the bridge methods generated for  call*
 
-  # bridge methods generated for call*  will be used but the actual
-  #   send of call* will be a special bytecode.
-  primitive          'call*' , '_rubyCall:'
+  # bridge methods generated for call*&  will be used 
+  # sends of variants of call without & use Bc_SEND_CALL bytecode
+  #  and bypass bridge methods
+  
+  primitive          'call*&' , '_rubyCall:block:'
 
-    def [](*args, &block)
-      self.call(*args, &block)
-    end
+  def [](*args)  # ignore any &block argument
+    self.call(*args)
+  end
 
-    # optimize common variants to avoid bridge method 
-    def []
-      self.call
-    end
+  # optimize common variants to avoid bridge method 
+  def []
+    self.call
+  end
 
-    def [](a)  # also used by Cext implementation
-      self.call(a)
-    end
+  def [](a)  # also used by Cext implementation
+    self.call(a)
+  end
 
-    def [](a, b)
-      self.call(a, b)
-    end
+  def [](a, b)
+    self.call(a, b)
+  end
 
-    def [](a, b, c)
-      self.call(a, b, c)
-    end
+  def [](a, b, c)
+    self.call(a, b, c)
+  end
 
-    def [](*args)  # also used by Cext implementation
-      self.call(*args)
-    end
+  # following to handle __send__ , they are not actually recursive
+  #   body uses Bc_SEND_CALL bytecode and avoid bridge to call*&
+  def call(a)
+    self.call(a)
+  end
+  def call(a, b)
+    self.call(a, b)
+  end
+  def call(a, b, c)
+    self.call(a, b, c)
+  end
+  def call(*a)
+    self.call(*a)
+  end
+  def call(a, *b)
+    arr = [ a ]
+    arr.concat(*b)
+    self.call(*arr)
+  end 
+  def call(a, b, *c)
+    arr = [ a , b ]
+    arr.concat(*c)
+    self.call(*arr)
+  end 
+  def call(a, b, c, *d)
+    arr = [ a , b, c ]
+    arr.concat(*d) 
+    self.call(*arr)
+  end
 
-    def [](&block)
-      self.call(&block)
-    end
+  def __signal_callback
+    # used by Signal#trap implemenation
+    self.call
+  end
 
-    def [](a, &block)
-      self.call(a, &block)
-    end
+  def __fficallback(*args)
+    # execution of an ExecBlock by an FFI callback invokes this method
+    # also used by Cext
+    self.call(*args)
+  end
 
-    def [](a, b, &block)
-      self.call(a, b, &block)
-    end
-
-    def [](a, b, c, &block)
-      self.call(a, b, c, &block)
-    end
-
-    def __signal_callback
-      # used by Signal#trap implemenation
-      self.call
-    end
-
-    def __fficallback(*args)
-      # execution of an ExecBlock by an FFI callback invokes this method
-      # also used by Cext
-      self.call(*args)
-    end
- 
-    def arity
-      na = self.__num_args 
-      if na._equal?(0)
-        if self.__no_declared_args
-          na = -1  # for Proc.new { }.arity == -1
-        end 
-      else
-        if self.__last_star
-          na = -(na)  # negated (num required args + 1)
-        end
+  def arity
+    na = self.__num_args 
+    if na._equal?(0)
+      if self.__no_declared_args
+	na = -1  # for Proc.new { }.arity == -1
+      end 
+    else
+      if self.__last_star
+	na = -(na)  # negated (num required args + 1)
       end
-      na
     end
+    na
+  end
 
-    primitive_nobridge 'inspect', '_rubyInspect' 
+  primitive_nobridge 'inspect', '_rubyInspect' 
 
-    def to_proc
-      Proc.new(self)
-    end
-    def __to_proc
-      # invoked from generated code when incoming block argument is
-      # used other than passing as value for & suffix char of a method call.
-      # The result of __to_proc is cached in an evaluation temporary generated
-      # as a method temp by the AST to IR transformation.
-      # Exceptions to automatic __to_proc coercion:
-      #   a) any code compiled as bootstrap code. In bootstrap code __to_proc
-      #      must be coded explicitly
-      #   b) 'NoToProcSelectors' defined by RubyAbstractCallNode>>initialize ,
-      #      defines private selectors for which receiver and/or last arg is
-      #      not automatically coerced.
-      Proc.new(self)
-    end
-    def __to_proc_arg
-      self
-    end
+  def to_proc
+    Proc.new(self)
+  end
+  def __to_proc
+    # invoked from generated code when incoming block argument is
+    # used other than passing as value for & suffix char of a method call.
+    # The result of __to_proc is cached in an evaluation temporary generated
+    # as a method temp by the AST to IR transformation.
+    # Exceptions to automatic __to_proc coercion:
+    #   a) any code compiled as bootstrap code. In bootstrap code __to_proc
+    #      must be coded explicitly
+    #   b) 'NoToProcSelectors' defined by RubyAbstractCallNode>>initialize ,
+    #      defines private selectors for which receiver and/or last arg is
+    #      not automatically coerced.
+    Proc.new(self)
+  end
+  def __to_proc_arg
+    self
+  end
 end
 
 
@@ -231,11 +237,14 @@ class Proc
 
     primitive_nobridge '__get_self' , 'selfValue'
 
-    def [](*args, &block)
+    def [](*args, &block)  
       @_st_block.call(*args, &block)
     end
 
-    # optimize common variants to avoid bridge methods
+    # variants without & must avoid bridge methods to call*&
+    #  and instead use the directly generated Bc_CALL_SEND bytecodes
+
+    # optimize common variants without & to avoid bridge methods
     def []
       @_st_block.call
     end
@@ -252,21 +261,25 @@ class Proc
       @_st_block.call(a, b, c)
     end
 
-    def [](&block)
-      @_st_block.call(&block)
+    def [](*a)
+      @_st_block.call(*a)
+    end
+    def [](a, *b)
+      arr = [ a ]
+      arr.concat(*b)
+      @_st_block.call(*arr)
+    end
+    def [](a, b, *c)
+      arr = [ a , b]
+      arr.concat(*c)
+      @_st_block.call(*arr)
+    end
+    def [](a, b, c, *d)
+      arr = [ a , b, c]
+      arr.concat(*d)
+      @_st_block.call(*arr)
     end
 
-    def [](a, &block)
-      @_st_block.call(a, &block)
-    end
-
-    def [](a, b, &block)
-      @_st_block.call(a, b, &block)
-    end
-
-    def [](a, b, c, &block)
-      @_st_block.call(a, b, c, &block)
-    end
 
     def __fficallback(*args)
       # execution of a Proc by an FFI callback invokes this method
@@ -312,7 +325,8 @@ class Proc
       @_st_block.call(*args, &block)
     end
 
-    # optimize common variants to avoid bridge methods
+    # variants without & must avoid bridge methods to call*&
+    #  and instead use the directly generated Bc_CALL_SEND bytecodes
     def call
       @_st_block.call
     end
@@ -325,17 +339,24 @@ class Proc
     def call(a, b, c)
       @_st_block.call(a, b, c)
     end
-    def call(&block)
-      @_st_block.call(&block)
+
+    def call(*a)
+      @_st_block.call(*a)
     end
-    def call(a, &block)
-      @_st_block.call(a, &block)
+    def call(a, *b)
+      arr = [ a ]
+      arr.concat(*b)
+      @_st_block.call(*arr)
     end
-    def call(a, b, &block)
-      @_st_block.call(a, b, &block)
+    def call(a, b, *c)
+      arr = [ a , b] 
+      arr.concat(*c) 
+      @_st_block.call(*arr)
     end
-    def call(a, b, c, &block)
-      @_st_block.call(a, b, c, &block)
+    def call(a, b, c, *d)
+      arr = [ a , b, c] 
+      arr.concat(*d) 
+      @_st_block.call(*arr)
     end
 
     #  Creating a Binding from a Proc is not yet supported.
