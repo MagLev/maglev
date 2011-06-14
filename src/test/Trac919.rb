e1c5a244be35a2a56c5b3b69640ef887877b7afc
Peter McLain <pmclain@vmware.com>
2011-06-14 12:31:52 -0700

    Added src/test/Trac919.rb

---
+++ src/test/Trac919.rb
@@ -0,0 +1,11 @@
+# MagLev raises an exception:
+#
+# error , a ArgumentError occurred (error 2718), cannot change protection of method in code_gen, aGsNMethod,
+#              during /Users/pmclain/tmp/pbm.rb
+# ERROR 2718 , a ArgumentError occurred (error 2718), cannot change protection of method in code_gen, aGsNMethod (ArgumentError)
+#
+class OrderedOptions < Hash
+  alias_method :_get, :[]
+  protected :_get
+end
+
