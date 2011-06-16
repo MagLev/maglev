257e6339f306affcc74b6256bea0ab48115c6606
Allen Otis <otisa@vmware.com>
2011-06-16 08:40:42 -0700

    additional tests

--- src/test/Trac913.rb
+++ src/test/Trac913.rb
@@ -4,4 +4,19 @@ end
 
 C.new.send('foo:bar:', 'today913')
 unless $ax == 'today913' ; raise 'fail' ; end
-true
+
+class D
+end
+D.send(:define_method, "foo:xx") { |x| $bx << x }
+
+$bx = []
+
+mx = D.instance_methods(false)
+
+D.new.send("foo:xx")
+D.new.send("foo:xx" , 99)
+
+bx = $bx
+unless bx == [ nil, 99 ] ; raise 'fail'; end
+puts mx.inspect
+puts "ok"
