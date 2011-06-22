e5fe4549f6c7c43c7be5d4e7c08d1c9a6f4937a8
Monty Williams <monty.williams@gemstone.com>
2011-06-21 17:19:59 -0700

    Release MagLev-26024

--- src/kernel/bootstrap/Globals.rb
+++ src/kernel/bootstrap/Globals.rb
@@ -149,12 +149,12 @@ RUBY_ENGINE = 'maglev'
 # RUBY_PLATFORM  is installed in RubyContext>>_initTransient:
 RUBY_VERSION = '1.8.7'
 # Note - the packager modifies the date on any line starting with RUBY_RELEASE_DATE
-RUBY_RELEASE_DATE = '2011-06-15'
+RUBY_RELEASE_DATE = '2011-06-21'
 RUBY_PATCHLEVEL = 249  # TODO: finalize for 1.8.7
 RUBY_DESCRIPTION = 'ruby 1.8.7 (maglev patchlevel 249)'
 RUBY_COPYRIGHT = 'ruby - Copyright (C) 1993-2010 Yukihiro Matsumoto; maglev additions Copyright(C) 2009-2010 GemStone Systems Inc.'
 
-VERSION = '26011-1286-4f1123e'
+VERSION = '26024-1287-027350a'
 
 MAGLEV_VERSION = VERSION  # per Trac 901
 
