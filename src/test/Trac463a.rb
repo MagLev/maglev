26a4213911ce5aebd14f07e301d21376ff5543e5
Allen Otis <otisa@vmware.com>
2011-06-24 11:54:00 -0700

    Fix Trac 924 and failures in language/def_spec,  includes fixes to break and define_method . Also fix some failures in sprintf specs.

--- src/test/Trac463a.rb
+++ src/test/Trac463a.rb
@@ -1,10 +1,55 @@
-$q = ['boo']
+# File Trac463a.rb  , 
+# coverage of break as used in Sinatra , 
+#  break out of a block with  ensure and rescue both active on the stack
 
-while not $q.empty?
-    begin
-        next if $q.shift
-      rescue ZeroDivisionError => err
-        p "This never happens"
+class C
+  def test
+    assert_raise(EOFError) {  geta() }  
+    ax = $ax
+    bx = $bx
+    unless [ ax , bx ] == [ 55, 66 ] ; raise 'fail'; end
+  end
+
+  def assert_raise(expected, &blk)
+    actual_exception = nil
+    got_ex = nil
+    rx = assert_block() {
+      begin
+        yield
+      rescue Exception => actual_exception
+        got_ex = 95
+        break 
       end
-   end
+    }
+    unless got_ex == 95 ; raise 'fail';end 
+    unless actual_exception.is_a?( expected) ; raise 'fail'; end
+  end
 
+  def assert_block(&blk)
+    rx = yield
+  end
+
+  def geta
+    begin
+      getb()
+    ensure
+      $ax = 55
+    end
+  end
+
+  def getb
+     begin
+       getc()
+     ensure
+       $bx = 66
+     end
+  end
+
+  def getc
+    raise EOFError
+  end
+
+end
+
+C.new.test
+true
