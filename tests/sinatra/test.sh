90f07cafdc7951292903897a3d3ebc4cc2500b45
Peter McLain <pmclain@vmware.com>
2011-07-06 14:59:22 -0700

    Make the sinatra tests run under Jenkins

--- tests/sinatra/test.sh
+++ tests/sinatra/test.sh
@@ -5,7 +5,6 @@
 
 PATH=$MAGLEV_HOME/bin:$PATH
 MAGLEV_OPTS=
-WORKSPACE=${WORKSPACE:-$PWD}  # allow to work from both command line and jenkins
 
 rm -rf sinatra
 git clone git://github.com/sinatra/sinatra.git
@@ -24,14 +23,23 @@ if [[ -f $badf ]]; then
 else
     echo "Could not find rack file to patch: $badf"
 fi
-mkdir -p "${WORKSPACE}/reports"
 
-maglev start
+# In the Jenkins environment, $WORKSPACE will be set.
+# We need to start maglev if we're under jenkins.
+if [[ -n $WORKSPACE ]]; then
+    mkdir -p "${WORKSPACE}/reports"
+    maglev start
+fi
+
 maglev-ruby -S bundle install
-bundle exec rake ci:setup:testunit test
+bundle exec rake -Ilib ci:setup:testunit test
 
 # Work around Maglev ci reporter builder bug
 # cd test/reports/
 # for i in *.xml; do
 #   cat "$i" | tail -n +2 > "${WORKSPACE}/reports/$i"
 # done
+
+if [[ -n $WORKSPACE ]]; then
+    maglev stop
+fi
