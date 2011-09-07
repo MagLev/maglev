89c08d13df21b62303a8f9373f1e318c4c871ab7
Peter McLain <pmclain@vmware.com>
2011-09-07 11:28:56 -0700

    Update rails example for Rails 3.1.0 final.

--- examples/rails/myapp/config/initializers/secret_token.rb
+++ examples/rails/myapp/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Myapp::Application.config.secret_token = 'aeafeef93adc31bfaf5e7cea7e0e73384d84aab370cc18853ea14ef24e882e032a4636aa2ea2047926fa4f7a08308333be3e8cc6348405caf13172d4879c215c'
+Myapp::Application.config.secret_token = 'f838e60b8ef13065a9f9bd14fa29bb0818f4d88b88877e54680ccd922ffbd76fc4fe64b8324acda9ccc61d82e1484c37c987d15567531d031b52a1fdf4e0081c'
