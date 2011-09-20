5c6d8c8a5110a27a5e63ce6b5a4b9e50564372db
Tim Felgentreff <timfelgentreff@gmail.com>
2011-09-19 17:58:17 -0700

    to provide some modified gems seamlessly, we prefer gems with the prefix '-maglev-' on it's name

--- lib/ruby/site_ruby/1.8/rubygems/defaults/maglev.rb
+++ lib/ruby/site_ruby/1.8/rubygems/defaults/maglev.rb
@@ -21,3 +21,48 @@ module Gem
   ConfigFile::PLATFORM_DEFAULTS['install'] = '--env-shebang'
   ConfigFile::PLATFORM_DEFAULTS['update']  = '--env-shebang'
 end
+
+Gem::MAGLEV_POSTFIX = "-maglev-"
+
+class Gem::Dependency
+  # MagLev specific gems should match in addition to the standard gems
+  def name
+    return @name if @name.nil?
+    return @__patched_name if @__patched_name && @name == @__patched_name
+
+    @__patched_name = @name.dup
+    def @__patched_name.===(other)
+      if other.to_s.end_with?(Gem::MAGLEV_POSTFIX)
+        super(other.to_s[0...-Gem::MAGLEV_POSTFIX.size])
+      else
+        super(other)
+      end
+    end
+    @__patched_name
+  end
+end
+
+class Gem::Specification
+  def name
+    return @name if @name.nil?
+    return @__patched_name if @__patched_name && @name == @__patched_name
+
+    @__patched_name = @name.dup
+    def @__patched_name.===(other)
+      if other.to_s.end_with?(Gem::MAGLEV_POSTFIX)
+        super(other.to_s[0...-Gem::MAGLEV_POSTFIX.size])
+      else
+        super(other)
+      end
+    end
+    @__patched_name
+  end
+
+  def sort_obj
+    # Prioritize MagLev specific gems
+    [@name.end_with?(Gem::MAGLEV_POSTFIX) ? 1 : 0,
+     @name,
+     @version,
+     @new_platform == Gem::Platform::RUBY ? -1 : 1]
+  end
+end
