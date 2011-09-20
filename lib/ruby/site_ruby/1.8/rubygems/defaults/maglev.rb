f05ff0d711bb87f66e3259ea46fd557420369890
Tim Felgentreff <timfelgentreff@gmail.com>
2011-09-20 11:27:21 -0700

    and the final addendum, that symlinks gem directories for custom -maglev- gems, so that we can just 'require' them transparently

--- lib/ruby/site_ruby/1.8/rubygems/defaults/maglev.rb
+++ lib/ruby/site_ruby/1.8/rubygems/defaults/maglev.rb
@@ -14,6 +14,7 @@
 # http://www.mail-archive.com/rubygems-developers@rubyforge.org/msg02161.html
 
 require 'rubygems/config_file'
+require 'fileutils'
 
 module Gem
   # Since maglev-ruby is a shell script, we want the shebang line for shell
@@ -22,7 +23,30 @@ module Gem
   ConfigFile::PLATFORM_DEFAULTS['update']  = '--env-shebang'
 end
 
+# The postfix we are look for to install patched gems released by the MagLev team
 Gem::MAGLEV_POSTFIX = "-maglev-"
+
+# A hook so that after installing a MagLev specific gem, the name of
+# which differs from the actual gem's name, we create a symlink, so
+# Rubygems' require works
+Gem.post_install do |installer|
+  if installer.spec.maglev_specific_gem?
+    spec = installer.spec
+    real_gem_dir = installer.gem_dir.reverse.
+      sub(spec.maglev_real_name.reverse, spec.name.reverse).reverse
+    if File.exist? real_gem_dir
+      case File.ftype(real_gem_dir)
+      when "directory"
+        FileUtils.rm_rf real_gem_dir
+      when "link"
+        File.delete(real_gem_dir)
+      else
+        return false
+      end
+    end
+    File.symlink(installer.gem_dir, real_gem_dir)
+  end
+end
 
 # Override the name accessors of both Gem::Dependency and Gem::Specification.
 # This allows us to publish Gems with the MAGLEV_POSTFIX, but have them report
@@ -56,6 +80,14 @@ class Gem::Dependency
 end
 
 class Gem::Specification
+  def maglev_specific_gem?
+    @name != @__patched_name
+  end
+
+  def maglev_real_name
+    @name.dup
+  end
+
   def name
     return @name if @name.nil?
     return @__patched_name if @__patched_name && @name == @__patched_name
