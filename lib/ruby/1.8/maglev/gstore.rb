b30cb04704de50061b37ae005999b72fe2f590d6
Tim Felgentreff <timfelgentreff@gmail.com>
2011-11-07 17:32:30 +0100

    expose Maglev.transaction_level to allow dealing with nested_transactions better

--- lib/ruby/1.8/maglev/gstore.rb
+++ lib/ruby/1.8/maglev/gstore.rb
@@ -4,25 +4,30 @@
 load "pstore.rb"
 
 # +GStore+ is a MagLev native re-implementation of the Ruby standard
-# library +PStore+.  +GStore+ implements a Gemstone/MagLev based persistence
-# mechanism based on a Hash.  User code can store arbitrary graphs of Ruby
-# objects into the +GStore+ file by name (keys).  User code may later read
-# values back from the data store or update as needed.
+# library +PStore+.  +GStore+ implements a Gemstone/MagLev based
+# persistence mechanism based on a Hash.  User code can store
+# arbitrary graphs of Ruby objects into the +GStore+ file by name
+# (keys).  User code may later read values back from the data store or
+# update as needed.
 #
-# +GStore+ is transactional, which ensures that updates to the store either
-# succeed or fail together.
+# +GStore+ uses a nested transaction. To actually commit across
+# process and to the stone, user code will have to call
+# +Maglev.commit_transaction+ outside a +GStore+ transaction. This is
+# to ensure that using +GStore+ doesn't mess with any user code using
+# the Maglev persistence.
 #
-# +GStore+ supports multiple, independent +GStore+ databases.  PStore relies on
-# Ruby's underlying marshal framework to write the objects to disk.  MagLev
-# persistence does not need to marshal, it writes the object directly to
-# disk.  If a program depends on the marshal hooks being called, then it
-# may not work with +GStore+.  A future version of +GStore+ may address this
-# issue.
+# +GStore+ supports multiple, independent +GStore+ databases.  PStore
+# relies on Ruby's underlying marshal framework to write the objects
+# to disk.  MagLev persistence does not need to marshal, it writes the
+# object directly to disk. If a program depends on the marshal hooks
+# being called, then it may not work with +GStore+.  A future version
+# of +GStore+ may address this issue.
 #
-# +GStore+ manages several "files".  These are really Ruby Hash objects
-# that are stored in the MagLev persistent root +Maglev::PERSISTENT_ROOT+.
-# +Maglev::PERSISTENT_ROOT+ is stored in the standard GemStone VM variable
-# +UserGlobals+, and is available for use by both Ruby and Smalltalk.
+# +GStore+ manages several "files".  These are really Ruby Hash
+# objects that are stored in the MagLev persistent root
+# +Maglev::PERSISTENT_ROOT+.  +Maglev::PERSISTENT_ROOT+ is stored in
+# the standard GemStone VM variable +UserGlobals+, and is available
+# for use by both Ruby and Smalltalk.
 #
 
 class GStore < PStore
@@ -31,8 +36,6 @@ class GStore < PStore
   # as the inital state of the store.  Otherwise, a new, empty Hash is
   # created.
   def initialize(file="")
-    raise PStore::Error, "Commit failed!" unless Maglev.commit_transaction
-
     # @all_data is the Ruby Hash that will store each of the "files"
     # managed by +GStore+.  Each +GStore+ "file" will be a Hash stored in
     # @all_data, keyed by the "file name".  @all_data is kept in
@@ -53,7 +56,7 @@ class GStore < PStore
       @transaction = true
       @rdonly = read_only
       @abort = false
-      Maglev.begin_transaction
+      Maglev.begin_nested_transaction
       @table = @all_data[@filename]
       begin
         catch(:gstore_abort_transaction) do
