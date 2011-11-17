33462e07db318ea3343ddceaf9f750b303b92eb7
Tim Felgentreff <timfelgentreff@gmail.com>
2011-11-17 20:44:46 +0100

    add LIBC constant to FFI::Library. Should fix #107, once #108 is fixed

--- lib/ruby/1.8/ffi.rb
+++ lib/ruby/1.8/ffi.rb
@@ -1,2 +1,15 @@
 # file: ffi.rb
-#  FFI is loaded by the Maglev bootstrap , so this file is empty.
+#  FFI is loaded by the Maglev bootstrap , so this file only deals with
+#  the system-specific things that have to be determined at runtime
+
+module FFI
+  module Library
+    case Object.__platform_str
+    when /darwin/i
+      LIBC = "libc.dylib"
+    # when /x86_64_Solaris/i # do we need this?
+    else
+      LIBC = "libc.so.6"
+    end
+  end
+end
