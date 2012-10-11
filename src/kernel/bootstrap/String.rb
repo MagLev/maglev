MagLev/maglev

commit 7bb772ca761cbb54e5aeda7384ea7b9d260a4bb1

allow aliasing of gsub by providing generic version

src/kernel/bootstrap/String.rb
@@ -731,6 +731,15 @@ class String
     self.__copyfrom_to( from + 1 , to )
   end
 
+  # Generic version of gsub, for aliasing
+  def gsub(regex, str=nil, &block)
+    if str
+      _gsub_internal(regex, str)[0]
+    else
+      _gsub_internal_with_block(regex, &block)
+    end
+  end
+
   # Returns a copy of <i>self</i> with <em>all</em> occurrences of <i>pattern</i>
   # replaced with either <i>replacement</i> or the value of the block. The
   # <i>pattern</i> will typically be a <code>Regexp</code>; if it is a
@@ -839,6 +848,10 @@ class String
 
 
   def gsub(regex, &block)
+    _gsub_internal_with_block(regex, &block)
+  end
+
+  def _gsub_internal_with_block(regex, &block)
     # if block_given?,
     #  $~ and related variables will be valid in block if
     #   blocks's home method and caller's home method are the same

src/test/github146.rb
@@ -1,0 +1,22 @@
+# https://github.com/MagLev/maglev/issues/143
+#
+# gsub has only specialized methods and thus cannot be aliased
+
+$gsub_runs = []
+
+class String
+  def gsub_with_logging(*arg, &block)
+    $gsub_runs << caller
+    gsub_without_logging(*arg, &block)
+  end
+  alias_method :gsub_without_logging, :gsub
+  alias_method :gsub, :gsub_with_logging
+end
+
+"hello".gsub("h", "b")
+raise unless $gsub_runs.size == 1
+
+"hello".gsub("h") { "b" }
+raise unless $gsub_runs.size == 2
+
+# Test passes if no exception is thrown

src/test/vmunit.conf
@@ -440,6 +440,7 @@ github106.rb
 github126.rb
 github140.rb
 github143.rb
+github146.rb
 
 testMarshal.rb   # after other coverage of Hash
 #

commit eb328f597b71946c1724c52b9554141ba603cad8

fix rubyspec failure introduced in 7bb772ca

src/kernel/bootstrap/String.rb
@@ -732,11 +732,36 @@ class String
   end
 
   # Generic version of gsub, for aliasing
+  # ELSE BRANCH COPIED FROM SPECIALIZED VERSION
   def gsub(regex, str=nil, &block)
     if str
       _gsub_internal(regex, str)[0]
     else
-      _gsub_internal_with_block(regex, &block)
+      # if block_given?,
+      #  $~ and related variables will be valid in block if
+      #   blocks's home method and caller's home method are the same
+      unless block_given?
+        # updating of $~ not implemented yet on  this path
+        return StringGsubEnumerator.new(self, :gsub, regex) # for 1.8.7
+      end
+      start = 0
+      out = self.class.__alloc
+      last_match = nil
+      self.__get_pattern(regex, true).__each_match_vcgl(self, 0x30) do |match|
+        last_match = match
+        out.__append_internal(self._gsub_copyfrom_to(start, match.begin(0)))
+        saveTilde = block.__fetchRubyVcGlobal(0)
+        begin
+          block.__setRubyVcGlobal(0, match)
+          out.__append_internal(block.call(match.__at(0)).to_s)
+        ensure
+          block.__setRubyVcGlobal(0, saveTilde)
+        end
+        start = match.end(0)
+      end
+      out.__append_internal(self.__copyfrom_to(start + 1, self.__size))
+      last_match.__storeRubyVcGlobal(0x20) # store into caller's $~
+      out
     end
   end
 
@@ -846,12 +871,9 @@ class String
     out
   end
 
-
+  # specialized version for invocation with block
+  # COPY TO ELSE BRANCH OF GENERIC VERSION ABOVE IF CHANGED
   def gsub(regex, &block)
-    _gsub_internal_with_block(regex, &block)
-  end
-
-  def _gsub_internal_with_block(regex, &block)
     # if block_given?,
     #  $~ and related variables will be valid in block if
     #   blocks's home method and caller's home method are the same