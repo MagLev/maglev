eeab045582ca44526d4a20159d0a0211d6407279
Tim Felgentreff <timfelgentreff@gmail.com>
2012-10-12 08:14:58 +0200

    refactor and support splat-rescue again, this time with late-evaluated contents

--- src/test/github154.rb
+++ src/test/github154.rb
@@ -17,3 +17,25 @@ end
 
 raise "raise got confused" unless $flag
 raise "raise got confused" unless $ensure
+
+$flag = false
+$ensure = false
+
+begin
+  a = []
+  class SomeOtherConst < StandardError
+  end
+  a << SomeOtherConst << ScriptError
+  raise SomeOtherConst.new
+rescue ScriptError
+  raise "should not pass through here"
+rescue *a
+  $flag = true
+else
+  raise "should not execute ELSE"
+ensure
+  $ensure = true
+end
+
+raise "raise got confused by splat rescue" unless $flag
+raise "raise got confused by splat rescue" unless $ensure
