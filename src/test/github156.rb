6015f826968ec8f6a8eea54418e80f060ca318ee
Matthias Springer <me@matthiasspringer.de>
2012-10-16 11:28:43 +0200

    Fixed regression test for github156.

--- src/test/github156.rb
+++ src/test/github156.rb
@@ -2,7 +2,7 @@
 #
 # A subsequent autoload should override the previous autoload.
 
-autoload :Github156Autoload, 'test_data/InvalidAutoload'
-autoload :Github156Autoload, 'test_data/ValidAutoload'
+autoload :Github156Autoload, File.dirname(__FILE__) + '/test_data/InvalidAutoload'
+autoload :Github156Autoload, File.dirname(__FILE__) + '/test_data/ValidAutoload'
 
 raise "Github issue 156: autoload behavior differs from MRI" unless Github156Autoload::evaluate
