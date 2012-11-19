caede3a1128561ccd68c3fd662866a874146ea25
Tim Felgentreff <timfelgentreff@gmail.com>
2012-11-19 18:01:59 +0100

    found where we put the version.txt

--- src/kernel/bootstrap/Globals.rb
+++ src/kernel/bootstrap/Globals.rb
@@ -156,12 +156,12 @@ RUBY_ENGINE = 'maglev'
 # RUBY_PLATFORM  is installed in RubyContext>>_initTransient:
 RUBY_VERSION = '1.9.3'
 # Note - the packager modifies the date on any line starting with RUBY_RELEASE_DATE
-RUBY_RELEASE_DATE = '2012-11-16'
+RUBY_RELEASE_DATE = '2012-12-05'
 RUBY_PATCHLEVEL = 327
 RUBY_DESCRIPTION = 'ruby 1.9.3 (maglev patchlevel 327)'
 RUBY_COPYRIGHT = 'ruby - Copyright (C) 1993-2010 Yukihiro Matsumoto; maglev additions Copyright(C) 2009-2010 GemStone Systems Inc.'
 
-VERSION = '2.0alpha'
+VERSION = '2.0dev'
 
 MAGLEV_VERSION = VERSION  # per Trac 901
 
