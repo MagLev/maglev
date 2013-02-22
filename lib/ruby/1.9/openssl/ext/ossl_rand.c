b63051fd9c90cc98acc8d9804e648537c1408266
Tim Felgentreff <timfelgentreff@gmail.com>
2013-02-22 18:52:02 +0100

    fix OpenSSL::Random.random_bytes

--- lib/ruby/1.9/openssl/ext/ossl_rand.c
+++ lib/ruby/1.9/openssl/ext/ossl_rand.c
@@ -95,15 +95,15 @@ ossl_rand_write_file(VALUE self, VALUE filename)
 static VALUE
 ossl_rand_bytes(VALUE self, VALUE len)
 {
-    VALUE str;
+    unsigned char* buf;
     int n = NUM2INT(len);
 
-    str = rb_str_new(0, n);
-    if (!RAND_bytes((unsigned char *)RSTRING_PTR(str), n)) {
+    buf = (unsigned char*)xmalloc(sizeof(unsigned char) * n);
+    if (!RAND_bytes(buf, n)) {
 	ossl_raise(eRandomError, NULL);
     }
 
-    return str;
+    return rb_str_new(buf, n);
 }
 
 /*
