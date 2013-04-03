5d083b474d1963c58b19438ff1dd3c1ea86093f5
Matthias Springer <me@matthiasspringer.de>
2013-04-03 18:32:27 +0200

    #291: Singleton class generation in GemStone again, generate 2 levels of singleton classes ahead.

--- src/kernel/bootstrap/Class.rb
+++ src/kernel/bootstrap/Class.rb
@@ -6,7 +6,7 @@ class Class
 
   primitive_nobridge 'ancestors' , 'rubyAncestors'
 
-  primitive_nobridge 'superclass', '_rubySuperclass'
+  primitive_nobridge 'superclass', 'superClass'
 
   primitive_nobridge '__ruby_methods', 'rubyMethods:protection:'
 
