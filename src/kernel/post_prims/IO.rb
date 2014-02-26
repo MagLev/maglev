b6007cd5f0133745fdfef4db46487f27d067e960
Tim Felgentreff <timfelgentreff@gmail.com>
2014-02-26 17:12:22 +0100

    add a Pipe<Socket class that can be waited upon using IO.select, but that delegates reading/writing to a File

--- src/kernel/post_prims/IO.rb
+++ src/kernel/post_prims/IO.rb
@@ -16,7 +16,9 @@ class IO
   def self.pipe
     pipefds = FFI::MemoryPointer.new :int, 2
     raise "Error creating pipes" if __cfunctions.pipe(pipefds) != 0
-    r,w = File.__fopen(pipefds[0], 'r'), File.__fopen(pipefds[1], 'w')
+    # r,w = File.__fopen(pipefds[0], 'r'), File.__fopen(pipefds[1], 'w')
+    r = Pipe.__send__(:new, pipefds[0], 'r', self.class)
+    w = Pipe.__send__(:new, pipefds[1], 'w', self.class)
     if block_given?
       begin
         return yield(r,w)
@@ -26,6 +28,58 @@ class IO
       end
     else
       return r,w
+    end
+  end
+
+  class Pipe < Socket
+    def read(*args)
+      @file.read(*args)
+    end
+
+    def readpartial(*args)
+      @file.readpartial(*args)
+    end
+
+    def sysread(*args)
+      @file.sysread(*args)
+    end
+
+    def getc
+      @file.getc
+    end
+
+    def flush
+      @file.flush
+    end
+
+    def fsync
+      @file.fsync
+    end
+
+    def write(*args)
+      @file.write(*args)
+      self
+    end
+    alias_method :<<, :write
+
+    def class
+      @apparant_class
+    end
+
+    private
+    def self.new(fd, mode, apparant_class)
+      r = self.__new_for_fd(fd)
+      r.__send__(:__open_file, fd, mode)
+      r.__send__(:__appear_as_class, apparant_class)
+      r
+    end
+
+    def __open_file(fd, mode)
+      @file = File.__fopen(fd, mode)
+    end
+
+    def __appear_as_class(klass)
+      @apparant_class = klass
     end
   end
 end
---
+++ src/test/github313.rb
@@ -0,0 +1,25 @@
+rp, wp = IO.pipe
+mesg = "ping "
+result = ""
+
+10.times do
+  rs, ws, = IO.select([rp], [wp])
+  # puts "rs: #{rs.inspect}"
+  # puts "ws: #{ws.inspect}"
+  if rs && r = rs[0]
+    ret = r.read(5)
+    result << ret
+    case ret
+    when /ping/
+      mesg = "pong\n"
+    when /pong/
+      mesg = "ping "
+    end
+  end
+  if ws && w = ws[0]
+    w.write(mesg)
+  end
+end
+
+expected = "ping pong\nping pong\nping pong\nping pong\nping "
+raise "IO select did not work" unless result == expected
