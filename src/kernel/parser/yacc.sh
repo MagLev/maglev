913fd74eda248db7abf616c2599562aefa667765
Ryan Davis <ryand-ruby@zenspider.com>
2015-03-16 13:24:12 -0700

    Tweak yacc.sh to generate structural diffs against mri 1.8/1.9.

--- src/kernel/parser/yacc.sh
+++ src/kernel/parser/yacc.sh
@@ -18,7 +18,7 @@ if ! test -x ./byacc/yacc; then
   fi
 fi
 
-rm -f rubygrammar.c
+rm -f rubygrammar.c rubygrammar.h
 ./byacc/yacc -d -g -o rubygrammar.c -P -t -v grammar.y
 if test $? -ne 0; then
   echo "byacc failed"
@@ -28,3 +28,9 @@ chmod -w rubygrammar.h
 chmod -w rubygrammar.c
 # no longer copying to server src directory
 echo "byacc ok"
+
+# for zenspider to get grammatical structure for comparison to MRI
+bison -r all grammar.y && mv grammar.output maglev.output && rm grammar.tab.c
+~/Work/p4/zss/src/ruby_parser/dev/yack.rb maglev.output > maglev.txt
+diff -u ~/Work/p4/zss/src/ruby_parser/dev/yacc18.txt maglev.txt > diff18.diff
+diff -u ~/Work/p4/zss/src/ruby_parser/dev/yacc19.txt maglev.txt > diff19.diff
