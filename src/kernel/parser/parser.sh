57a674b07dbda6cdc5621c3a855e7b0c685b5bfa
Ryan Davis <ryand-ruby@zenspider.com>
2015-04-06 16:51:44 -0700

    Normalized build system to work on both linux and osx.

--- src/kernel/parser/parser.sh
+++ src/kernel/parser/parser.sh
@@ -37,16 +37,21 @@ fi
 case $OS in
     "Linux" )
         EXT=so
+        OSLDFLAGS="-Wl,--version-script=magparse.exp -Wl,--warn-unresolved-symbols -Wl,-Bdynamic,-hlibmagparse.${EXT} -lcrypt -lrt"
+        OSCCWARN="-Wformat"
+        OSCCINC=""
         ;;
     "Darwin")
         EXT=dylib
+        OSLDFLAGS="-Wl,-flat_namespace -Wl,-undefined -Wl,warning -Wl,-exported_symbols_list -Wl,magparse.osx.exp"
+        OSCCWARN=""
+        OSCCINC="-I/usr/local/Cellar/icu4c/54.1/include"
         ;;
 esac
 
-# TODO: probably need to push this into the above case for OS/compiler specifics
-CCWARN="-Wchar-subscripts -Wcomment -Werror -Wformat -Wmissing-braces -Wmultichar -Wno-aggregate-return -Wno-unused-function -Wparentheses -Wreturn-type -Wshadow -Wsign-compare -Wsign-promo -Wswitch -Wsystem-headers -Wtrigraphs -Wtrigraphs -Wuninitialized -Wunused-label -Wunused-value -Wunused-variable -Wwrite-strings"
+CCWARN="-Wchar-subscripts -Wcomment -Werror -Wmissing-braces -Wmultichar -Wno-aggregate-return -Wno-unused-function -Wparentheses -Wreturn-type -Wshadow -Wsign-compare -Wsign-promo -Wswitch -Wsystem-headers -Wtrigraphs -Wtrigraphs -Wuninitialized -Wunused-label -Wunused-value -Wunused-variable -Wwrite-strings $OSCCWARN"
 CCDEF="-DFLG_FAST=1 -DNOT_JAVA_VM -D_GNU_SOURCE -D_REENTRANT"
-CCINC="-I$GEMSTONE/include -I."
+CCINC="-I$GEMSTONE/include -I. $OSCCINC"
 CFLAGS="$CCDEF $CCINC $CCWARN -O3 -fPIC -fcheck-new -fmessage-length=0 -fno-exceptions -fno-strict-aliasing -g -m64 -pipe -pthread -x c++"
 
 $CC $CFLAGS -c rubygrammar.c -o rubygrammar.o
@@ -67,7 +72,7 @@ fi
 rm -f libmagparse.${EXT}
 
 echo "Linking libmagparse.${EXT}"
-LDFLAGS="-shared -Wl,--version-script=magparse.exp -Wl,--warn-unresolved-symbols -Wl,-Bdynamic,-hlibmagparse.${EXT} -lc -lcrypt -ldl -lm -lpthread -lrt -m64"
+LDFLAGS="-shared -lc -ldl -lm -lpthread -m64 $OSLDFLAGS"
 
 $CC $LDFLAGS rubyast.o rubygrammar.o -o libmagparse.${EXT}
 
